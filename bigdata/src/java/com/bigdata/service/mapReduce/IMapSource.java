91cb31a2b2e2ffce0e19a1164e1a65e971d8a275
thompsonbry <thompsonbry@users.sourceforge.net>
2007-09-21 19:51:55 +0000

    Adds a the basis for a map/reduce feature.  This is only running against an embedded federation at present and lacks test cases.
    This will probably also break the build as it relies on other changes.
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@235 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata/src/java/com/bigdata/service/mapReduce/IMapSource.java
@@ -0,0 +1,18 @@
+package com.bigdata.service.mapReduce;
+
+import java.io.File;
+import java.util.Iterator;
+
+/**
+ * Interface responsible for identifying input sources for map tasks. For
+ * example, by scanning the files in a local directory on the map server.
+ */
+public interface IMapSource {
+    
+    /**
+     * An iterator that will visit each source to be read by the map task in
+     * turn.
+     */
+    public Iterator<File> getSources();
+
+}
