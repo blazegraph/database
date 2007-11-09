8b1fc6a72aa194529131d41713487e30c0d50c00
thompsonbry <thompsonbry@users.sourceforge.net>
2007-11-09 13:55:30 +0000

    Working on license consistency.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@361 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/concurrent/ResourceQueue.java
+++ bigdata/src/java/com/bigdata/concurrent/ResourceQueue.java
@@ -1,3 +1,27 @@
+/*
+
+Copyright (C) SYSTAP, LLC 2006-2007.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
 package com.bigdata.concurrent;
 
 import java.util.Iterator;
@@ -10,7 +34,6 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 import org.apache.log4j.Logger;
-
 
 /**
  * Unbounded queue of operations waiting to gain an exclusive lock on a
