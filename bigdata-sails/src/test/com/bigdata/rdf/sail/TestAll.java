6859b45b4a014893e4d50ca3c61061f84dbcb7ed
thompsonbry <thompsonbry@users.sourceforge.net>
2007-11-09 14:41:37 +0000

    Modified to force the default logging level to WARN when running the unit tests from the top-level TestAll.suite() methods IFF it is at DEBUG (the default when log4j is not configured explicitly).  This drammatically reduces the burden on the server running the nightly builds.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@366 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-sails/src/test/com/bigdata/rdf/sail/TestAll.java
+++ bigdata-sails/src/test/com/bigdata/rdf/sail/TestAll.java
@@ -34,6 +34,9 @@ package com.bigdata.rdf.sail;
 
 import java.util.Properties;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -64,6 +67,27 @@ public class TestAll extends TestCase {
 
     public static Test suite() {
 
+        /*
+         * log4j defaults to DEBUG which will produce simply huge amounts of
+         * logging information when running the unit tests. Therefore we
+         * explicitly set the default logging level to WARN. If you are using a
+         * log4j configuration file then this is unlikely to interact with your
+         * configuration, and in any case you can override specific loggers.
+         */
+        {
+
+            Logger log = Logger.getRootLogger();
+
+            if (log.getLevel().equals(Level.DEBUG)) {
+
+                log.setLevel(Level.WARN);
+
+                log.warn("Defaulting debugging level to WARN for the unit tests");
+
+            }
+            
+        }
+        
         TestSuite suite = new TestSuite("Sesame 1.x integration");
         
         // test suite for RDFS closure correctness.
--- bigdata/src/test/com/bigdata/TestAll.java
+++ bigdata/src/test/com/bigdata/TestAll.java
@@ -27,6 +27,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package com.bigdata;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -58,6 +61,27 @@ public class TestAll extends TestCase {
     public static Test suite()
     {
 
+        /*
+         * log4j defaults to DEBUG which will produce simply huge amounts of
+         * logging information when running the unit tests. Therefore we
+         * explicitly set the default logging level to WARN. If you are using a
+         * log4j configuration file then this is unlikely to interact with your
+         * configuration, and in any case you can override specific loggers.
+         */
+        {
+
+            Logger log = Logger.getRootLogger();
+
+            if (log.getLevel().equals(Level.DEBUG)) {
+
+                log.setLevel(Level.WARN);
+
+                log.warn("Defaulting debugging level to WARN for the unit tests");
+
+            }
+            
+        }
+        
         TestSuite suite = new TestSuite("bigdata");
 
         suite.addTest( com.bigdata.cache.TestAll.suite() );
