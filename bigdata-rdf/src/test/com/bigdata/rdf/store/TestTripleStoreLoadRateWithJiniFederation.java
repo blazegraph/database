c09b04b7291191fc784f4e3582a7610acc0013e1
thompsonbry <thompsonbry@users.sourceforge.net>
2008-01-14 13:19:01 +0000

    import cleanup.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@459 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-rdf/src/test/com/bigdata/rdf/store/TestTripleStoreLoadRateWithJiniFederation.java
+++ bigdata-rdf/src/test/com/bigdata/rdf/store/TestTripleStoreLoadRateWithJiniFederation.java
@@ -84,7 +84,7 @@ public class TestTripleStoreLoadRateWithJiniFederation extends AbstractDistribut
 
     public void test_U1() {
         
-        new ConcurrentDataLoader(store, 3/*nthreads*/, 10000 /*bufferCapacity*/,  new File("../rdf-data/lehigh/U1"), new FilenameFilter(){
+        new ConcurrentDataLoader(store, 20/*nthreads*/, 10000 /*bufferCapacity*/,  new File("../rdf-data/lehigh/U1"), new FilenameFilter(){
 
             public boolean accept(File dir, String name) {
                 if(name.endsWith(".owl")) return true;
@@ -97,7 +97,7 @@ public class TestTripleStoreLoadRateWithJiniFederation extends AbstractDistribut
     
     public void test_U10() {
         
-        new ConcurrentDataLoader(store, 3/*nthreads*/, 10000 /*bufferCapacity*/,  new File("../rdf-data/lehigh/U10"), new FilenameFilter(){
+        new ConcurrentDataLoader(store, 10/*nthreads*/, 10000 /*bufferCapacity*/,  new File("../rdf-data/lehigh/U10"), new FilenameFilter(){
 
             public boolean accept(File dir, String name) {
                 if(name.endsWith(".owl")) return true;
