df73f7d1993b8df0515316c0df574d1ae2977dfb
thompsonbry <thompsonbry@users.sourceforge.net>
2008-04-22 10:08:49 +0000

    trapped more exceptions in the SarCPUUtilizationCollector when parsing the timestamp.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@631 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/counters/linux/SarCpuUtilizationCollector.java
+++ bigdata/src/java/com/bigdata/counters/linux/SarCpuUtilizationCollector.java
@@ -349,7 +349,7 @@ public class SarCpuUtilizationCollector extends AbstractProcessCollector
                 final String s = data.substring(0, 11);
                 try {
                     lastModified = StatisticsCollectorForLinux.f.parse(s).getTime();
-                } catch (ParseException e) {
+                } catch (Exception e) {
                     log.warn("Could not parse time: [" + s + "] : " + e);
                     lastModified = System.currentTimeMillis(); // should be pretty close.
                 }
