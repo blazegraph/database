c6d42ea1dae2785acc344ae282329483eac8136a
thompsonbry <thompsonbry@users.sourceforge.net>
2008-09-05 20:16:24 +0000

    commented out unneeded stuff
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@910 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/relation/rule/ISolutionExpander.java
+++ bigdata/src/java/com/bigdata/relation/rule/ISolutionExpander.java
@@ -44,44 +44,44 @@ import com.bigdata.relation.rule.eval.IJoinNexus;
  */
 public interface ISolutionExpander<E> extends Serializable {
 
-    /**
-     * This method is invoked for each solution for a JOIN. The implementation
-     * may cause zero or more solutions to be emitted each time it is invoked.
-     * If the implementation does nothing, then the solution will be discarded.
-     * If the implementation simply emits the solution represented by the
-     * {@link IBindingSet} then the implementation has the semantics of a NOP.
-     * <p>
-     * In addition, if {@link IPredicate#isOptional()} then the
-     * {@link ISolutionExpander} is invoked one more time once all solutions in
-     * the data have been exhausted (and exactly once if there are no solutions
-     * in the data).
-     * 
-     * @param joinNexus
-     * @param bindingSet
-     * @param predicate
-     * @param isSolution
-     */
-    void expand(IJoinNexus joinNexus, IBindingSet bindingSet,
-            IPredicate<E> predicate, boolean isSolution);
-    
-    /**
-     * Return the range count for the {@link IPredicate}. This may be used to
-     * override the default rangeCount behavior. The returned value MAY be
-     * approximate and is ONLY used when computing an {@link IEvaluationPlan}.
-     * In general, the return value will be cached so this method will not be
-     * invoked more than once for a given {@link IPredicate} during the
-     * evaluation of an {@link IRule}.
-     * 
-     * @param accessPath
-     *            The {@link IAccesspath} obtained for the {@link IPredicate}.
-     *            This MAY be based on a fused view of {@link IRelation}s for
-     *            some evaluation context. For example, truth maintenance for
-     *            the RDF DB uses a fused view.
-     * 
-     * @return The #of elements that will be visited by an {@link IAccessPath}
-     *         reading on the {@link IPredicate} assuming NO bindings.
-     */
-    long rangeCount(IAccessPath<E> accessPath);
+//    /**
+//     * This method is invoked for each solution for a JOIN. The implementation
+//     * may cause zero or more solutions to be emitted each time it is invoked.
+//     * If the implementation does nothing, then the solution will be discarded.
+//     * If the implementation simply emits the solution represented by the
+//     * {@link IBindingSet} then the implementation has the semantics of a NOP.
+//     * <p>
+//     * In addition, if {@link IPredicate#isOptional()} then the
+//     * {@link ISolutionExpander} is invoked one more time once all solutions in
+//     * the data have been exhausted (and exactly once if there are no solutions
+//     * in the data).
+//     * 
+//     * @param joinNexus
+//     * @param bindingSet
+//     * @param predicate
+//     * @param isSolution
+//     */
+//    void expand(IJoinNexus joinNexus, IBindingSet bindingSet,
+//            IPredicate<E> predicate, boolean isSolution);
+//    
+//    /**
+//     * Return the range count for the {@link IPredicate}. This may be used to
+//     * override the default rangeCount behavior. The returned value MAY be
+//     * approximate and is ONLY used when computing an {@link IEvaluationPlan}.
+//     * In general, the return value will be cached so this method will not be
+//     * invoked more than once for a given {@link IPredicate} during the
+//     * evaluation of an {@link IRule}.
+//     * 
+//     * @param accessPath
+//     *            The {@link IAccesspath} obtained for the {@link IPredicate}.
+//     *            This MAY be based on a fused view of {@link IRelation}s for
+//     *            some evaluation context. For example, truth maintenance for
+//     *            the RDF DB uses a fused view.
+//     * 
+//     * @return The #of elements that will be visited by an {@link IAccessPath}
+//     *         reading on the {@link IPredicate} assuming NO bindings.
+//     */
+//    long rangeCount(IAccessPath<E> accessPath);
     
     /**
      * Return the {@link IAccessPath} that will be used to evaluate the
