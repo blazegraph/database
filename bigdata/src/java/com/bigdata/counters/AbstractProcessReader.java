9b57ee07a60933e996ead9f382bc6d4ae2f94e50
thompsonbry <thompsonbry@users.sourceforge.net>
2008-10-27 12:33:19 +0000

    conditional logging and reduce use of Thread.currentThread()
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@1028 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/counters/AbstractProcessReader.java
+++ bigdata/src/java/com/bigdata/counters/AbstractProcessReader.java
@@ -2,7 +2,6 @@ package com.bigdata.counters;
 
 import java.io.InputStream;
 
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 /**
@@ -19,14 +18,12 @@ public abstract class AbstractProcessReader implements Runnable {
     /**
      * True iff the {@link #log} level is DEBUG or less.
      */
-    final protected static boolean DEBUG = log.getEffectiveLevel().toInt() <= Level.DEBUG
-            .toInt();
+    final protected static boolean DEBUG = log.isInfoEnabled();
 
     /**
      * True iff the {@link #log} level is INFO or less.
      */
-    final protected static boolean INFO = log.getEffectiveLevel().toInt() <= Level.INFO
-            .toInt();
+    final protected static boolean INFO = log.isDebugEnabled();
 
     /**
      * The {@link InputStream} from which the output of the process will be
@@ -43,7 +40,8 @@ public abstract class AbstractProcessReader implements Runnable {
      */
     public void start(InputStream is) {
 
-        log.info("");
+        if(INFO) 
+            log.info("");
 
         if (is == null)
             throw new IllegalArgumentException();
