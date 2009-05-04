ece1981d5e57f8b3a704908fe7df801082a21462
mrpersonick <mrpersonick@users.sourceforge.net>
2009-05-04 21:11:08 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@1530 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/btree/IndexSegmentDumpUtil.java
+++ bigdata/src/java/com/bigdata/btree/IndexSegmentDumpUtil.java
@@ -1,6 +1,8 @@
 package com.bigdata.btree;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 
 import com.bigdata.btree.IndexSegment.ImmutableLeafCursor;
 import com.bigdata.btree.IndexSegment.ImmutableNodeFactory.ImmutableLeaf;
@@ -23,15 +25,54 @@ public class IndexSegmentDumpUtil {
             
             File file = new File(args[0]);
             
+            // File file = new File("D:\\temp\\segs\\U100000_spo_SPO_part00139_51095.seg");
+            
             IndexSegmentStore store = new IndexSegmentStore(file);
             
             IndexSegment ndxSegment = store.loadIndexSegment();
             
+            // wait for enter
+            
+            long before = Runtime.getRuntime().freeMemory();
+            
+            System.err.println("free memory before loading first leaf: " + before);
+            
+            System.err.println("take your memory dump and then press enter to load the first leaf");
+            
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            
+            String input = reader.readLine();
+            
             ImmutableLeafCursor cursor = ndxSegment.newLeafCursor(SeekEnum.First);
             
             ImmutableLeaf leaf = cursor.first();
+
+            long after = Runtime.getRuntime().freeMemory();
             
-            Thread.sleep(120*1000);  // gives you 2 minutes to go get the memory dump
+            long used = before - after;
+            
+            System.err.println("free memory after loading first leaf: " + after);
+            
+            System.err.println("free memory used loading first leaf: " + used);
+            
+            // byte counts on disk
+            
+            System.err.println("bytes on disk for root: " + store.getByteCount(store.getCheckpoint().addrRoot));
+
+            System.err.println("bytes on disk for first leaf: " + store.getByteCount(store.getCheckpoint().addrFirstLeaf));
+            
+            System.err.println("take your memory dump and then press enter to end the program");
+            
+            input = reader.readLine();
+            
+            // hit enter
+            
+            // compare free memory before and after loading leaf
+            
+            // measuer of inflation in memory
+            
+            // exclude weak references, leaves and nodes have weak refs to segment
+            // total retained memory for node and leaf
             
         } catch (Exception ex) {
             
