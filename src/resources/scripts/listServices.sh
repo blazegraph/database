361e0b0150376758e3085a4c2f7ae07e73d63abf
thompsonbry <thompsonbry@users.sourceforge.net>
2009-05-12 18:37:51 +0000

    changing how jini is started to use a non-activatable service descriptor
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@1580 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- src/resources/scripts/listServices.sh
+++ src/resources/scripts/listServices.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Lists the discovered services.
+#
+# Note: You can override the repeatCount by adding the following to the
+# command line.
+#
+# com.bigdata.service.jini.util.ListServices.repeatCount=0
 
 source `dirname $0`/bigdataenv
 
 java ${JAVA_OPTS} \
     com.bigdata.service.jini.util.ListServices \
-    ${BIGDATA_CONFIG} ${BIGDATA_CONFIG_OVERRIDES}
-
+    ${BIGDATA_CONFIG} ${BIGDATA_CONFIG_OVERRIDES} $*
