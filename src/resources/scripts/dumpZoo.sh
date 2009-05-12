ef2049bd3a501f60ca2424fb278af4feef9ec663
thompsonbry <thompsonbry@users.sourceforge.net>
2009-05-12 15:20:50 +0000

    various patches to config files
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@1563 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- src/resources/scripts/dumpZoo.sh
+++ src/resources/scripts/dumpZoo.sh
@@ -10,4 +10,4 @@ source `dirname $0`/bigdataenv
 java ${JAVA_OPTS} \
     -cp ${CLASSPATH} \
     com.bigdata.zookeeper.DumpZookeeper \
-    /opt2/src/bigdata-lubm/src/resources/config/bigdataCluster.config
+     ${BIGDATA_CONFIG} ${BIGDATA_CONFIG_OVERRIDES}
