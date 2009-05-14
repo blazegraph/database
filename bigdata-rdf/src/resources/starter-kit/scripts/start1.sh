656efe0601b142ae373ebc43aa945886a7e653b0
thompsonbry <thompsonbry@users.sourceforge.net>
2009-05-14 15:59:02 +0000

    added the lib-dl and lib-ext directories of jars for jini 2.1 and moved the existing jini jars so as to have the same directory structure as the jini 2.1 distribution for the bundled JARs.  Modified the jini configuration (startAll.config) to use the new directory structure for those JARs.  Removed the JINI_HOME environment variable as it is no longer used.
    Removed the old start-kit stuff.
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@1617 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-rdf/src/resources/starter-kit/scripts/start1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Clear out persistent data.
-rm -rf /var/bigdata
-
-cd /nas/config/DataServer0 && /nas/scripts/startService.sh com.bigdata.service.jini.DataServer && sleep 1
-
-cd /nas/config/DataServer1 && /nas/scripts/startService.sh com.bigdata.service.jini.DataServer && sleep 1
