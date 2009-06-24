ad0a3c78c99c793b1dada48548f815010ac1e4a0
thompsonbry <thompsonbry@users.sourceforge.net>
2009-06-24 21:09:40 +0000

    added a variant script which can be used to generate the data for N clients, where the data for each client goes into its own subdirectory
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@1852 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-lubm/resources/scripts/lubmGen.sh
+++ bigdata-lubm/resources/scripts/lubmGen.sh
@@ -7,15 +7,15 @@ source `dirname $0`/bigdataenv
 #
 
 if [ -z "$1" ]; then
-	echo $"usage: $0 <univ_num>"
-	echo "	univ_num is the #of universities."
-	echo ""
-	echo "WARNING: Output is written into the CURRENT working directory."
-	exit 1
+    echo $"usage: $0 <univ_num>"
+    echo "	univ_num is the #of universities."
+    echo ""
+    echo "WARNING: Output is written into the CURRENT working directory."
+    exit 1
 fi
 
 java ${JAVA_OPTS} \
-	-Xmx400m \
+    -Xmx400m \
     -cp @install.lubm.lib.dir@/bigdata-lubm.jar \
     edu.lehigh.swat.bench.uba.Generator \
     -univ $1 \
