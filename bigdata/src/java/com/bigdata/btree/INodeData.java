0a68cb35f162260c57720c07723b23114e47342f
thompsonbry <thompsonbry@users.sourceforge.net>
2009-08-24 13:20:32 +0000

    checkpoint before refactor of the Leaf and Node impls to provide indirection of the ILeafData and INodeData APIs.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BTREE_BUFFER_BRANCH@2028 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/btree/INodeData.java
+++ bigdata/src/java/com/bigdata/btree/INodeData.java
@@ -45,46 +45,15 @@ public interface INodeData extends IAbstractNodeData {
     public int getChildCount();
 
     /**
-     * The backing array of the persistent addresses of the children. Only the
-     * first {@link #getChildCount()} entries in the returned array are defined.
-     * If an entry is zero(0L), then the corresponding child is not persistent.
-     * The use of this array is dangerous since mutations are directly reflected
-     * in the node, but it may be highly efficient. Callers MUST exercise are to
-     * perform only read-only operations against the returned array.
-     * 
-     * @return The backing array of persistent child addresses.
-     * 
-     * @deprecated This is only used to persistent the child addresses, which is
-     *             done by the {@link NodeSerializer}. With the refactor to use
-     *             a binary image for the persistent nodes and leaves we will no
-     *             need this method. It can disappear into the implementation.
-     *             <p>
-     *             However, getChildAddr(int) will be needed by
-     *             {@link Node#getChild(int)}.
-     */
-    public long[] getChildAddr();
-
-    /**
      * Return the persistent addresses of the specified child node.
      * 
      * @param index
-     *            The index of the child in [0:nkeys+1].
+     *            The index of the child in [0:nkeys].
      * 
      * @return The persistent child address -or- zero(0L) if the child is not
      *         persistent.
      */
     public long getChildAddr(int index);
-
-//    /**
-//     * The #of entries (aka keys or values) spanned by each child of this node.
-//     * The sum of the defined values in this array should always be equal to the
-//     * value returned by {@link #getSpannedTupleCount()}. These data are used to
-//     * support fast computation of the index at which a key occurs and the #of
-//     * entries in a given key range.
-//     * 
-//     * @deprecated by #getChildEntryCount(int).
-//     */
-//    public int[] getChildEntryCounts();
 
     /**
      * Return the #of tuples spanned by the indicated child of this node. The
@@ -94,7 +63,7 @@ public interface INodeData extends IAbstractNodeData {
      * key occurs and the #of entries in a given key range.
      * 
      * @param index
-     *            The index of the child in [0:nkeys+1].
+     *            The index of the child in [0:nkeys].
      * 
      * @return The #of tuples spanned by that child.
      */
