d31f2f25cb17ab819685986c2e200a500b228c41
mrpersonick <mrpersonick@users.sourceforge.net>
2009-09-01 14:40:07 +0000

    turned off the auto-commit feature entirely
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@2036 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-sails/src/java/com/bigdata/rdf/sail/BigdataSailRepositoryConnection.java
+++ bigdata-sails/src/java/com/bigdata/rdf/sail/BigdataSailRepositoryConnection.java
@@ -4,6 +4,7 @@ import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.parser.ParsedGraphQuery;
 import org.openrdf.query.parser.QueryParserUtil;
+import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.sail.SailGraphQuery;
 import org.openrdf.repository.sail.SailRepositoryConnection;
 import org.openrdf.sail.SailConnection;
@@ -28,4 +29,20 @@ public class BigdataSailRepositoryConnection extends SailRepositoryConnection {
         return new BigdataSailGraphQuery(parsedQuery, this);
         
     }
+
+    @Override
+    public void commit() throws RepositoryException {
+        
+        // auto-commit is heinously inefficient
+        if (isAutoCommit()) {
+            
+            throw new RuntimeException(
+                    "auto-commit not supported, please setAutoCommit(false)");
+            
+        }
+        
+        super.commit();
+    }
+    
+    
 }
