a18c80690ebcbf5be085122b3e11fb3ab2b1f267
thompsonbry <thompsonbry@users.sourceforge.net>
2010-02-05 14:56:59 +0000

    It turns out that the pipeline join does not respect isStable() or ISlice constraints.  I have added notes to this effect to the TestSlice test suite and to the pipeline join.  I plan to fix these issues when we refactor the pipeline join to impose threading constraints (a fixed pool of join threads).  In the meantime, query hints can be used to specify the nested subquery join if you need stable evaluation.  In the long run, SPARQL aware query caching is the right way to handle stable query and SLICE for scale-out since a stable evaluation order can impose far too much constraint on parallelism otherwise.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/DEV_BRANCH_27_OCT_2009@2393 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/relation/rule/eval/pipeline/LocalJoinMasterTask.java
+++ bigdata/src/java/com/bigdata/relation/rule/eval/pipeline/LocalJoinMasterTask.java
@@ -36,8 +36,8 @@ public class LocalJoinMasterTask extends JoinMasterTask {
      * @param joinNexus
      * @param buffer
      */
-    public LocalJoinMasterTask(IRule rule, IJoinNexus joinNexus,
-            IBuffer<ISolution[]> buffer) {
+    public LocalJoinMasterTask(final IRule rule, final IJoinNexus joinNexus,
+            final IBuffer<ISolution[]> buffer) {
 
         super(rule, joinNexus, buffer);
 
@@ -91,11 +91,6 @@ public class LocalJoinMasterTask extends JoinMasterTask {
 
             // the predicate for this join dimension.
             final IPredicate predicate = rule.getTail(orderIndex);
-            
-//            // the index on which that predicate must read.
-//            // FIXME This must allow for a local view in order to support TM. 
-//            final String indexName = predicate.getOnlyRelationName()
-//                    + ruleState.getKeyOrder()[order[orderIndex]];
 
             // source for this join dimension.
             final IAsynchronousIterator<IBindingSet[]> src = sources[orderIndex];
