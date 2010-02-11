ab890d21c7e5542840e7e69705bdb54f58e15cb3
thompsonbry <thompsonbry@users.sourceforge.net>
2010-02-11 22:06:40 +0000

    Javadoc edits to the IWriteCache interface.
    Stubbed out most of the WriteCacheService implementation.  flush() still needs to be written and needs a sense of an "write set" on which it waits and some synchronization with the write task as buffers are written onto the backing channel.  And unit tests.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/DEV_BRANCH_27_OCT_2009@2443 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/io/IWriteCache.java
+++ bigdata/src/java/com/bigdata/io/IWriteCache.java
@@ -42,7 +42,10 @@ import java.util.concurrent.TimeoutException;
 public interface IWriteCache {
 
     /**
-     * Write the record on the cache.
+     * Write the record on the cache. This interface DOES NOT provide any
+     * guarantee about the ordering of writes. Callers who require a specific
+     * ordering must coordinate that ordering themselves, e.g., by synchronizing
+     * across their writes onto the cache.
      * 
      * @param offset
      *            The offset of that record (maybe relative to a base offset).
