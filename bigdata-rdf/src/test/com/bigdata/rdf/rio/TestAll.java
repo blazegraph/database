f21146fddb176e246ab0cef50ac44102573125a7
thompsonbry <thompsonbry@users.sourceforge.net>
2010-04-01 00:47:51 +0000

    Since the CI problem does not appear to be local to the AsynchronousStatementBuffer test suite, I've instead disabled the RDF EmbeddedFederation test suite to see if we can get a CI build to complete (including the SAIL tests).
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@2566 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-rdf/src/test/com/bigdata/rdf/rio/TestAll.java
+++ bigdata-rdf/src/test/com/bigdata/rdf/rio/TestAll.java
@@ -79,15 +79,8 @@ public class TestAll extends TestCase {
         /*
          * Correctness tests for the asynchronous bulk data loader. This
          * requires the scale-out architecture. SIDs are not supported yet.
-         * 
-         * FIXME This has been temporarily disabled while we get CI working.
          */
-        if (false)
-            suite.addTestSuite(TestAsynchronousStatementBufferFactory.class);
-        else
-            System.err.println("Not running: "
-                    + TestAsynchronousStatementBufferFactory.class + " from "
-                    + TestAll.class);
+        suite.addTestSuite(TestAsynchronousStatementBufferFactory.class);
 
         return suite;
         
