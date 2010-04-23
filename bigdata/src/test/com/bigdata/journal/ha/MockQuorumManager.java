d2ee3d8086ce887dd91136f9845a74090080afe8
thompsonbry <thompsonbry@users.sourceforge.net>
2010-04-23 13:18:54 +0000

    added mock quorum manager
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/JOURNAL_HA_BRANCH@2681 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata/src/test/com/bigdata/journal/ha/MockQuorumManager.java
@@ -0,0 +1,52 @@
+/**
+
+Copyright (C) SYSTAP, LLC 2006-2010.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/*
+ * Created on Apr 23, 2010
+ */
+
+package com.bigdata.journal.ha;
+
+class MockQuorumManager implements QuorumManager {
+
+    public void assertQuorum(long token) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public Quorum awaitQuorum() throws InterruptedException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Quorum getQuorum() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public int replicationFactor() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
