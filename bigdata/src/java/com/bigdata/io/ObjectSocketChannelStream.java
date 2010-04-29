cb2276e3cf916881bad810fe5b3e47c96d6628c5
martyncutcher <martyncutcher@users.sourceforge.net>
2010-04-29 15:41:41 +0000

    sync
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/JOURNAL_HA_BRANCH@2726 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/io/ObjectSocketChannelStream.java
+++ bigdata/src/java/com/bigdata/io/ObjectSocketChannelStream.java
@@ -24,11 +24,17 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package com.bigdata.io;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.ByteChannel;
 import java.nio.channels.Channel;
+import java.nio.channels.Channels;
 import java.nio.channels.SocketChannel;
 
 /**
@@ -38,29 +44,168 @@ import java.nio.channels.SocketChannel;
  *
  */
 public class ObjectSocketChannelStream {
-	final ObjectOutputStream outStr;
-	final ObjectInputStream inStr;
-	final SocketChannel channel;
+	ObjectOutputStream outStr = null;
+	ObjectInputStream inStr = null;
+	final ByteChannel channel;
+	
+	byte[] buf = new byte[2048];
 
-	public ObjectSocketChannelStream(Socket socket) {
-		this.channel = socket.getChannel();
-		try {
-			this.inStr = new ObjectInputStream(socket.getInputStream());
-			this.outStr = new ObjectOutputStream(socket.getOutputStream());
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
+	public ObjectSocketChannelStream(ByteChannel channel) {
+		this.channel = channel;
 	}
 	
-	public SocketChannel getChannel() {
+	public ByteChannel getChannel() {
 		return channel;
 	}
 	
+	public void writeObject(Object obj) throws IOException {
+		ByteArrayOutputStream baout = new ByteArrayOutputStream();
+		ObjectOutputStream outobj = new ObjectOutputStream(baout);
+		
+		outobj.writeObject(obj);
+		outobj.flush();
+		byte[] buf = baout.toByteArray();
+		channel.write(ByteBuffer.wrap(buf));
+	}
+	
+	public Object readObject() throws IOException {
+		ByteArrayOutputStream baout = new ByteArrayOutputStream();
+		ObjectOutputStream outobj = new ObjectOutputStream(baout);
+		
+		// outobj.writeObject(obj);
+		outobj.flush();
+		byte[] buf = baout.toByteArray();
+		channel.write(ByteBuffer.wrap(buf));
+		
+		return null;
+	}
+	
 	public ObjectInputStream getInputStream() {
+		if (inStr == null) {
+			try {
+				this.inStr = new ObjectInputStream(Channels.newInputStream(channel));
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+			
+		}
 		return inStr;
 	}
 	
 	public ObjectOutputStream getOutputStream() {
+		if (outStr == null) {
+			try {
+				this.outStr = new ObjectOutputStream(Channels.newOutputStream(channel));
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
 		return outStr;
+	}
+
+	private void checkBuffer(int sze) {
+		if (buf.length < sze) {
+			buf = new byte[sze + 2048];
+		}
+	}
+	public byte[] readByteArray(final int sze) throws IOException {
+		checkBuffer(sze);
+		
+		int totrd = 0;
+		int tsze = sze;
+		while (tsze > 0) {
+			int rdlen = getInputStream().read(buf, totrd, tsze);
+			tsze -= rdlen;
+			totrd += rdlen;
+		}
+		System.out.println("Read buffer of " + sze + " bytes, actual: " + totrd);
+		
+		return buf;
+	}
+
+	
+	public void write(ByteBuffer tmp) throws IOException {
+		int sze = tmp.position();
+		tmp.position(0);
+		byte[] loc = null;
+		if (tmp.hasArray()) {
+			loc = tmp.array();
+		} else {
+			checkBuffer(sze);			
+			tmp.get(buf, 0, sze);
+			loc = buf;
+		}
+		System.out.println("Writing buffer of " + sze + " bytes");
+		getOutputStream().write(loc, 0, sze);
+	}
+	
+	public static OutputStream newOutputStream(final ByteChannel channel) {
+		
+	    return new OutputStream() {
+			final ByteBuffer buf = ByteBuffer.allocate(2048);
+
+			public synchronized void write(int b) throws IOException {
+	        	if (buf.remaining() == 0) {
+	        		flush();
+	        	}
+	        	
+	            buf.put((byte) b);
+	        }
+
+	        public synchronized void write(byte[] bytes, int off, int len) throws IOException {
+	        	int rem = buf.remaining();
+	        	while (len > rem) {
+	        		len -= rem;
+	        		buf.put(bytes, off, rem);
+	        		off += rem;
+	        		flush();
+	        		rem = buf.remaining();
+	        	}
+	            buf.put(bytes, off, len);
+	        }
+	        
+	        public synchronized void flush() {
+	        	try {
+					channel.write(buf);
+					buf.reset();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        }
+	    };
+	}
+
+	// Returns an input stream for a ByteBuffer.
+	// The read() methods use the relative ByteBuffer get() methods.
+	public static InputStream newInputStream(final ByteChannel channel) {
+		final ByteBuffer buf = ByteBuffer.allocate(2048);
+
+		return new InputStream() {
+	        public synchronized int read() throws IOException {
+	            if (!buf.hasRemaining()) {
+	                channel.read(buf);
+	            }
+	            return buf.get();
+	        }
+
+	        public synchronized int read(byte[] bytes, int off, int len) throws IOException {
+	        	int rem = buf.remaining();
+
+	        	while (rem < len) {
+	        		if (rem > 0) {
+		            	buf.get(bytes, off, rem);
+		            	off += rem;
+		            	len -= rem;
+	        		}
+	            	channel.read(buf);
+	            	rem = buf.remaining();
+	            }
+	            
+	            buf.get(bytes, off, len);
+	            
+	            return len;
+	        }
+	    };
 	}
 }
