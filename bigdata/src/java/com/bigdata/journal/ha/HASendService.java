9e7094187ff64a3bc205d98cbf866ade14e0822e
thompsonbry <thompsonbry@users.sourceforge.net>
2010-05-14 12:41:24 +0000

    code and javadoc review on the HASendService
    javadoc edit on AbstractJournal's Environment impl.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/JOURNAL_HA_BRANCH@2811 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/journal/ha/HASendService.java
+++ bigdata/src/java/com/bigdata/journal/ha/HASendService.java
@@ -28,72 +28,234 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.nio.channels.WritableByteChannel;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.RejectedExecutionException;
 
 import org.apache.log4j.Logger;
 
 /**
- * This class has a private single-threaded Executor to which it submits a SendTask (implements Callable<Void>).  
- * The SendTask will open a blocking-mode socket connection to the specified service and send the bytes remaining 
- * in the buffer to that service.  The data will be sent on the socket using WritableByteChannel#write(buffer).  
- * Since this is a blocking-mode connection, the call should block until all bytes have been sent or the current 
- * thread is interrupted, e.g., by cancelling its Future.  Regardless, verify that all bytes were sent as a 
- * post-condition (position() == limit).  If there is any problem, then the socket is closed and the original 
- * exception is thrown out of call().  If the socket is closed from the other end while we are still writing data, 
- * then that exception gets thrown out as well.  The socket connection is closed as a post-condition (it does 
- * not survive across multiple sends).  Closing the socket connection after each success or unsuccessful send 
- * gives us a strong indication of success or failure for the data transfer which is independent of the RMI 
- * message and makes it trivial to re-synchronize the HASendService since it is basically stateless.
+ * A service for sending raw {@link ByteBuffer}s across a socket. This service
+ * supports the HA write pipeline. This service is designed to be paired with an
+ * {@link HAReceiveService}, which typically is running on a different host. The
+ * {@link HASendService} provides only an efficient raw data transfer. The HA
+ * write pipeline coordinates the transfer of data using RMI messages which tell
+ * the receiver how much data to expect, the checksum of the data, etc.
  * 
- * See HAReceiveService for downstream process
+ * <h2>Implementation</h2>
+ * 
+ * This class has a private single-threaded Executor to which it submits a
+ * {@link SendTask}. The {@link SendTask} will open a blocking-mode
+ * {@link SocketChannel} to the service at the configured
+ * {@link InetSocketAddress} and send the bytes remaining in a
+ * {@link ByteBuffer} to that service on that {@link SocketChannel}. The data
+ * will be sent on the socket using
+ * {@link WritableByteChannel#write(ByteBuffer)}, which is optimized for the NIO
+ * transfer of direct {@link ByteBuffer}s. Since this is a blocking-mode
+ * connection, the write operation will block until all bytes have been sent or
+ * the current thread is interrupted, e.g., by cancelling its Future.
+ * <p>
+ * The {@link SendTask} verifies that all bytes were sent as a post-condition
+ * (position() == limit). If there is any problem, then the
+ * {@link SocketChannel} is closed and the original exception is thrown out of
+ * {@link SendTask#call()}. If the socket is closed from the other end while we
+ * are still writing data, then that exception gets thrown out as well. The
+ * socket connection is closed as a post-condition (it does not survive across
+ * multiple sends). Closing the socket connection after each success or
+ * unsuccessful send gives us a strong indication of success or failure for the
+ * data transfer which is independent of the RMI message and makes it trivial to
+ * re-synchronize the {@link HASendService} since it is basically stateless.
+ * 
+ * @see HAReceiveService
  * 
  * @author Martyn Cutcher
- *
+ * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan Thompson</a>
  */
 public class HASendService {
 	
     protected static final Logger log = Logger.getLogger(HASendService.class);
 
-    private InetSocketAddress inetSocketAddress;
-	private ExecutorService executor = Executors.newSingleThreadExecutor();
-	
-	public HASendService(final InetSocketAddress inetSocketAddress) {
-		this.inetSocketAddress = inetSocketAddress;
+    /**
+     * The Internet socket address of the receiving service.
+     */
+    private final InetSocketAddress addr;
+
+    /**
+     * A single threaded executor on which {@link SendTask}s will be executed.
+     */
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    /**
+     * Starts an {@link HASendService} which will transfer data to a service
+     * listening at the specified {@link InetSocketAddress}.
+     * 
+     * @param addr The Internet socket address of the receiving service.
+     */
+    public HASendService(final InetSocketAddress addr) {
+        
+        if (addr == null)
+            throw new IllegalArgumentException();
+		
+        this.addr = addr;
+        
 	}
 
-	SocketChannel openChannel() throws IOException {
-		SocketChannel socketChannel = SocketChannel.open();
+    /**
+     * Ensure that the private executor service is always terminated.
+     */
+    protected void finalize() throws Exception {
+        
+        terminate();
+        
+    }
+    
+    /**
+     * Immediate shutdown.  Any transfer in process will be interrupted.
+     */
+    public void terminate() {
+        
+        executor.shutdownNow();
+        
+    }
+    
+    /**
+     * Send the bytes {@link ByteBuffer#remaining()} in the buffer to the
+     * configured {@link InetSocketAddress}. This operation DOES NOT have a side
+     * effect on the position, limit or mark for the buffer.
+     * <p>
+     * Note: In order to use efficient NIO operations this MUST be a direct
+     * {@link ByteBuffer}.
+     * 
+     * @param buffer
+     *            The buffer.
+     * 
+     * @return The {@link Future} which can be used to await the outcome of this
+     *         operation.
+     * 
+     * @throws IllegalArgumentException
+     *             if the buffer is <code>null</code>.
+     * @throws IllegalArgumentException
+     *             if the buffer is empty (no bytes remaining).
+     * @throws RejectedExecutionException
+     *             if this service has been shutdown.
+     */
+	public Future<Void> send(final ByteBuffer buffer) {
+
+        if (buffer == null)
+            throw new IllegalArgumentException();
+
+        if (buffer.remaining() == 0)
+            throw new IllegalArgumentException();
+	 
+        // Note: wrapped as a read-only buffer to prevent side-effects.
+	    return executor.submit(newSendTask(buffer.asReadOnlyBuffer()));
+	    
+	}
+
+    /**
+     * Factory for the {@link SendTask}.
+     * 
+     * @param buffer
+     *            The buffer whose data are to be sent.
+     *            
+     * @return The task which will send the data to the configured
+     *         {@link InetSocketAddress}.
+     */
+    protected Callable<Void> newSendTask(final ByteBuffer buffer) {
+
+        return new SendTask(addr, buffer);
+	    
+    }
+
+    /**
+     * Open a blocking mode socket channel to the specified socket address.
+     * 
+     * @param addr
+     *            The socket address.
+     * 
+     * @return The socket channel.
+     * 
+     * @throws IOException
+     */
+    static protected SocketChannel openChannel(final InetSocketAddress addr)
+            throws IOException {
+        
+        final SocketChannel socketChannel = SocketChannel.open();
+
         socketChannel.configureBlocking(true);
 
         if (log.isTraceEnabled())
-        	log.trace("Connecting to " + inetSocketAddress);
-        
-        socketChannel.connect(inetSocketAddress);
-        socketChannel.finishConnect();
-        return socketChannel;
-	}
-	
-	public Future<Void> send(final ByteBuffer data) {
-		return executor.submit(new Callable<Void>() {
+            log.trace("Connecting to " + addr);
 
-			public Void call() throws Exception {
-				SocketChannel socketChannel = openChannel();
-				try {
-					socketChannel.write(data);
-					
-					// check all data written
-					if (data.position() != data.limit()) {
-						throw new RuntimeException("Not all data has been sent");
-					}
-				} finally {
-					socketChannel.close();
-				}
-				
-				return null;
-			}});
-	}
+        socketChannel.connect(addr);
+        
+        socketChannel.finishConnect();
+
+        return socketChannel;
+        
+    }
+
+    /**
+     * This task implements the raw data transfer. Each instance of this task
+     * sends the {@link ByteBuffer#remaining()} bytes in a single
+     * {@link ByteBuffer} to the receiving service on a specified
+     * {@link InetSocketAddress}.
+     */
+    protected static class SendTask implements Callable<Void> {
+
+        private final InetSocketAddress addr;
+        private final ByteBuffer data;
+
+        public SendTask(final InetSocketAddress addr, final ByteBuffer data) {
+
+            if (addr == null)
+                throw new IllegalArgumentException();
+
+            if (data == null)
+                throw new IllegalArgumentException();
+
+            this.addr = addr;
+            
+            this.data = data;
+
+        }
+
+        public Void call() throws Exception {
+
+            // The #of bytes to transfer.
+            final int remaining = data.remaining();
+            
+            // Open the blocking-mode socket channel.
+            final SocketChannel socketChannel = openChannel(addr);
+            try {
+
+                /*
+                 * Write the data -- should block until finished or until this
+                 * thread is interrupted, e.g., by shutting down the thread pool
+                 * on which it is running.
+                 */
+                socketChannel.write(data);
+
+            } finally {
+
+                // always close the socket.
+                socketChannel.close();
+
+            }
+
+            if(log.isTraceEnabled())
+                log.trace("Sent "+remaining+" bytes to "+addr);
+            
+            // check all data written
+            assert data.remaining() == 0 : "remaining=" + data.remaining();
+
+            return null;
+            
+        }
+
+    }
+
 }
