efca81c10ee2551e8e9a4b04af26b004c0fe12e8
martyncutcher <martyncutcher@users.sourceforge.net>
2010-05-17 17:48:53 +0000

    add checksum unit test
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/JOURNAL_HA_BRANCH@2838 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/test/com/bigdata/journal/ha/TestHASendAndReceive3Nodes.java
+++ bigdata/src/test/com/bigdata/journal/ha/TestHASendAndReceive3Nodes.java
@@ -69,209 +69,246 @@ public class TestHASendAndReceive3Nodes extends TestCase3 {
 
 	}
 
-    /**
-     * Returns random data that will fit in <i>nbytes</i>.
-     * 
-     * @return A new {@link ByteBuffer} wrapping a new <code>byte[]</code>
-     *         having random contents.
-     */
-    public ByteBuffer getRandomData(final int nbytes) {
+	/**
+	 * Returns random data that will fit in <i>nbytes</i>.
+	 * 
+	 * @return A new {@link ByteBuffer} wrapping a new <code>byte[]</code>
+	 *         having random contents.
+	 */
+	public ByteBuffer getRandomData(final int nbytes) {
 
-        final byte[] bytes = new byte[nbytes];
+		final byte[] bytes = new byte[nbytes];
 
-        r.nextBytes(bytes);
+		r.nextBytes(bytes);
 
-        return ByteBuffer.wrap(bytes);
+		return ByteBuffer.wrap(bytes);
 
-    }
+	}
 
-    /**
-     * Returns random data that will fit in <i>nbytes</i>.
-     * 
-     * @return A new {@link ByteBuffer} wrapping a new <code>byte[]</code>
-     *         having random contents.
-     */
-    public ByteBuffer getRandomData(final ByteBuffer b, final int nbytes) {
+	/**
+	 * Returns random data that will fit in <i>nbytes</i>.
+	 * 
+	 * @return A new {@link ByteBuffer} wrapping a new <code>byte[]</code>
+	 *         having random contents.
+	 */
+	public ByteBuffer getRandomData(final ByteBuffer b, final int nbytes) {
 
-        final byte[] a = new byte[nbytes];
+		final byte[] a = new byte[nbytes];
 
-        r.nextBytes(a);
-        
-        b.limit(nbytes);
-        b.position(0);
-        b.put(a);
-        
-        b.flip();
-        
-        return b;
+		r.nextBytes(a);
 
-    }
-    
-    public TestHASendAndReceive3Nodes() {
+		b.limit(nbytes);
+		b.position(0);
+		b.put(a);
 
-    }
-    
-    public TestHASendAndReceive3Nodes(String name) {
-        
-        super(name);
+		b.flip();
 
-    }
-    
+		return b;
+
+	}
+
+	public TestHASendAndReceive3Nodes() {
+
+	}
+
+	public TestHASendAndReceive3Nodes(String name) {
+
+		super(name);
+
+	}
+
 	private HASendService sendService;
 	private HAReceiveService<HAWriteMessage> receiveService1;
 	private HAReceiveService<HAWriteMessage> receiveService2;
-    
+
 	protected void setUp() throws Exception {
 
-        final InetSocketAddress receiveAddr2 = new InetSocketAddress(getPort(0));
+		final InetSocketAddress receiveAddr2 = new InetSocketAddress(getPort(0));
 
-        receiveService2 = new HAReceiveService<HAWriteMessage>(receiveAddr2, null/* downstream */);
-        receiveService2.start();
+		receiveService2 = new HAReceiveService<HAWriteMessage>(receiveAddr2, null/* downstream */);
+		receiveService2.start();
 
-        final InetSocketAddress receiveAddr1 = new InetSocketAddress(getPort(0));
-        
-        receiveService1 = new HAReceiveService<HAWriteMessage>(receiveAddr1, receiveAddr2);
-        receiveService1.start();
+		final InetSocketAddress receiveAddr1 = new InetSocketAddress(getPort(0));
 
-        sendService = new HASendService(receiveAddr1);
+		receiveService1 = new HAReceiveService<HAWriteMessage>(receiveAddr1, receiveAddr2);
+		receiveService1.start();
 
-        if (log.isInfoEnabled()) {
-            log.info("receiveService1: addr=" + receiveAddr1);
-            log.info("receiveService2: addr=" + receiveAddr2);
-        }
+		sendService = new HASendService(receiveAddr1);
 
-    }
+		if (log.isInfoEnabled()) {
+			log.info("receiveService1: addr=" + receiveAddr1);
+			log.info("receiveService2: addr=" + receiveAddr2);
+		}
 
-    protected void tearDown() throws Exception {
-
-        if (receiveService1 != null)
-            receiveService1.terminate();
-
-        if (receiveService2 != null)
-            receiveService2.terminate();
-
-        if (sendService != null)
-            sendService.terminate();
-	    
 	}
 
-    /**
-     * Return an open port on current machine. Try the suggested port first. If
-     * suggestedPort is zero, just select a random port
-     */
-    private static int getPort(int suggestedPort) throws IOException {
-        
-        ServerSocket openSocket;
-        try {
-            openSocket = new ServerSocket(suggestedPort);
-        } catch (BindException ex) {
-            // the port is busy, so look for a random open port
-            openSocket = new ServerSocket(0);
-        }
+	protected void tearDown() throws Exception {
 
-        final int port = openSocket.getLocalPort();
-        
-        openSocket.close();
+		if (receiveService1 != null)
+			receiveService1.terminate();
 
-        if (suggestedPort != 0 && port != suggestedPort) {
+		if (receiveService2 != null)
+			receiveService2.terminate();
 
-            log.warn("suggestedPort is busy: suggestedPort=" + suggestedPort
-                    + ", using port=" + port + " instead");
-            
-        }
+		if (sendService != null)
+			sendService.terminate();
 
-        return port;
+	}
 
-    }
+	/**
+	 * Return an open port on current machine. Try the suggested port first. If
+	 * suggestedPort is zero, just select a random port
+	 */
+	private static int getPort(int suggestedPort) throws IOException {
 
-    public void testSimpleExchange() throws InterruptedException, ExecutionException
-        
-        {
-            final ByteBuffer tst1 = getRandomData(50);
-            final HAWriteMessage msg1 = new HAWriteMessage(50, chk.checksum(tst1));
-            final ByteBuffer rcv1 = ByteBuffer.allocate(2000);
-            final ByteBuffer rcv2 = ByteBuffer.allocate(2000);
-//            rcv.limit(50);
-            final Future<Void> futRec1 = receiveService1.receiveData(msg1, rcv1);
-            final Future<Void> futRec2 = receiveService2.receiveData(msg1, rcv2);
-            final Future<Void> futSnd = sendService.send(tst1);
-            while (!futSnd.isDone() && !futRec2.isDone()) {
-                try {
-                    futSnd.get(10L, TimeUnit.MILLISECONDS);
-                } catch (TimeoutException ignore) {
-                }
-                try {
-                    futRec2.get(10L, TimeUnit.MILLISECONDS);
-                } catch (TimeoutException ignore) {
-                }
-            }
-            futSnd.get();
-            futRec1.get();
-            futRec2.get();
-            assertEquals(tst1, rcv1);
-            assertEquals(rcv1, rcv2);
-        }
+		ServerSocket openSocket;
+		try {
+			openSocket = new ServerSocket(suggestedPort);
+		} catch (BindException ex) {
+			// the port is busy, so look for a random open port
+			openSocket = new ServerSocket(0);
+		}
 
-    public void testStressDirectBuffers() throws InterruptedException {
+		final int port = openSocket.getLocalPort();
 
-        ByteBuffer tst = null, rcv1 = null, rcv2 = null;
-        int i = -1, sze = -1;
-        try {
-            tst = DirectBufferPool.INSTANCE.acquire();
-            rcv1 = DirectBufferPool.INSTANCE.acquire();
-            rcv2 = DirectBufferPool.INSTANCE.acquire();
-            for (i = 0; i < 100; i++) {
+		openSocket.close();
 
-                log.info("Transferring message #" + i);
-                
-                sze = 1 + r.nextInt(tst.capacity());
-                getRandomData(tst, sze);
-                final HAWriteMessage msg = new HAWriteMessage(sze, chk.checksum(tst));
-                assertEquals(0,tst.position());
-                assertEquals(sze,tst.limit());
-                // FutureTask return ensures remote ready for Socket data
-                final Future<Void> futRec1 = receiveService1.receiveData(msg, rcv1);
-                final Future<Void> futRec2 = receiveService2.receiveData(msg, rcv2);
-                final Future<Void> futSnd = sendService.send(tst);
-                while (!futSnd.isDone() && !futRec1.isDone() && !futRec2.isDone()) {
-                    try {
-                        futSnd.get(10L, TimeUnit.MILLISECONDS);
-                    } catch (TimeoutException ignored) {
-                    }
-                    try {
-                        futRec1.get(10L, TimeUnit.MILLISECONDS);
-                    } catch (TimeoutException ignored) {
-                    }
-                    try {
-                        futRec2.get(10L, TimeUnit.MILLISECONDS);
-                    } catch (TimeoutException ignored) {
-                    }
-                }
-                futSnd.get();
-                futRec1.get();
-                futRec2.get();
-                assertEquals(tst, rcv1); // make sure buffer has been transmitted
-                assertEquals(rcv1, rcv2); // make sure buffer has been transmitted
-                log.info("Looks good for #" + i);
-            }
-        } catch (Throwable t) {
-            throw new RuntimeException("i=" + i + ", sze=" + sze + " : " + t, t);
-        } finally {
-            try {
-                if (tst != null) {
-                    DirectBufferPool.INSTANCE.release(tst);
-                }
-            } finally {
-            	try {
-	                if (rcv1 != null) {
-	                    DirectBufferPool.INSTANCE.release(rcv1);
-	                }
-            	} finally {
-	                if (rcv2 != null) {
-	                    DirectBufferPool.INSTANCE.release(rcv2);
-	                }
-            	}
-            }
-        }
-    }
+		if (suggestedPort != 0 && port != suggestedPort) {
+
+			log.warn("suggestedPort is busy: suggestedPort=" + suggestedPort + ", using port=" + port + " instead");
+
+		}
+
+		return port;
+
+	}
+
+	public void testSimpleExchange() throws InterruptedException, ExecutionException
+
+	{
+		final ByteBuffer tst1 = getRandomData(50);
+		final HAWriteMessage msg1 = new HAWriteMessage(50, chk.checksum(tst1));
+		final ByteBuffer rcv1 = ByteBuffer.allocate(2000);
+		final ByteBuffer rcv2 = ByteBuffer.allocate(2000);
+		// rcv.limit(50);
+		final Future<Void> futRec1 = receiveService1.receiveData(msg1, rcv1);
+		final Future<Void> futRec2 = receiveService2.receiveData(msg1, rcv2);
+		final Future<Void> futSnd = sendService.send(tst1);
+		while (!futSnd.isDone() && !futRec2.isDone()) {
+			try {
+				futSnd.get(10L, TimeUnit.MILLISECONDS);
+			} catch (TimeoutException ignore) {
+			}
+			try {
+				futRec2.get(10L, TimeUnit.MILLISECONDS);
+			} catch (TimeoutException ignore) {
+			}
+		}
+		futSnd.get();
+		futRec1.get();
+		futRec2.get();
+		assertEquals(tst1, rcv1);
+		assertEquals(rcv1, rcv2);
+	}
+
+	public void testChecksumError() throws InterruptedException, ExecutionException
+
+	{
+		final ByteBuffer tst1 = getRandomData(50);
+		final HAWriteMessage msg1 = new HAWriteMessage(50, chk.checksum(tst1) + 1);
+		final ByteBuffer rcv1 = ByteBuffer.allocate(2000);
+		final ByteBuffer rcv2 = ByteBuffer.allocate(2000);
+		// rcv.limit(50);
+		final Future<Void> futRec1 = receiveService1.receiveData(msg1, rcv1);
+		final Future<Void> futRec2 = receiveService2.receiveData(msg1, rcv2);
+		final Future<Void> futSnd = sendService.send(tst1);
+		while (!futSnd.isDone() && !futRec2.isDone()) {
+			try {
+				futSnd.get(10L, TimeUnit.MILLISECONDS);
+			} catch (TimeoutException ignore) {
+			} catch (ExecutionException e) {
+				assertTrue(e.getCause().getMessage().equals("Checksum Error"));
+			}
+			try {
+				futRec2.get(10L, TimeUnit.MILLISECONDS);
+			} catch (TimeoutException ignore) {
+			} catch (ExecutionException e) {
+				assertTrue(e.getCause().getMessage().equals("Checksum Error"));
+			}
+		}
+		futSnd.get();
+		try {
+			futRec1.get();
+			futRec2.get();
+		} catch (ExecutionException e) {
+			assertTrue(e.getCause().getMessage().equals("Checksum Error"));
+		}
+		assertEquals(tst1, rcv1);
+		assertEquals(rcv1, rcv2);
+	}
+
+	public void testStressDirectBuffers() throws InterruptedException {
+
+		ByteBuffer tst = null, rcv1 = null, rcv2 = null;
+		int i = -1, sze = -1;
+		try {
+			tst = DirectBufferPool.INSTANCE.acquire();
+			rcv1 = DirectBufferPool.INSTANCE.acquire();
+			rcv2 = DirectBufferPool.INSTANCE.acquire();
+			for (i = 0; i < 100; i++) {
+
+				log.info("Transferring message #" + i);
+
+				sze = 1 + r.nextInt(tst.capacity());
+				getRandomData(tst, sze);
+				final HAWriteMessage msg = new HAWriteMessage(sze, chk.checksum(tst));
+				assertEquals(0, tst.position());
+				assertEquals(sze, tst.limit());
+				// FutureTask return ensures remote ready for Socket data
+				final Future<Void> futRec1 = receiveService1.receiveData(msg, rcv1);
+				final Future<Void> futRec2 = receiveService2.receiveData(msg, rcv2);
+				final Future<Void> futSnd = sendService.send(tst);
+				while (!futSnd.isDone() && !futRec1.isDone() && !futRec2.isDone()) {
+					try {
+						futSnd.get(10L, TimeUnit.MILLISECONDS);
+					} catch (TimeoutException ignored) {
+					}
+					try {
+						futRec1.get(10L, TimeUnit.MILLISECONDS);
+					} catch (TimeoutException ignored) {
+					}
+					try {
+						futRec2.get(10L, TimeUnit.MILLISECONDS);
+					} catch (TimeoutException ignored) {
+					}
+				}
+				futSnd.get();
+				futRec1.get();
+				futRec2.get();
+				assertEquals(tst, rcv1); // make sure buffer has been
+											// transmitted
+				assertEquals(rcv1, rcv2); // make sure buffer has been
+											// transmitted
+				log.info("Looks good for #" + i);
+			}
+		} catch (Throwable t) {
+			throw new RuntimeException("i=" + i + ", sze=" + sze + " : " + t, t);
+		} finally {
+			try {
+				if (tst != null) {
+					DirectBufferPool.INSTANCE.release(tst);
+				}
+			} finally {
+				try {
+					if (rcv1 != null) {
+						DirectBufferPool.INSTANCE.release(rcv1);
+					}
+				} finally {
+					if (rcv2 != null) {
+						DirectBufferPool.INSTANCE.release(rcv2);
+					}
+				}
+			}
+		}
+	}
 }
