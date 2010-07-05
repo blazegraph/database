7e836b5dd91ff62bbe05986af1f6b3351215057e
martyncutcher <martyncutcher@users.sourceforge.net>
2010-07-05 10:53:22 +0000

    Modify to handle removal of trailing original ordered members
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/JOURNAL_HA_BRANCH@3142 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-jini/src/java/com/bigdata/quorum/zk/OrderedSetDifference.java
+++ bigdata-jini/src/java/com/bigdata/quorum/zk/OrderedSetDifference.java
@@ -46,32 +46,29 @@ class OrderedSetDifference<T> {
      */
     public OrderedSetDifference(final T[] aold, final T[] anew) {
 
-    	// boundary condition if new list is empty
-    	if (anew.length == 0) {
-    		for (int i = 0; i < aold.length; i++) {
-				removed.add(aold[i]);
-    		}   		
-    	} else {
-	        int oi = 0;
-	        
-	        for (int i = 0; i < anew.length; i++) {
-	        	boolean found = false;
-	    		while (!found && oi < aold.length) {
-	    			if (aold[oi].equals(anew[i])) {
-	    				found = true;
-	    			} else {
-	    				removed.add(aold[oi]);
-	    			}
-	    			
-	    			oi++;
-	    		}
-	    		
-	        	if (!found) {
-	        		added.add(anew[i]); // none left in old, so must be added
-	        	}
-	        }
-    	}
-    }
+        int oi = 0;
+        
+        for (int i = 0; i < anew.length; i++) {
+        	boolean found = false;
+    		while (!found && oi < aold.length) {
+    			if (aold[oi].equals(anew[i])) {
+    				found = true;
+    			} else {
+    				removed.add(aold[oi]);
+    			}
+    			
+    			oi++;
+    		}
+    		
+        	if (!found) {
+        		added.add(anew[i]); // none left in old, so must be added
+        	}
+        }
+        
+        while (oi < aold.length) {
+        	removed.add(aold[oi++]);
+        }
+     }
 
     public String toString() {
 
