3d22ecde869cbd5c8ec3a9b68604291306c358d7
mrpersonick <mrpersonick@users.sourceforge.net>
2010-07-19 16:57:46 +0000

    fixing import location
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/LEXICON_REFACTOR_BRANCH@3229 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-rdf/src/test/com/bigdata/rdf/relation/rule/TestAll.java
+++ bigdata-rdf/src/test/com/bigdata/rdf/relation/rule/TestAll.java
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 package com.bigdata.rdf.relation.rule;
 
+import com.bigdata.rdf.internal.constraints.TestInlineConstraints;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
