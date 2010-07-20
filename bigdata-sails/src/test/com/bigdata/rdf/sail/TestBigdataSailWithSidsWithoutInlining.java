546212ddfd0955a8c8c177e347f06c6a01a60e10
mrpersonick <mrpersonick@users.sourceforge.net>
2010-07-20 19:40:23 +0000

    test suite for no inlining
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/LEXICON_REFACTOR_BRANCH@3249 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata-sails/src/test/com/bigdata/rdf/sail/TestBigdataSailWithSidsWithoutInlining.java
@@ -0,0 +1,130 @@
+/**
+Copyright (C) SYSTAP, LLC 2006-2007.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/*
+ * Created on Sep 4, 2008
+ */
+
+package com.bigdata.rdf.sail;
+
+import java.util.Properties;
+
+import junit.extensions.proxy.ProxyTestSuite;
+import junit.framework.Test;
+
+import com.bigdata.rdf.sail.BigdataSail.Options;
+
+/**
+ * Test suite for the {@link BigdataSail} with statement identifiers enabled.
+ * 
+ * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan Thompson</a>
+ * @version $Id$
+ */
+public class TestBigdataSailWithSidsWithoutInlining extends AbstractBigdataSailTestCase {
+
+    /**
+     * 
+     */
+    public TestBigdataSailWithSidsWithoutInlining() {
+    }
+
+    public TestBigdataSailWithSidsWithoutInlining(String name) {
+        super(name);
+    }
+    
+    public static Test suite() {
+        
+        final TestBigdataSailWithSidsWithoutInlining delegate = new TestBigdataSailWithSidsWithoutInlining(); // !!!! THIS CLASS !!!!
+
+        /*
+         * Use a proxy test suite and specify the delegate.
+         */
+
+        final ProxyTestSuite suite = new ProxyTestSuite(delegate, "SAIL with SIDS (no inlining)");
+
+        // test pruning of variables not required for downstream processing.
+        suite.addTestSuite(TestPruneBindingSets.class);
+
+        // test of the search magic predicate
+        suite.addTestSuite(TestSearchQuery.class);
+        
+        // high-level query tests.
+        suite.addTestSuite(TestQuery.class);
+
+        // test of high-level query on a graph with statements about statements.
+        suite.addTestSuite(TestProvenanceQuery.class);
+
+        suite.addTestSuite(TestBigdataSailEvaluationStrategyImpl.class);
+        
+        suite.addTestSuite(TestReadWriteTransactions.class);
+        
+        return suite;
+        
+    }
+    
+    @Override
+    protected BigdataSail getSail(Properties properties) {
+        
+        return new BigdataSail(properties);
+        
+    }
+
+    public Properties getProperties() {
+
+        final Properties properties = new Properties(super.getProperties());
+/*
+        properties.setProperty(Options.STATEMENT_IDENTIFIERS, "true");
+        
+        properties.setProperty(Options.QUADS, "false");
+*/
+        properties.setProperty(Options.TRIPLES_MODE_WITH_PROVENANCE, "true");
+        
+        properties.setProperty(Options.INLINE_TERMS, "false");
+        
+        return properties;
+        
+    }
+    
+    @Override
+    protected BigdataSail reopenSail(BigdataSail sail) {
+
+        final Properties properties = sail.database.getProperties();
+
+        if (sail.isOpen()) {
+
+            try {
+
+                sail.shutDown();
+
+            } catch (Exception ex) {
+
+                throw new RuntimeException(ex);
+
+            }
+
+        }
+        
+        return getSail(properties);
+        
+    }
+
+}
