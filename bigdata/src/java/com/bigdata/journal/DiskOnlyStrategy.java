5d76c727a0c06cd22828da5b581a8f569c4b0a8f
martyncutcher <martyncutcher@users.sourceforge.net>
2010-07-29 12:11:02 +0000

    remove commit overide to flush the write cache.  This has introduced some IO concurrency errors.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@3339 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/journal/DiskOnlyStrategy.java
+++ bigdata/src/java/com/bigdata/journal/DiskOnlyStrategy.java
@@ -472,15 +472,6 @@ public class DiskOnlyStrategy extends AbstractBufferStrategy implements
     }
     
     /**
-     * Flushes the write cache (if enabled).
-     */
-    public void commit() {
-
-        flushWriteCache();
-
-    }
-
-    /**
      * Writes the {@link #writeCache} through to the disk and its position is
      * reset to zero.
      * <p>
