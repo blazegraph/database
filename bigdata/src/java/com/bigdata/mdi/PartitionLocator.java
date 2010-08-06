f9aacdffd689955e1c6c05acb293b474115c68c7
thompsonbry <thompsonbry@users.sourceforge.net>
2010-08-06 20:14:52 +0000

    javadoc edit
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/trunk@3429 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/mdi/PartitionLocator.java
+++ bigdata/src/java/com/bigdata/mdi/PartitionLocator.java
@@ -185,8 +185,7 @@ o     */
         
     }
 
-    // Note: used by assertEquals in the test cases.
-    public boolean equals(Object o) {
+    public boolean equals(final Object o) {
 
         if (this == o)
             return true;
