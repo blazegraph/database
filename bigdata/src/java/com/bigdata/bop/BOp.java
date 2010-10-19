74e243890ae5518ae656d760b792f84e8f148716
thompsonbry <thompsonbry@users.sourceforge.net>
2010-10-19 17:29:08 +0000

    Added a Constant constructor variant which accept the name of the variable bound to that constant.  BOpContext knows how to use this to insert the constant into the binding set in bind(...) [that will be in a separate commit].
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/QUADS_QUERY_BRANCH@3823 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/bop/BOp.java
+++ bigdata/src/java/com/bigdata/bop/BOp.java
@@ -108,6 +108,18 @@ public interface BOp extends Cloneable, Serializable, IPropertySet {
     <T> T getProperty(final String name, final T defaultValue);
 
     /**
+     * Unconditionally sets the property.
+     * 
+     * @param name
+     *            The name.
+     * @param value
+     *            The value.
+     *            
+     * @return A copy of this {@link BOp} on which the property has been set.
+     */
+    BOp setProperty(final String name, final Object value);
+    
+    /**
      * Return the value of the named annotation.
      * 
      * @param name
