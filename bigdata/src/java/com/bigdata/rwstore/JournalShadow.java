f6357aec60be10c43714060d0dbea313afea2ef7
thompsonbry <thompsonbry@users.sourceforge.net>
2010-11-04 13:13:27 +0000

    Clean up on JournalShadow prior to integration into AbstractTask.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/JOURNAL_HA_BRANCH@3889 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/rwstore/JournalShadow.java
+++ bigdata/src/java/com/bigdata/rwstore/JournalShadow.java
@@ -25,83 +25,102 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 package com.bigdata.rwstore;
 
 import java.nio.ByteBuffer;
-import java.util.concurrent.atomic.AtomicLong;
 
 import com.bigdata.journal.AbstractJournal;
+import com.bigdata.journal.IBufferStrategy;
 import com.bigdata.journal.IJournal;
 import com.bigdata.journal.JournalDelegate;
 import com.bigdata.journal.RWStrategy;
 
 /**
- * A JournalShadow wraps a Journal as a JournalDelegate but provides itself
- * as the allocation context to be passed through to any interested
- * BufferStrategy.
- * 
- * This is the path by which RWStore allocators are provided the context for
- * the allocations and deletes made
+ * A {@link JournalShadow} wraps an Journal but provides itself as the
+ * allocation context to be passed through to any interested
+ * {@link IBufferStrategy}. This is the path by which {@link RWStore} allocators
+ * are provided with the context for the allocations and deletes made.
  * 
  * @author Martyn Cutcher
- *
  */
 public class JournalShadow extends JournalDelegate implements IAllocationContext {
-	static AtomicLong s_idCounter = new AtomicLong(23);
-	int m_id = (int) s_idCounter.incrementAndGet();
 	
-	private JournalShadow(AbstractJournal source) {
-		super(source);
+//    private final static AtomicLong s_idCounter = new AtomicLong(23);
+//	
+//    final private int m_id = (int) s_idCounter.incrementAndGet();
+	
+	private JournalShadow(final AbstractJournal source) {
+		
+	    super(source);
+	    
 	}
 
-	public long write(ByteBuffer data) {
-		return delegate.write(data, this);
+	public long write(final ByteBuffer data) {
+		
+	    return delegate.write(data, this);
+	    
 	}
 
-	public long write(ByteBuffer data, long oldAddr) {
-		return delegate.write(data, oldAddr, this);
+	public long write(final ByteBuffer data, final long oldAddr) {
+		
+	    return delegate.write(data, oldAddr, this);
+	    
 	}
 
 	public void delete(long oldAddr) {
-		delegate.delete(oldAddr, this);
+
+	    delegate.delete(oldAddr, this);
+	    
 	}
 
-	public int compareTo(Object o) {
-		if (o instanceof JournalShadow) {
-			JournalShadow js = (JournalShadow) o;
-			return m_id - js.m_id;
-		} else {
-			return -1;
-		}
-	}
+//	public int compareTo(Object o) {
+//		if (o instanceof JournalShadow) {
+//			JournalShadow js = (JournalShadow) o;
+//			return m_id - js.m_id;
+//		} else {
+//			return -1;
+//		}
+//	}
 
-	/**
-	 * TODO: should retrieve from localTransactionService or Journal
-	 * properties
-	 */
-	public long minimumReleaseTime() {
-		return 0;
-	}
+//	/**
+//	 * TODO: should retrieve from localTransactionService or Journal
+//	 * properties
+//	 */
+//	public long minimumReleaseTime() {
+//		return 0;
+//	}
 	
 	/**
 	 * Release itself from the wrapped Journal, this unlocks the allocator for
 	 * the RWStore
 	 */
 	public void detach() {
-		delegate.detachContext(this);
+
+	    delegate.detachContext(this);
+	    
 	}
 
-	/**
-	 * This factory pattern creates a shadow for a RWStrategy-backed Journal
-	 * to support protected allocations while allowing for deletion and 
-	 * re-allocation where possible.  If the Journal is not backed by a
-	 * RWStrategy, then the original Journal is returned.
-	 * 
-	 * @param journal - the journal to be shadowed
-	 * @return the shadowed journal if necessary
-	 */
-	public static IJournal newShadow(AbstractJournal journal) {
-		if (journal.getBufferStrategy() instanceof RWStrategy) {
-			return new JournalShadow(journal);
-		} else {
-			return journal;
-		}
-	}
+    /**
+     * This factory pattern creates a shadow for a RWStrategy-backed Journal to
+     * support protected allocations while allowing for deletion and
+     * re-allocation where possible. If the Journal is not backed by a
+     * RWStrategy, then the original Journal is returned.
+     * 
+     * @param journal
+     *            The journal to be shadowed
+     * 
+     * @return The shadowed journal if necessary and otherwise the
+     *         <i>journal</i>.
+     */
+    public static IJournal newShadow(AbstractJournal journal) {
+
+        if (journal.getBufferStrategy() instanceof RWStrategy) {
+        
+            return new JournalShadow(journal);
+            
+        } else {
+            
+            return journal;
+            
+        }
+
+    }
+
 }
