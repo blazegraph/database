0ca9c282d73da6edf6e6630b6b1402d33bf9c5ce
thompsonbry <thompsonbry@users.sourceforge.net>
2011-03-23 12:26:50 +0000

    Bug fix for the ICUVersionRecord deserialization.  Extension of the unit tests to correctly detect this problem.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/QUADS_QUERY_BRANCH@4326 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/test/com/bigdata/btree/keys/TestICUVersionRecord.java
+++ bigdata/src/test/com/bigdata/btree/keys/TestICUVersionRecord.java
@@ -28,6 +28,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 package com.bigdata.btree.keys;
 
 import com.bigdata.io.SerializerUtil;
+import com.ibm.icu.util.VersionInfo;
 
 import junit.framework.TestCase2;
 
@@ -59,6 +60,33 @@ public class TestICUVersionRecord extends TestCase2 {
         final ICUVersionRecord r2 = ICUVersionRecord.newInstance();
         
         assertTrue(r1.equals(r2));
+        
+        final ICUVersionRecord r3 = (ICUVersionRecord) SerializerUtil
+                .deserialize(SerializerUtil.serialize(r1));
+
+        assertTrue(r1.equals(r3));
+
+    }
+    
+    public void test_roundTrip2() {
+
+        final ICUVersionRecord r1 = new ICUVersionRecord(
+                VersionInfo.getInstance(3, 6, 2, 1),//
+                VersionInfo.getInstance(1, 8, 5, 7),//
+                VersionInfo.getInstance(6, 3, 1, 8),//
+                VersionInfo.getInstance(4, 6, 8, 12)//
+                );
+
+        final ICUVersionRecord r2 = new ICUVersionRecord( 
+            VersionInfo.getInstance(3, 6, 2, 1),//
+            VersionInfo.getInstance(1, 8, 5, 7),//
+            VersionInfo.getInstance(6, 3, 1, 8),//
+            VersionInfo.getInstance(4, 6, 8, 12)//
+            );
+        
+        assertTrue(r1.equals(r2));
+        
+        assertFalse(r1.equals(ICUVersionRecord.newInstance()));
         
         final ICUVersionRecord r3 = (ICUVersionRecord) SerializerUtil
                 .deserialize(SerializerUtil.serialize(r1));
