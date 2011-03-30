0806f8e173a8dd45936c075289e6b188fa0a9773
thompsonbry <thompsonbry@users.sourceforge.net>
2011-03-30 19:51:01 +0000

    some import cleanup.
    hooked the REST API test suite into the top-level TestAll, but this does not hook it in yet for CI builds which depend on the correct behavior of the top-level "junit"
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/QUADS_QUERY_BRANCH@4355 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-sails/src/java/com/bigdata/rdf/sail/webapp/StatusServlet.java
+++ bigdata-sails/src/java/com/bigdata/rdf/sail/webapp/StatusServlet.java
@@ -6,9 +6,6 @@ import java.util.List;
 import java.util.TreeMap;
 import java.util.UUID;
 
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
