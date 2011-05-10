6dd93ee0c4444d53bff34ec5252f9b6f1737276b
thompsonbry <thompsonbry@users.sourceforge.net>
2011-05-10 13:21:13 +0000

    Removed @Override of a method.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/QUADS_QUERY_BRANCH@4473 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/htree/MutableDirectoryPageData.java
+++ bigdata/src/java/com/bigdata/htree/MutableDirectoryPageData.java
@@ -228,7 +228,6 @@ public class MutableDirectoryPageData implements IDirectoryData {
 	 * 
 	 * TODO Alternatively, could report the #of non-null child addresses.
 	 */
-	@Override
 	final public int getChildCount() {
 
 		return childAddr.length;
