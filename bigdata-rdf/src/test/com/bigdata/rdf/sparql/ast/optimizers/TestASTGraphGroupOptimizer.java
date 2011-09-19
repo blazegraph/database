66821f1f004c755a5b39235daac84011e1661942
thompsonbry <thompsonbry@users.sourceforge.net>
2011-09-19 18:14:51 +0000

    Added unit test for case when we do add a SameTerm() constraint for a nested GRAPH pattern having a different variable for GRAPH ?foo.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/TERMS_REFACTOR_BRANCH@5215 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/optimizers/TestASTGraphGroupOptimizer.java
+++ bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/optimizers/TestASTGraphGroupOptimizer.java
@@ -38,9 +38,12 @@ import com.bigdata.rdf.model.BigdataValueFactory;
 import com.bigdata.rdf.sail.TestNamedGraphs;
 import com.bigdata.rdf.sail.sparql.Bigdata2ASTSPARQLParser;
 import com.bigdata.rdf.sparql.ast.AST2BOpContext;
+import com.bigdata.rdf.sparql.ast.AST2BOpUtility;
 import com.bigdata.rdf.sparql.ast.ASTContainer;
 import com.bigdata.rdf.sparql.ast.AbstractASTEvaluationTestCase;
 import com.bigdata.rdf.sparql.ast.ConstantNode;
+import com.bigdata.rdf.sparql.ast.FilterNode;
+import com.bigdata.rdf.sparql.ast.FunctionNode;
 import com.bigdata.rdf.sparql.ast.JoinGroupNode;
 import com.bigdata.rdf.sparql.ast.QueryRoot;
 import com.bigdata.rdf.sparql.ast.StatementPatternNode;
@@ -78,10 +81,6 @@ public class TestASTGraphGroupOptimizer extends
      *   }
      * }
      * </pre>
-     * 
-     * FIXME Variant where the graph variables are distinct and verify that
-     * SameTerm was added to the inner group (in this case, an inner optional
-     * group).
      */
     @SuppressWarnings({ "unchecked", "rawtypes" })
     public void test_graphGroupOptimizer_01() throws MalformedQueryException {
@@ -160,7 +159,7 @@ public class TestASTGraphGroupOptimizer extends
 
         }
 
-        assertSameAST(expectedClause, queryRoot.getWhereClause());        
+        assertSameAST(expectedClause, queryRoot.getWhereClause());
 
     }
 
@@ -177,13 +176,96 @@ public class TestASTGraphGroupOptimizer extends
      *   }
      * }
      * </pre>
-    * FIXME Variant where the graph variables are distinct and verify that
-    * SameTerm was added to the inner group (in this case, an inner optional
-    * group).
     */
    @SuppressWarnings({ "unchecked", "rawtypes" })
    public void test_graphGroupOptimizer_02() throws MalformedQueryException {
-       fail("write test");
-   }
+       
+       final String queryStr = ""//
+               + "PREFIX : <http://example.org/>\n"//
+               + "SELECT ?s\n"//
+               + "WHERE {\n"//
+               + "  GRAPH ?g {\n"//
+               + "    ?s :p :o .\n"//
+               + "    GRAPH ?g1 { ?g :p2 ?s }\n"//
+               + "  }\n"//
+               + "}";
+
+       /*
+        * Add the Values used in the query to the lexicon. This makes it
+        * possible for us to explicitly construct the expected AST and
+        * the verify it using equals().
+        */
+       final BigdataValueFactory f = store.getValueFactory();
+       final BigdataURI p = f.createURI("http://example.org/p");
+       final BigdataURI p2= f.createURI("http://example.org/p2");
+       final BigdataURI o = f.createURI("http://example.org/o");
+       final BigdataValue[] values = new BigdataValue[] { p, p2, o };
+       store.getLexiconRelation()
+               .addTerms(values, values.length, false/* readOnly */);
+
+       final ASTContainer astContainer = new Bigdata2ASTSPARQLParser(store)
+               .parseQuery2(queryStr, baseURI);
+
+       final AST2BOpContext context = new AST2BOpContext(astContainer, store);
+
+       QueryRoot queryRoot = astContainer.getOriginalAST();
+       
+       queryRoot = (QueryRoot) new ASTSetValueExpressionsOptimizer().optimize(
+               context, queryRoot, null/* bindingSets */);
+
+       queryRoot = BOpUtility.deepCopy(queryRoot);
+       
+       queryRoot = (QueryRoot) new ASTGraphGroupOptimizer().optimize(
+               context, queryRoot, null/* bindingSets */);
+
+       /*
+        * Create the expected AST.
+        */
+       final JoinGroupNode expectedClause = new JoinGroupNode();
+       {
+
+           final VarNode g = new VarNode("g");
+           final VarNode g1 = new VarNode("g1");
+           final VarNode s = new VarNode("s");
+           
+           final JoinGroupNode graphGroup = new JoinGroupNode();
+           expectedClause.addChild(graphGroup);
+           graphGroup.setContext(g);
+
+           // ?s :p :o
+           graphGroup.addChild(new StatementPatternNode(//
+                   s,// s
+                   new ConstantNode(new Constant(p.getIV())),// p
+                   new ConstantNode(new Constant(o.getIV())),// o
+                   g,// c
+                   Scope.NAMED_CONTEXTS//
+                   ));
+       
+           final JoinGroupNode innerGraphGroup = new JoinGroupNode();
+           graphGroup.addChild(innerGraphGroup);
+           innerGraphGroup.setContext(g1);
+           
+           // ?g :p2 ?s
+           innerGraphGroup.addChild(new StatementPatternNode(//
+                   g,// s
+                   new ConstantNode(new Constant(p2.getIV())),// p
+                   s,// o
+                   g1,// c
+                   Scope.NAMED_CONTEXTS//
+                   ));
+           
+            final FilterNode filterNode = new FilterNode(FunctionNode.sameTerm(
+                    g, g1));
+
+            AST2BOpUtility.toVE(context.getLexiconNamespace(),
+                    filterNode.getValueExpressionNode());
+            
+            innerGraphGroup.addChild(filterNode);
+
+        }
+
+        assertSameAST(expectedClause, queryRoot.getWhereClause());
+
+    }
 
 }
