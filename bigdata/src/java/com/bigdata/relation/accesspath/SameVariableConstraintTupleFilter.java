e179869ef3ad25526219f0d3e152ec4608be42e3
thompsonbry <thompsonbry@users.sourceforge.net>
2011-09-22 12:45:33 +0000

    The SameVariableConstraint was being attached to the AccessPath by an inline TupleFilter subclass.  This was causing the SPOAccessPath reference to be dragged in, which resulted in the not serializable exception.
    A top-level SameVariableConstraintTupleFilter class was created to replace the inline class.  The AccessPath constructor was modified to use this top-level class and thus avoid dragging in the AccessPath instance when the filter is serialized.
    
    This issue was observed in the development branch (TERMS_REFACTOR_BRANCH).  However, it seems likely that the same exception could appear in the 1.0.x release.
    
    The changes were applied to both the 1.0.x maintenance branch and the development branch.
    
    @see https://sourceforge.net/apps/trac/bigdata/ticket/379
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/TERMS_REFACTOR_BRANCH@5234 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata/src/java/com/bigdata/relation/accesspath/SameVariableConstraintTupleFilter.java
@@ -0,0 +1,64 @@
+/**
+
+Copyright (C) SYSTAP, LLC 2006-2011.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/*
+ * Created on Sep 22, 2011
+ */
+
+package com.bigdata.relation.accesspath;
+
+import java.io.Serializable;
+
+import com.bigdata.bop.ap.filter.SameVariableConstraint;
+import com.bigdata.btree.ITuple;
+import com.bigdata.btree.filter.TupleFilter;
+
+/**
+ * {@link TupleFilter} class wrapping the {@link SameVariableConstraint}.
+ * <p>
+ * Note: This filter can execute local to the index shard in scale-out.
+ * Therefore it MUST NOT have a reference to the {@link AccessPath} in order to
+ * be {@link Serializable}. This used to be an "inline" class in
+ * {@link AccessPath}. It was promoted to a top-level class for this reason.
+ */
+public class SameVariableConstraintTupleFilter<E> extends TupleFilter<E> {
+
+    private static final long serialVersionUID = 1L;
+
+    private final SameVariableConstraint<E> sameVariableConstraint;
+
+    SameVariableConstraintTupleFilter(
+            final SameVariableConstraint<E> sameVariableConstraint) {
+
+        this.sameVariableConstraint = sameVariableConstraint;
+
+    }
+
+    @Override
+    protected boolean isValid(final ITuple tuple) {
+
+        return sameVariableConstraint.isValid(tuple.getObject());
+
+    }
+
+}
