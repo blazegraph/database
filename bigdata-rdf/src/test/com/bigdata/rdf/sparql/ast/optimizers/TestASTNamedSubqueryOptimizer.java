cae4df7fdcc286c4e51cd4bae89a01b5f75ffc7c
thompsonbry <thompsonbry@users.sourceforge.net>
2011-10-24 15:55:50 +0000

    Unit test for assignment of join variables for named subqueries.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/TERMS_REFACTOR_BRANCH@5408 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/optimizers/TestASTNamedSubqueryOptimizer.java
+++ bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/optimizers/TestASTNamedSubqueryOptimizer.java
@@ -27,20 +27,34 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package com.bigdata.rdf.sparql.ast.optimizers;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 
+import org.openrdf.model.impl.URIImpl;
+import org.openrdf.model.vocabulary.RDF;
 import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.algebra.StatementPattern.Scope;
 
+import com.bigdata.bop.IBindingSet;
 import com.bigdata.bop.IVariable;
 import com.bigdata.bop.Var;
+import com.bigdata.rdf.internal.IV;
 import com.bigdata.rdf.sail.sparql.Bigdata2ASTSPARQLParser;
 import com.bigdata.rdf.sparql.ast.ASTContainer;
 import com.bigdata.rdf.sparql.ast.AbstractASTEvaluationTestCase;
+import com.bigdata.rdf.sparql.ast.ConstantNode;
 import com.bigdata.rdf.sparql.ast.IBindingProducerNode;
+import com.bigdata.rdf.sparql.ast.IQueryNode;
+import com.bigdata.rdf.sparql.ast.JoinGroupNode;
+import com.bigdata.rdf.sparql.ast.NamedSubqueriesNode;
 import com.bigdata.rdf.sparql.ast.NamedSubqueryInclude;
 import com.bigdata.rdf.sparql.ast.NamedSubqueryRoot;
+import com.bigdata.rdf.sparql.ast.ProjectionNode;
 import com.bigdata.rdf.sparql.ast.QueryRoot;
+import com.bigdata.rdf.sparql.ast.QueryType;
+import com.bigdata.rdf.sparql.ast.StatementPatternNode;
 import com.bigdata.rdf.sparql.ast.StaticAnalysis;
+import com.bigdata.rdf.sparql.ast.VarNode;
 import com.bigdata.rdf.sparql.ast.eval.AST2BOpContext;
 
 /**
@@ -142,6 +156,407 @@ public class TestASTNamedSubqueryOptimizer extends
         assertEquals(joinVars, nsr.getJoinVars());
 
         assertEquals(joinVars, nsi.getJoinVars());
+
+    }
+
+    /**
+     * This unit test is a based on
+     * <code>bigdata-perf/CI/govtrack/queries/query10.rq</code>
+     * <p>
+     * Rewrite:
+     * 
+     * <pre>
+     * SELECT ?_var1
+     *  WHERE {
+     *         ?_var1 a <http://www.rdfabout.com/rdf/schema/politico/Politician>
+     *         OPTIONAL {
+     *                 ?_var1 <http://www.rdfabout.com/rdf/schema/usgovt/name> ?_var6
+     *         }.
+     *         OPTIONAL {
+     *                 ?_var12 <http://www.rdfabout.com/rdf/schema/usbill/sponsor> ?_var1.
+     *                 ?_var12 <http://www.rdfabout.com/rdf/schema/usbill/title> ?_var4
+     *         }.
+     *         OPTIONAL {
+     *                 ?_var1 <http://www.w3.org/2001/vcard-rdf/3.0#N> ?_var13.
+     *                 ?_var13 <http://www.w3.org/2001/vcard-rdf/3.0#Family> ?_var10
+     *         }
+     * }
+     * </pre>
+     * 
+     * as:
+     * 
+     * <pre>
+     * SELECT  ?_var1
+     * WITH {
+     *         SELECT ?_var1 ?_var6
+     *         WHERE {
+     *                 ?_var1 a <http://www.rdfabout.com/rdf/schema/politico/Politician>
+     *                 OPTIONAL {
+     *                      ?_var1 <http://www.rdfabout.com/rdf/schema/usgovt/name> ?_var6
+     *                 }.
+     *         }
+     * } as %_set1
+     * WITH {
+     *         SELECT ?_var1 ?_var4 ?_var12
+     *         WHERE {
+     *            INCLUDE %_set1
+     *            OPTIONAL {
+     *                 ?_var12 <http://www.rdfabout.com/rdf/schema/usbill/sponsor> ?_var1.
+     *                 ?_var12 <http://www.rdfabout.com/rdf/schema/usbill/title> ?_var4
+     *            }.
+     *         }
+     * } as %_set2
+     * WITH {
+     *      SELECT ?_var1 ?_var10 ?_var13
+     *      WHERE {
+     *         INCLUDE %_set1
+     *         OPTIONAL {
+     *                 ?_var1 <http://www.w3.org/2001/vcard-rdf/3.0#N> ?_var13.
+     *                 ?_var13 <http://www.w3.org/2001/vcard-rdf/3.0#Family> ?_var10
+     *         }
+     *     }
+     * } as %_set3
+     *  WHERE {
+     *         INCLUDE %_set2 .
+     *         INCLUDE %_set3 JOIN ON (?_var1) .
+     * }
+     * </pre>
+     * 
+     * @see https://sourceforge.net/apps/trac/bigdata/ticket/397
+     * 
+     * TODO We need a unit test where there are some filters which should
+     * move and some which should not.
+     */
+    public void test_rewriteComplexOptional() {
+        /*
+         * Note: DO NOT share structures in this test!!!!
+         */
+        final IBindingSet[] bsets = new IBindingSet[]{};
+
+        @SuppressWarnings("rawtypes")
+        final IV a = makeIV(RDF.TYPE);
+        @SuppressWarnings("rawtypes")
+        final IV polititian = makeIV(new URIImpl("http://www.rdfabout.com/rdf/schema/politico/Politician"));
+        @SuppressWarnings("rawtypes")
+        final IV name = makeIV(new URIImpl("http://www.rdfabout.com/rdf/schema/usgovt/name"));
+        @SuppressWarnings("rawtypes")
+        final IV sponsor = makeIV(new URIImpl("http://www.rdfabout.com/rdf/schema/usgovt/sponsor"));
+        @SuppressWarnings("rawtypes")
+        final IV title = makeIV(new URIImpl("http://www.rdfabout.com/rdf/schema/usgovt/title"));
+        @SuppressWarnings("rawtypes")
+        final IV N = makeIV(new URIImpl("http://www.w3.org/2001/vcard-rdf/3.0#N"));
+        @SuppressWarnings("rawtypes")
+        final IV family = makeIV(new URIImpl("http://www.w3.org/2001/vcard-rdf/3.0#Family"));
+
+        // The source AST.
+        final QueryRoot given = new QueryRoot(QueryType.SELECT);
+        {
+
+            {
+                final ProjectionNode projection = new ProjectionNode();
+                given.setProjection(projection);
+                projection.addProjectionVar(new VarNode("var1"));
+            }
+
+            final String set1 = "--nsr-1";
+            final String set2 = "--nsr-2";
+            final String set3 = "--nsr-3";
+
+            final NamedSubqueriesNode namedSubqueries = new NamedSubqueriesNode();
+            given.setNamedSubqueries(namedSubqueries);
+
+            // Required joins and simple optionals lifted into a named subquery.
+            {
+                final NamedSubqueryRoot nsr = new NamedSubqueryRoot(
+                        QueryType.SELECT, set1);
+                namedSubqueries.add(nsr);
+
+                final ProjectionNode projection = new ProjectionNode();
+                nsr.setProjection(projection);
+                projection.addProjectionVar(new VarNode("var1"));
+                projection.addProjectionVar(new VarNode("var6"));
+                
+                final JoinGroupNode whereClause = new JoinGroupNode();
+                nsr.setWhereClause(whereClause);
+
+                // ?_var1 a <http://www.rdfabout.com/rdf/schema/politico/Politician>
+                whereClause.addChild(new StatementPatternNode(new VarNode("var1"),
+                        new ConstantNode(a), new ConstantNode(polititian),
+                        null/* c */, Scope.DEFAULT_CONTEXTS));
+
+                // ?_var1 <http://www.rdfabout.com/rdf/schema/usgovt/name> ?_var6
+                {
+                    final StatementPatternNode sp = new StatementPatternNode(
+                            new VarNode("var1"), new ConstantNode(name),
+                            new VarNode("var6"), null/* c */,
+                            Scope.DEFAULT_CONTEXTS);
+                    sp.setSimpleOptional(true);
+                    whereClause.addChild(sp);
+                }
+                
+            }
+
+            // First complex optional group.
+            {
+                final NamedSubqueryRoot nsr = new NamedSubqueryRoot(
+                        QueryType.SELECT, set2);
+                namedSubqueries.add(nsr);
+
+                final JoinGroupNode whereClause = new JoinGroupNode();
+                nsr.setWhereClause(whereClause);
+
+                final ProjectionNode projection = new ProjectionNode();
+                nsr.setProjection(projection);
+                projection.addProjectionVar(new VarNode("var1"));
+                projection.addProjectionVar(new VarNode("var6"));
+                projection.addProjectionVar(new VarNode("var12"));
+                projection.addProjectionVar(new VarNode("var4"));
+                
+                whereClause.addChild(new NamedSubqueryInclude(set1));
+
+                {
+                    
+                    final JoinGroupNode optionalGroup1 = new JoinGroupNode(true/* optional */);
+                    whereClause.addChild(optionalGroup1);
+
+                    // ?_var12 <http://www.rdfabout.com/rdf/schema/usbill/sponsor> ?_var1.
+                    optionalGroup1.addChild(new StatementPatternNode(new VarNode(
+                            "var12"), new ConstantNode(sponsor), new VarNode(
+                            "var1"), null/* c */, Scope.DEFAULT_CONTEXTS));
+                    
+                    // ?_var12 <http://www.rdfabout.com/rdf/schema/usbill/title> ?_var4
+                    optionalGroup1.addChild(new StatementPatternNode(new VarNode(
+                            "var12"), new ConstantNode(title), new VarNode(
+                            "var4"), null/* c */, Scope.DEFAULT_CONTEXTS));
+                    
+                }
+
+            }
+
+            // Second complex optional group.
+            {
+                final NamedSubqueryRoot nsr = new NamedSubqueryRoot(
+                        QueryType.SELECT, set3);
+                namedSubqueries.add(nsr);
+
+                final JoinGroupNode whereClause = new JoinGroupNode();
+                nsr.setWhereClause(whereClause);
+
+                final ProjectionNode projection = new ProjectionNode();
+                nsr.setProjection(projection);
+                projection.addProjectionVar(new VarNode("var1"));
+                projection.addProjectionVar(new VarNode("var6"));
+                projection.addProjectionVar(new VarNode("var13"));
+                projection.addProjectionVar(new VarNode("var10"));
+                
+                whereClause.addChild(new NamedSubqueryInclude(set1));
+
+                {
+
+                    final JoinGroupNode optionalGroup2 = new JoinGroupNode(true/* optional */);
+                    whereClause.addChild(optionalGroup2);
+
+                    // ?_var1 <http://www.w3.org/2001/vcard-rdf/3.0#N> ?_var13.
+                    optionalGroup2.addChild(new StatementPatternNode(
+                            new VarNode("var1"), new ConstantNode(N),
+                            new VarNode("var13"), null/* c */,
+                            Scope.DEFAULT_CONTEXTS));
+
+                    // ?_var13 <http://www.w3.org/2001/vcard-rdf/3.0#Family>
+                    // ?_var10
+                    optionalGroup2.addChild(new StatementPatternNode(
+                            new VarNode("var13"), new ConstantNode(family),
+                            new VarNode("var10"), null/* c */,
+                            Scope.DEFAULT_CONTEXTS));
+
+                }
+            }
+            
+            // WHERE
+            {
+                final JoinGroupNode whereClause = new JoinGroupNode();
+                given.setWhereClause(whereClause);
+                whereClause.addChild(new NamedSubqueryInclude(set2));
+                whereClause.addChild(new NamedSubqueryInclude(set3));
+            }
+
+        }
+
+        // The expected AST after the rewrite.
+        final QueryRoot expected = new QueryRoot(QueryType.SELECT);
+        final JoinGroupNode complexOpt1;
+        final JoinGroupNode complexOpt2;
+        {
+
+            {
+                final ProjectionNode projection = new ProjectionNode();
+                expected.setProjection(projection);
+                projection.addProjectionVar(new VarNode("var1"));
+            }
+
+            final String set1 = "--nsr-1";
+            final String set2 = "--nsr-2";
+            final String set3 = "--nsr-3";
+
+            final NamedSubqueriesNode namedSubqueries = new NamedSubqueriesNode();
+            expected.setNamedSubqueries(namedSubqueries);
+
+            // Required joins and simple optionals lifted into a named subquery.
+            {
+                final NamedSubqueryRoot nsr = new NamedSubqueryRoot(
+                        QueryType.SELECT, set1);
+                namedSubqueries.add(nsr);
+                nsr.setJoinVars(new VarNode[]{/*none*/});
+                nsr.setProperty(NamedSubqueryRoot.Annotations.DEPENDS_ON,new String[]{});
+
+                final ProjectionNode projection = new ProjectionNode();
+                nsr.setProjection(projection);
+                projection.addProjectionVar(new VarNode("var1"));
+                projection.addProjectionVar(new VarNode("var6"));
+                
+                final JoinGroupNode whereClause = new JoinGroupNode();
+                nsr.setWhereClause(whereClause);
+
+                // ?_var1 a <http://www.rdfabout.com/rdf/schema/politico/Politician>
+                whereClause.addChild(new StatementPatternNode(new VarNode("var1"),
+                        new ConstantNode(a), new ConstantNode(polititian),
+                        null/* c */, Scope.DEFAULT_CONTEXTS));
+
+                // ?_var1 <http://www.rdfabout.com/rdf/schema/usgovt/name> ?_var6
+                {
+                    final StatementPatternNode sp = new StatementPatternNode(
+                            new VarNode("var1"), new ConstantNode(name),
+                            new VarNode("var6"), null/* c */,
+                            Scope.DEFAULT_CONTEXTS);
+                    sp.setSimpleOptional(true);
+                    whereClause.addChild(sp);
+                }
+                
+            }
+
+            // First complex optional group.
+            {
+                final NamedSubqueryRoot nsr = new NamedSubqueryRoot(
+                        QueryType.SELECT, set2);
+                namedSubqueries.add(nsr);
+                nsr.setJoinVars(new VarNode[]{/*none*/});
+                nsr.setProperty(NamedSubqueryRoot.Annotations.DEPENDS_ON,new String[]{set1});
+
+                final JoinGroupNode whereClause = new JoinGroupNode();
+                nsr.setWhereClause(whereClause);
+
+                final ProjectionNode projection = new ProjectionNode();
+                nsr.setProjection(projection);
+                projection.addProjectionVar(new VarNode("var1"));
+                projection.addProjectionVar(new VarNode("var6"));
+                projection.addProjectionVar(new VarNode("var12"));
+                projection.addProjectionVar(new VarNode("var4"));
+                
+                final NamedSubqueryInclude nsi1 = new NamedSubqueryInclude(set1);
+                nsi1.setJoinVars(new VarNode[]{/*none*/});
+                whereClause.addChild(nsi1);
+
+                {
+                    
+                    final JoinGroupNode optionalGroup1 = new JoinGroupNode(true/* optional */);
+                    whereClause.addChild(optionalGroup1);
+
+                    // ?_var12 <http://www.rdfabout.com/rdf/schema/usbill/sponsor> ?_var1.
+                    optionalGroup1.addChild(new StatementPatternNode(new VarNode(
+                            "var12"), new ConstantNode(sponsor), new VarNode(
+                            "var1"), null/* c */, Scope.DEFAULT_CONTEXTS));
+                    
+                    // ?_var12 <http://www.rdfabout.com/rdf/schema/usbill/title> ?_var4
+                    optionalGroup1.addChild(new StatementPatternNode(new VarNode(
+                            "var12"), new ConstantNode(title), new VarNode(
+                            "var4"), null/* c */, Scope.DEFAULT_CONTEXTS));
+                    
+                    complexOpt1 = optionalGroup1;
+                }
+
+            }
+
+            // Second complex optional group.
+            {
+                final NamedSubqueryRoot nsr = new NamedSubqueryRoot(
+                        QueryType.SELECT, set3);
+                namedSubqueries.add(nsr);
+                nsr.setJoinVars(new VarNode[]{new VarNode("var1")});
+                nsr.setProperty(NamedSubqueryRoot.Annotations.DEPENDS_ON,new String[]{set1});
+
+                final JoinGroupNode whereClause = new JoinGroupNode();
+                nsr.setWhereClause(whereClause);
+
+                final ProjectionNode projection = new ProjectionNode();
+                nsr.setProjection(projection);
+                projection.addProjectionVar(new VarNode("var1"));
+                projection.addProjectionVar(new VarNode("var6"));
+                projection.addProjectionVar(new VarNode("var13"));
+                projection.addProjectionVar(new VarNode("var10"));
+                
+                final NamedSubqueryInclude nsi1 = new NamedSubqueryInclude(set1);
+                nsi1.setJoinVars(new VarNode[]{/*none*/});
+                whereClause.addChild(nsi1);
+
+                {
+
+                    final JoinGroupNode optionalGroup2 = new JoinGroupNode(true/* optional */);
+                    whereClause.addChild(optionalGroup2);
+
+                    // ?_var1 <http://www.w3.org/2001/vcard-rdf/3.0#N> ?_var13.
+                    optionalGroup2.addChild(new StatementPatternNode(
+                            new VarNode("var1"), new ConstantNode(N),
+                            new VarNode("var13"), null/* c */,
+                            Scope.DEFAULT_CONTEXTS));
+
+                    // ?_var13 <http://www.w3.org/2001/vcard-rdf/3.0#Family>
+                    // ?_var10
+                    optionalGroup2.addChild(new StatementPatternNode(
+                            new VarNode("var13"), new ConstantNode(family),
+                            new VarNode("var10"), null/* c */,
+                            Scope.DEFAULT_CONTEXTS));
+
+                    complexOpt2 = optionalGroup2;
+                }
+            }
+            
+            // WHERE
+            {
+                final JoinGroupNode whereClause = new JoinGroupNode();
+                expected.setWhereClause(whereClause);
+                
+                final NamedSubqueryInclude nsi2 = new NamedSubqueryInclude(set2);
+                nsi2.setJoinVars(new VarNode[]{/*none*/});
+                whereClause.addChild(nsi2);
+                
+                final NamedSubqueryInclude nsi3 = new NamedSubqueryInclude(set3);
+                nsi3.setJoinVars(new VarNode[]{new VarNode("var1")});
+                whereClause.addChild(nsi3);
+            }
+
+        }
+
+        final IASTOptimizer rewriter = new ASTNamedSubqueryOptimizer();
+        
+        final AST2BOpContext context = new AST2BOpContext(new ASTContainer(
+                given), store);
+
+        final IQueryNode actual = rewriter.optimize(context,
+                given/* queryNode */, bsets);
+
+        assertSameAST(expected, actual);
+
+        final StaticAnalysis sa = new StaticAnalysis(expected);
+
+        // Test "incoming" bindings.
+        assertEquals(asSet(new Var[] { Var.var("var1") }),
+                sa.getDefinitelyIncomingBindings(complexOpt1,
+                        new LinkedHashSet<IVariable<?>>()));
+
+        // Test "incoming" bindings.
+        assertEquals(asSet(new Var[] { Var.var("var1") }),
+                sa.getDefinitelyIncomingBindings(complexOpt2,
+                        new LinkedHashSet<IVariable<?>>()));
 
     }
 
