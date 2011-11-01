d68881ea39a0b629ade564c1aae99b1df68ea1ef
thompsonbry <thompsonbry@users.sourceforge.net>
2011-11-01 12:36:26 +0000

    Finished port of unit tests from TestBigdataSailEvaluationStrategyImpl.
    Note: TestOptionals#test_nested_optionals() is failing.  I am going to ask MikeP to take a look at this.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/TERMS_REFACTOR_BRANCH@5470 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/eval/TestComboJoins.java
@@ -0,0 +1,103 @@
+/**
+
+Copyright (C) SYSTAP, LLC 2006-2011.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/*
+ * Created on Sep 4, 2011
+ */
+
+package com.bigdata.rdf.sparql.ast.eval;
+
+/**
+ * Data driven test suite.
+ * 
+ * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan Thompson</a>
+ * @version $Id$
+ */
+public class TestComboJoins extends AbstractDataDrivenSPARQLTestCase {
+
+    /**
+     * 
+     */
+    public TestComboJoins() {
+    }
+
+    /**
+     * @param name
+     */
+    public TestComboJoins(String name) {
+        super(name);
+    }
+
+    /**
+     * <pre>
+     * SELECT ?a ?y ?d ?z
+     * {
+     *     ?a :p ?c OPTIONAL { ?a :r ?d }.  
+     *     ?a ?p 1 { ?p a ?y } UNION { ?a ?z ?p } 
+     * }
+     * </pre>
+     * 
+     * Note: This is a port of
+     * TestBigdataSailEvaluationStrategyImpl#test_join_combo_1()
+     */
+    public void test_join_combo_1() throws Exception {
+        
+        new TestHelper("join-combo-1").runTest();
+        
+    }
+
+    /**
+     * <pre>
+     * SELECT ?x ?y ?z
+     * {
+     *    GRAPH ?g { ?x ?p 1 } { ?x :p ?y } UNION { ?p a ?z }
+     * }
+     * </pre>
+     * 
+     * Note: This is a port of
+     * TestBigdataSailEvaluationStrategyImpl#test_join_combo_1()
+     */
+    public void test_join_combo_2() throws Exception {
+        
+        new TestHelper("join-combo-2").runTest();
+        
+    }
+
+    /**
+     * <pre>
+     * SELECT *
+     * {
+     *     { ?x :p ?y } UNION { ?p :a ?z }
+     * }
+     * </pre>
+     * 
+     * Note: This is a port of
+     * TestBigdataSailEvaluationStrategyImpl#test_join_combo_1()
+     */
+    public void test_join_combo_3() throws Exception {
+        
+        new TestHelper("join-combo-3").runTest();
+        
+    }
+
+}
