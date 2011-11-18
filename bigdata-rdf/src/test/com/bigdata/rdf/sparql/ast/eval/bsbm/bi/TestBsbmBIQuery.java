eee64a8406730cfa1e35cda86c6d07021616774f
thompsonbry <thompsonbry@users.sourceforge.net>
2011-11-18 19:53:11 +0000

    comments on the various BSBM BI queries.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/TERMS_REFACTOR_BRANCH@5694 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/eval/bsbm/bi/TestBsbmBIQuery.java
+++ bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/eval/bsbm/bi/TestBsbmBIQuery.java
@@ -28,7 +28,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 package com.bigdata.rdf.sparql.ast.eval.bsbm.bi;
 
 import com.bigdata.rdf.sparql.ast.eval.AbstractDataDrivenSPARQLTestCase;
-import com.bigdata.rdf.sparql.ast.optimizers.ASTFlattenUnionsOptimizer;
 
 /**
  * Data driven test suite for complex queries.
@@ -115,114 +114,10 @@ public class TestBsbmBIQuery extends AbstractDataDrivenSPARQLTestCase {
 
     }
 
-    /**
-     * <pre>
-     * prefix bsbm: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/>
-     * prefix bsbm-inst: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/>
-     * prefix rev: <http://purl.org/stuff/rev#>
-     * prefix xsd: <http://www.w3.org/2001/XMLSchema#>
-     * 
-     * Select ?reviewer (avg(xsd:float(?score)) As ?reviewerAvgScore)
-     * {
-     *   { Select (avg(xsd:float(?score)) As ?avgScore)
-     *     {
-     *       ?product bsbm:producer <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/dataFromProducer1/Producer1> .
-     *       ?review bsbm:reviewFor ?product .
-     *       { ?review bsbm:rating1 ?score . } UNION
-     *       { ?review bsbm:rating2 ?score . } UNION
-     *       { ?review bsbm:rating3 ?score . } UNION
-     *       { ?review bsbm:rating4 ?score . }
-     *     }
-     *   }
-     *   ?product bsbm:producer <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/dataFromProducer1/Producer1> .
-     *   ?review bsbm:reviewFor ?product .
-     *   ?review rev:reviewer ?reviewer .
-     *   { ?review bsbm:rating1 ?score . } UNION
-     *   { ?review bsbm:rating2 ?score . } UNION
-     *   { ?review bsbm:rating3 ?score . } UNION
-     *   { ?review bsbm:rating4 ?score . }
-     * }
-     * Group By ?reviewer
-     * Having (avg(xsd:float(?score)) > min(?avgScore) * 1.5)
-     * </pre>
-     * 
-     * <pre>
-     * PREFIX bsbm: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/>
-     * PREFIX bsbm-inst: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/>
-     * PREFIX rev: <http://purl.org/stuff/rev#>
-     * PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-     * QueryType: SELECT
-     * SELECT ( VarNode(reviewer) AS VarNode(reviewer) ) ( com.bigdata.rdf.sparql.ast.FunctionNode(FunctionNode(com.bigdata.rdf.internal.constraints.FuncBOp(score)[ com.bigdata.rdf.internal.constraints.FuncBOp.namespace=kb.lex, com.bigdata.rdf.internal.constraints.FuncBOp.function=http://www.w3.org/2001/XMLSchema#float]))[ com.bigdata.rdf.sparql.ast.FunctionNode.functionURI=http://www.w3.org/2006/sparql-functions#average, valueExpr=com.bigdata.bop.rdf.aggregate.AVERAGE(com.bigdata.rdf.internal.constraints.FuncBOp(score)[ com.bigdata.rdf.internal.constraints.FuncBOp.namespace=kb.lex, com.bigdata.rdf.internal.constraints.FuncBOp.function=http://www.w3.org/2001/XMLSchema#float])] AS VarNode(reviewerAvgScore) )
-     *   JoinGroupNode {
-     *     JoinGroupNode {
-     *       QueryType: SELECT
-     *       SELECT ( com.bigdata.rdf.sparql.ast.FunctionNode(FunctionNode(com.bigdata.rdf.internal.constraints.FuncBOp(score)[ com.bigdata.rdf.internal.constraints.FuncBOp.namespace=kb.lex, com.bigdata.rdf.internal.constraints.FuncBOp.function=http://www.w3.org/2001/XMLSchema#float]))[ com.bigdata.rdf.sparql.ast.FunctionNode.functionURI=http://www.w3.org/2006/sparql-functions#average, valueExpr=com.bigdata.bop.rdf.aggregate.AVERAGE(com.bigdata.rdf.internal.constraints.FuncBOp(score)[ com.bigdata.rdf.internal.constraints.FuncBOp.namespace=kb.lex, com.bigdata.rdf.internal.constraints.FuncBOp.function=http://www.w3.org/2001/XMLSchema#float])] AS VarNode(avgScore) )
-     *         JoinGroupNode {
-     *           StatementPatternNode(VarNode(product), ConstantNode(TermId(695U)[http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/producer]), ConstantNode(TermId(675U)[http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/dataFromProducer1/Producer1]), DEFAULT_CONTEXTS)
-     *           StatementPatternNode(VarNode(review), ConstantNode(TermId(1500U)[http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/reviewFor]), VarNode(product), DEFAULT_CONTEXTS)
-     *           UnionNode {
-     *             JoinGroupNode {
-     *               StatementPatternNode(VarNode(review), ConstantNode(TermId(1523U)[http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/rating1]), VarNode(score), DEFAULT_CONTEXTS)
-     *             }
-     *             JoinGroupNode {
-     *               StatementPatternNode(VarNode(review), ConstantNode(TermId(1497U)[http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/rating2]), VarNode(score), DEFAULT_CONTEXTS)
-     *             }
-     *             JoinGroupNode {
-     *               StatementPatternNode(VarNode(review), ConstantNode(TermId(1498U)[http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/rating3]), VarNode(score), DEFAULT_CONTEXTS)
-     *             }
-     *             JoinGroupNode {
-     *               StatementPatternNode(VarNode(review), ConstantNode(TermId(1524U)[http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/rating4]), VarNode(score), DEFAULT_CONTEXTS)
-     *             }
-     *           }
-     *         }
-     *     }
-     *     StatementPatternNode(VarNode(product), ConstantNode(TermId(695U)[http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/producer]), ConstantNode(TermId(675U)[http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/dataFromProducer1/Producer1]), DEFAULT_CONTEXTS)
-     *     StatementPatternNode(VarNode(review), ConstantNode(TermId(1500U)[http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/reviewFor]), VarNode(product), DEFAULT_CONTEXTS)
-     *     StatementPatternNode(VarNode(review), ConstantNode(TermId(1475U)[http://purl.org/stuff/rev#reviewer]), VarNode(reviewer), DEFAULT_CONTEXTS)
-     *     UnionNode {
-     *       JoinGroupNode {
-     *         StatementPatternNode(VarNode(review), ConstantNode(TermId(1523U)[http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/rating1]), VarNode(score), DEFAULT_CONTEXTS)
-     *       }
-     *       JoinGroupNode {
-     *         StatementPatternNode(VarNode(review), ConstantNode(TermId(1497U)[http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/rating2]), VarNode(score), DEFAULT_CONTEXTS)
-     *       }
-     *       JoinGroupNode {
-     *         StatementPatternNode(VarNode(review), ConstantNode(TermId(1498U)[http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/rating3]), VarNode(score), DEFAULT_CONTEXTS)
-     *       }
-     *       JoinGroupNode {
-     *         StatementPatternNode(VarNode(review), ConstantNode(TermId(1524U)[http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/rating4]), VarNode(score), DEFAULT_CONTEXTS)
-     *       }
-     *     }
-     *   }
-     * group by ( VarNode(reviewer) AS VarNode(reviewer) )
-     * having com.bigdata.rdf.sparql.ast.FunctionNode(FunctionNode(com.bigdata.bop.rdf.aggregate.AVERAGE(com.bigdata.rdf.internal.constraints.FuncBOp(score)[ com.bigdata.rdf.internal.constraints.FuncBOp.namespace=kb.lex, com.bigdata.rdf.internal.constraints.FuncBOp.function=http://www.w3.org/2001/XMLSchema#float])),FunctionNode(MULTIPLY(com.bigdata.bop.rdf.aggregate.MIN(avgScore), XSDDecimal(1.5))))[ com.bigdata.rdf.sparql.ast.FunctionNode.functionURI=http://www.w3.org/2005/xpath-functions#greater-than, valueExpr=com.bigdata.rdf.internal.constraints.CompareBOp(com.bigdata.bop.rdf.aggregate.AVERAGE(com.bigdata.rdf.internal.constraints.FuncBOp(score)[ com.bigdata.rdf.internal.constraints.FuncBOp.namespace=kb.lex, com.bigdata.rdf.internal.constraints.FuncBOp.function=http://www.w3.org/2001/XMLSchema#float]),MULTIPLY(com.bigdata.bop.rdf.aggregate.MIN(avgScore), XSDDecimal(1.5)))[ com.bigdata.rdf.internal.constraints.CompareBOp.op=GT]]
-     * </pre>
-     * 
-     * Note: The first problem with this query was that we were not flattening
-     * the UNIONs. I've introduced {@link ASTFlattenUnionsOptimizer} which takes
-     * care of that. However, performance is still terrible unless the query is
-     * rewritten such that the subquery runs first.  So, the next step is to
-     * understand why we need to do that for this case.
-     */
     public void test_bsbm_bi_query6() throws Exception {
 
         new TestHelper("query6", // name
                 "bsbm/bi/query6.rq",// query
-                dataset,//
-                "bsbm/bi/empty.srx"// result
-        ).runTest();
-
-    }
-
-    /**
-     * A variant on Q6 where the sub-select is rewritten as a named subquery
-     * such that it runs first. This runs MUCH faster, but it is still a slow
-     * query (around 3s of query time on PC 10, which is a very small data set).
-     */
-    public void test_bsbm_bi_query6b() throws Exception {
-
-        new TestHelper("query6b", // name
-                "bsbm/bi/query6b.rq",// query
                 dataset,//
                 "bsbm/bi/empty.srx"// result
         ).runTest();
