1e3ed6f6086b10417cf500ae43d5f6ca866169e9
thompsonbry <thompsonbry@users.sourceforge.net>
2011-11-22 14:56:40 +0000

    Added unit test for runFirst/runLast based on query hints attached to a SERVICE node and to a JoinGroupNode.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/TERMS_REFACTOR_BRANCH@5726 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/optimizers/TestASTRunFirstRunLastOptimizer.java
+++ bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/optimizers/TestASTRunFirstRunLastOptimizer.java
@@ -27,6 +27,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package com.bigdata.rdf.sparql.ast.optimizers;
 
+import org.openrdf.model.URI;
 import org.openrdf.model.impl.LiteralImpl;
 import org.openrdf.model.impl.URIImpl;
 
@@ -44,14 +45,13 @@ import com.bigdata.rdf.sparql.ast.ProjectionNode;
 import com.bigdata.rdf.sparql.ast.QueryHints;
 import com.bigdata.rdf.sparql.ast.QueryRoot;
 import com.bigdata.rdf.sparql.ast.QueryType;
+import com.bigdata.rdf.sparql.ast.ServiceNode;
 import com.bigdata.rdf.sparql.ast.StatementPatternNode;
-import com.bigdata.rdf.sparql.ast.TermNode;
 import com.bigdata.rdf.sparql.ast.ValueExpressionNode;
 import com.bigdata.rdf.sparql.ast.VarNode;
-import com.bigdata.rdf.sparql.ast.eval.AST2BOpBase.Annotations;
 
 /**
- * Test suite for {@link ASTStaticJoinOptimizer}.
+ * Test suite for {@link ASTRunFirstRunLastOptimizer}.
  */
 public class TestASTRunFirstRunLastOptimizer extends AbstractASTEvaluationTestCase {
 
@@ -68,7 +68,28 @@ public class TestASTRunFirstRunLastOptimizer extends AbstractASTEvaluationTestCa
         super(name);
     }
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
+    /**
+     * Given:
+     * 
+     * <pre>
+     * SELECT * 
+     *   JoinGroupNode {
+     *     FILTER( com.bigdata.rdf.sparql.ast.FunctionNode(VarNode(x),ConstantNode(TermId(0U)[0]))[ com.bigdata.rdf.sparql.ast.FunctionNode.functionURI=http://www.w3.org/2005/xpath-functions#greater-than] )
+     *     StatementPatternNode(VarNode(a), ConstantNode(TermId(0U)[http://example/a]), ConstantNode(TermId(0U)[http://example/a]), DEFAULT_CONTEXTS)
+     *     StatementPatternNode(VarNode(b), ConstantNode(TermId(0U)[http://example/b]), ConstantNode(TermId(0U)[http://example/b]), DEFAULT_CONTEXTS)
+     *     StatementPatternNode(VarNode(c), ConstantNode(TermId(0U)[http://example/c]), ConstantNode(TermId(0U)[http://example/c]), DEFAULT_CONTEXTS)
+     *       queryHints={com.bigdata.rdf.sparql.ast.QueryHints.runLast=true}
+     *     StatementPatternNode(VarNode(d), ConstantNode(TermId(0U)[http://example/d]), ConstantNode(TermId(0U)[http://example/d]), DEFAULT_CONTEXTS)
+     *     StatementPatternNode(VarNode(e), ConstantNode(TermId(0U)[http://example/e]), ConstantNode(TermId(0U)[http://example/e]), DEFAULT_CONTEXTS)
+     *       queryHints={com.bigdata.rdf.sparql.ast.QueryHints.runFirst=true}
+     *     StatementPatternNode(VarNode(f), ConstantNode(TermId(0U)[http://example/f]), ConstantNode(TermId(0U)[http://example/f]), DEFAULT_CONTEXTS)
+     *     StatementPatternNode(VarNode(g), ConstantNode(TermId(0U)[http://example/g]), ConstantNode(TermId(0U)[http://example/g]), DEFAULT_CONTEXTS)
+     *     FILTER( com.bigdata.rdf.sparql.ast.FunctionNode(VarNode(y),ConstantNode(TermId(0U)[0]))[ com.bigdata.rdf.sparql.ast.FunctionNode.functionURI=http://www.w3.org/2005/xpath-functions#greater-than] )
+     *     FILTER( com.bigdata.rdf.sparql.ast.FunctionNode(VarNode(z),ConstantNode(TermId(0U)[0]))[ com.bigdata.rdf.sparql.ast.FunctionNode.functionURI=http://www.w3.org/2005/xpath-functions#greater-than] )
+     *   }
+     * </pre>
+     */
+    @SuppressWarnings("rawtypes")
     public void test_runFirst() {
 
         /*
@@ -76,25 +97,18 @@ public class TestASTRunFirstRunLastOptimizer extends AbstractASTEvaluationTestCa
          */
         final IBindingSet[] bsets = new IBindingSet[]{};
 
-        @SuppressWarnings("rawtypes")
         final IV a = makeIV(new URIImpl("http://example/a"));
         
-        @SuppressWarnings("rawtypes")
         final IV b = makeIV(new URIImpl("http://example/b"));
         
-        @SuppressWarnings("rawtypes")
         final IV c = makeIV(new URIImpl("http://example/c"));
 
-        @SuppressWarnings("rawtypes")
         final IV d = makeIV(new URIImpl("http://example/d"));
 
-        @SuppressWarnings("rawtypes")
         final IV e = makeIV(new URIImpl("http://example/e"));
 
-        @SuppressWarnings("rawtypes")
         final IV f = makeIV(new URIImpl("http://example/f"));
 
-        @SuppressWarnings("rawtypes")
         final IV g = makeIV(new URIImpl("http://example/g"));
 
         // The source AST.
@@ -239,31 +253,267 @@ public class TestASTRunFirstRunLastOptimizer extends AbstractASTEvaluationTestCa
         assertSameAST(expected, actual);
 
     }
-    
-    private StatementPatternNode newStatementPatternNode(
-            final TermNode s, final TermNode p, final TermNode o, 
-            final long cardinality) {
+
+    /**
+     * Unit test with non-SP joins.
+     * Given:
+     * 
+     * <pre>
+     * SELECT * 
+     *   JoinGroupNode {
+     *     FILTER( com.bigdata.rdf.sparql.ast.FunctionNode(VarNode(x),ConstantNode(TermId(0U)[0]))[ com.bigdata.rdf.sparql.ast.FunctionNode.functionURI=http://www.w3.org/2005/xpath-functions#greater-than] )
+     *     StatementPatternNode(VarNode(a), ConstantNode(TermId(0U)[http://example/a]), ConstantNode(TermId(0U)[http://example/a]), DEFAULT_CONTEXTS)
+     *     StatementPatternNode(VarNode(b), ConstantNode(TermId(0U)[http://example/b]), ConstantNode(TermId(0U)[http://example/b]), DEFAULT_CONTEXTS)
+     *     {
+     *       StatementPatternNode(VarNode(c), ConstantNode(TermId(0U)[http://example/c]), ConstantNode(TermId(0U)[http://example/c]), DEFAULT_CONTEXTS)
+     *       StatementPatternNode(VarNode(d), ConstantNode(TermId(0U)[http://example/d]), ConstantNode(TermId(0U)[http://example/d]), DEFAULT_CONTEXTS)
+     *     } queryHints={com.bigdata.rdf.sparql.ast.QueryHints.runLast=true}
+     *     SERVICE {
+     *       StatementPatternNode(VarNode(f), ConstantNode(TermId(0U)[http://example/f]), ConstantNode(TermId(0U)[http://example/f]), DEFAULT_CONTEXTS)
+     *       StatementPatternNode(VarNode(e), ConstantNode(TermId(0U)[http://example/e]), ConstantNode(TermId(0U)[http://example/e]), DEFAULT_CONTEXTS)
+     *     } queryHints={com.bigdata.rdf.sparql.ast.QueryHints.runFirst=true}
+     *     StatementPatternNode(VarNode(g), ConstantNode(TermId(0U)[http://example/g]), ConstantNode(TermId(0U)[http://example/g]), DEFAULT_CONTEXTS)
+     *     FILTER( com.bigdata.rdf.sparql.ast.FunctionNode(VarNode(y),ConstantNode(TermId(0U)[0]))[ com.bigdata.rdf.sparql.ast.FunctionNode.functionURI=http://www.w3.org/2005/xpath-functions#greater-than] )
+     *     FILTER( com.bigdata.rdf.sparql.ast.FunctionNode(VarNode(z),ConstantNode(TermId(0U)[0]))[ com.bigdata.rdf.sparql.ast.FunctionNode.functionURI=http://www.w3.org/2005/xpath-functions#greater-than] )
+     *   }
+     * </pre>
+     */
+    @SuppressWarnings("rawtypes")
+    public void test_runFirst2() {
+
+        /*
+         * Note: DO NOT share structures in this test!!!!
+         */
+        final IBindingSet[] bsets = new IBindingSet[]{};
+
+        final IV serviceURI = makeIV(new URIImpl("http://example/service"));
+
+        final IV a = makeIV(new URIImpl("http://example/a"));
         
-        return newStatementPatternNode(s, p, o, cardinality, false);
+        final IV b = makeIV(new URIImpl("http://example/b"));
         
-    }
-        
-    private StatementPatternNode newStatementPatternNode(
-            final TermNode s, final TermNode p, final TermNode o, 
-            final long cardinality, final boolean optional) {
-        
-        final StatementPatternNode sp = new StatementPatternNode(s, p, o);
-        
-        sp.setProperty(Annotations.ESTIMATED_CARDINALITY, cardinality);
-        
-        if (optional) {
+        final IV c = makeIV(new URIImpl("http://example/c"));
+
+        final IV d = makeIV(new URIImpl("http://example/d"));
+
+        final IV e = makeIV(new URIImpl("http://example/e"));
+
+        final IV f = makeIV(new URIImpl("http://example/f"));
+
+        final IV g = makeIV(new URIImpl("http://example/g"));
+
+        // The source AST.
+        final QueryRoot given = new QueryRoot(QueryType.SELECT);
+        {
+
+            final ProjectionNode projection = new ProjectionNode();
+            projection.addProjectionVar(new VarNode("*"));
             
-            sp.setOptional(true);
+            final JoinGroupNode where = new JoinGroupNode();
+
+            // pre-filter
+            where.addChild(new FilterNode(
+                    new FunctionNode(FunctionRegistry.GT, null,
+                            new ValueExpressionNode[] {
+                                new VarNode("x"),
+                                new ConstantNode(makeIV(new LiteralImpl("0", XSD.INTEGER)))
+                    })));
+
+            where.addChild(new StatementPatternNode(new VarNode("a"),
+                    new ConstantNode(a), new ConstantNode(a)));
+            
+            where.addChild(new StatementPatternNode(new VarNode("b"),
+                    new ConstantNode(b), new ConstantNode(b)));
+
+            {
+                final JoinGroupNode group1 = new JoinGroupNode();
+                where.addChild(group1);
+
+                group1.setQueryHint(QueryHints.RUN_LAST, "true");
+
+                group1.addChild(new StatementPatternNode(new VarNode("c"),
+                        new ConstantNode(c), new ConstantNode(c)));
+
+                group1.addChild(new StatementPatternNode(new VarNode("d"),
+                        new ConstantNode(d), new ConstantNode(d)));
+            }
+
+            {
+                
+                final JoinGroupNode serviceGroup = new JoinGroupNode();
+
+                final ServiceNode service = new ServiceNode(
+                        (URI) serviceURI.getValue(), serviceGroup);
+                where.addChild(service);
+
+                serviceGroup.addChild(new StatementPatternNode(
+                        new VarNode("e"), new ConstantNode(e),
+                        new ConstantNode(e)));
+
+                serviceGroup.addChild(new StatementPatternNode(
+                        new VarNode("f"), new ConstantNode(f),
+                        new ConstantNode(f)));
+
+                service.setQueryHint(QueryHints.RUN_FIRST, "true");
+
+            }
+            
+            where.addChild(new StatementPatternNode(new VarNode("g"),
+                    new ConstantNode(g), new ConstantNode(g)));
+
+            // post-filter
+            where.addChild(new FilterNode(
+                    new FunctionNode(FunctionRegistry.GT, null,
+                            new ValueExpressionNode[] {
+                                new VarNode("y"),
+                                new ConstantNode(makeIV(new LiteralImpl("0", XSD.INTEGER)))
+                    })));
+            
+            // post-filter
+            where.addChild(new FilterNode(
+                    new FunctionNode(FunctionRegistry.GT, null,
+                            new ValueExpressionNode[] {
+                                new VarNode("z"),
+                                new ConstantNode(makeIV(new LiteralImpl("0", XSD.INTEGER)))
+                    })));
+            
+            given.setProjection(projection);
+            given.setWhereClause(where);
+            System.err.println(given);
+        }
+
+        // The expected AST after the rewrite.
+        final QueryRoot expected = new QueryRoot(QueryType.SELECT);
+        {
+            
+            final ProjectionNode projection = new ProjectionNode();
+            projection.addProjectionVar(new VarNode("*"));
+            
+            final JoinGroupNode where = new JoinGroupNode();
+
+            // pre-filter
+            where.addChild(new FilterNode(
+                    new FunctionNode(FunctionRegistry.GT, null,
+                            new ValueExpressionNode[] {
+                                new VarNode("x"),
+                                new ConstantNode(makeIV(new LiteralImpl("0", XSD.INTEGER)))
+                    })));
+            
+  //*     FILTER( com.bigdata.rdf.sparql.ast.FunctionNode(VarNode(x),ConstantNode(TermId(0U)[0]))[ com.bigdata.rdf.sparql.ast.FunctionNode.functionURI=http://www.w3.org/2005/xpath-functions#greater-than] )
+  //*     StatementPatternNode(VarNode(a), ConstantNode(TermId(0U)[http://example/a]), ConstantNode(TermId(0U)[http://example/a]), DEFAULT_CONTEXTS)
+  //*     StatementPatternNode(VarNode(b), ConstantNode(TermId(0U)[http://example/b]), ConstantNode(TermId(0U)[http://example/b]), DEFAULT_CONTEXTS)
+  //*     {
+  //*       StatementPatternNode(VarNode(c), ConstantNode(TermId(0U)[http://example/c]), ConstantNode(TermId(0U)[http://example/c]), DEFAULT_CONTEXTS)
+  //*       StatementPatternNode(VarNode(d), ConstantNode(TermId(0U)[http://example/d]), ConstantNode(TermId(0U)[http://example/d]), DEFAULT_CONTEXTS)
+  //*     } queryHints={com.bigdata.rdf.sparql.ast.QueryHints.runLast=true}
+  //*     SERVICE {
+  //*       StatementPatternNode(VarNode(f), ConstantNode(TermId(0U)[http://example/f]), ConstantNode(TermId(0U)[http://example/f]), DEFAULT_CONTEXTS)
+  //*       StatementPatternNode(VarNode(e), ConstantNode(TermId(0U)[http://example/e]), ConstantNode(TermId(0U)[http://example/e]), DEFAULT_CONTEXTS)
+  //*     } queryHints={com.bigdata.rdf.sparql.ast.QueryHints.runFirst=true}
+  //*     StatementPatternNode(VarNode(g), ConstantNode(TermId(0U)[http://example/g]), ConstantNode(TermId(0U)[http://example/g]), DEFAULT_CONTEXTS)
+  //*     FILTER( com.bigdata.rdf.sparql.ast.FunctionNode(VarNode(y),ConstantNode(TermId(0U)[0]))[ com.bigdata.rdf.sparql.ast.FunctionNode.functionURI=http://www.w3.org/2005/xpath-functions#greater-than] )
+  //*     FILTER( com.bigdata.rdf.sparql.ast.FunctionNode(VarNode(z),ConstantNode(TermId(0U)[0]))[ com.bigdata.rdf.sparql.ast.FunctionNode.functionURI=http://www.w3.org/2005/xpath-functions#greater-than] )
+
+            {
+             
+                final JoinGroupNode serviceGroup = new JoinGroupNode();
+
+                final ServiceNode service = new ServiceNode(
+                        (URI) serviceURI.getValue(), serviceGroup);
+
+                where.addChild(service);
+
+                serviceGroup.addChild(new StatementPatternNode(
+                        new VarNode("e"), new ConstantNode(e),
+                        new ConstantNode(e)));
+
+                serviceGroup.addChild(new StatementPatternNode(
+                        new VarNode("f"), new ConstantNode(f),
+                        new ConstantNode(f)));
+
+                service.setQueryHint(QueryHints.RUN_FIRST, "true");
+
+            }
+
+            where.addChild(new StatementPatternNode(new VarNode("a"),
+                    new ConstantNode(a), new ConstantNode(a)));
+
+            where.addChild(new StatementPatternNode(new VarNode("b"),
+                    new ConstantNode(b), new ConstantNode(b)));
+
+            where.addChild(new StatementPatternNode(new VarNode("g"),
+                    new ConstantNode(g), new ConstantNode(g)));
+
+            {
+
+                final JoinGroupNode group1 = new JoinGroupNode();
+                where.addChild(group1);
+
+                group1.addChild(new StatementPatternNode(new VarNode("c"),
+                        new ConstantNode(c), new ConstantNode(c)));
+
+                group1.addChild(new StatementPatternNode(new VarNode("d"),
+                        new ConstantNode(d), new ConstantNode(d)));
+
+                group1.setQueryHint(QueryHints.RUN_LAST, "true");
+
+            }
+
+            // post-filter
+            where.addChild(new FilterNode(
+                    new FunctionNode(FunctionRegistry.GT, null,
+                            new ValueExpressionNode[] {
+                                new VarNode("y"),
+                                new ConstantNode(makeIV(new LiteralImpl("0", XSD.INTEGER)))
+                    })));
+            
+            // post-filter
+            where.addChild(new FilterNode(
+                    new FunctionNode(FunctionRegistry.GT, null,
+                            new ValueExpressionNode[] {
+                                new VarNode("z"),
+                                new ConstantNode(makeIV(new LiteralImpl("0", XSD.INTEGER)))
+                    })));
+
+            expected.setProjection(projection);
+            expected.setWhereClause(where);
             
         }
+
+        final IASTOptimizer rewriter = new ASTRunFirstRunLastOptimizer();
         
-        return sp;
-        
+        final IQueryNode actual = rewriter.optimize(null/* AST2BOpContext */,
+                given/* queryNode */, bsets);
+
+        assertSameAST(expected, actual);
+
     }
+    
+
+//    private StatementPatternNode newStatementPatternNode(
+//            final TermNode s, final TermNode p, final TermNode o, 
+//            final long cardinality) {
+//        
+//        return newStatementPatternNode(s, p, o, cardinality, false);
+//        
+//    }
+//        
+//    private StatementPatternNode newStatementPatternNode(
+//            final TermNode s, final TermNode p, final TermNode o, 
+//            final long cardinality, final boolean optional) {
+//        
+//        final StatementPatternNode sp = new StatementPatternNode(s, p, o);
+//        
+//        sp.setProperty(Annotations.ESTIMATED_CARDINALITY, cardinality);
+//        
+//        if (optional) {
+//            
+//            sp.setOptional(true);
+//            
+//        }
+//        
+//        return sp;
+//        
+//    }
 
 }
