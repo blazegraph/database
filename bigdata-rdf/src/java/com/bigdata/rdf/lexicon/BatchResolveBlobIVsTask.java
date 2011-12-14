a4a9010ecd6ab593ae998ca6eecffe900640d4d8
thompsonbry <thompsonbry@users.sourceforge.net>
2011-12-14 14:07:22 +0000

    formatting and removed unused logger
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/TERMS_REFACTOR_BRANCH@5778 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-rdf/src/java/com/bigdata/rdf/lexicon/BatchResolveBlobIVsTask.java
+++ bigdata-rdf/src/java/com/bigdata/rdf/lexicon/BatchResolveBlobIVsTask.java
@@ -9,7 +9,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 
-import org.apache.log4j.Logger;
 import org.openrdf.model.Value;
 
 import com.bigdata.btree.IIndex;
@@ -22,156 +21,156 @@ import com.bigdata.rdf.model.BigdataValue;
 import com.bigdata.rdf.model.BigdataValueFactory;
 
 /**
-     * Batch resolve {@link BlobIV}s to RDF {@link Value}s.
-     * 
-     * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan
-     *         Thompson</a>
-     */
-    class BatchResolveBlobIVsTask implements Callable<Void> {
+ * Batch resolve {@link BlobIV}s to RDF {@link Value}s.
+ * 
+ * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan
+ *         Thompson</a>
+ */
+class BatchResolveBlobIVsTask implements Callable<Void> {
 
-        static private final transient Logger log = Logger
-                .getLogger(BatchResolveBlobIVsTask.class);
+//    static private final transient Logger log = Logger
+//            .getLogger(BatchResolveBlobIVsTask.class);
 
-        private final ExecutorService service;
-        private final IIndex ndx;
-        private final Collection<BlobIV<?>> ivs;
-        private final ConcurrentHashMap<IV<?,?>/* iv */, BigdataValue/* term */> ret;
-        private final IConcurrentWeakValueCache<IV<?,?>, BigdataValue> termCache;
-        private final BigdataValueFactory valueFactory;
+    private final ExecutorService service;
+    private final IIndex ndx;
+    private final Collection<BlobIV<?>> ivs;
+    private final ConcurrentHashMap<IV<?,?>/* iv */, BigdataValue/* term */> ret;
+    private final IConcurrentWeakValueCache<IV<?,?>, BigdataValue> termCache;
+    private final BigdataValueFactory valueFactory;
 
-        public BatchResolveBlobIVsTask(
-                final ExecutorService service,
-                final IIndex ndx,
-                final Collection<BlobIV<?>> ivs,
-                final ConcurrentHashMap<IV<?, ?>/* iv */, BigdataValue/* term */> ret,
-                final IConcurrentWeakValueCache<IV<?,?>, BigdataValue> termCache,
-                final BigdataValueFactory valueFactory) {
+    public BatchResolveBlobIVsTask(
+            final ExecutorService service,
+            final IIndex ndx,
+            final Collection<BlobIV<?>> ivs,
+            final ConcurrentHashMap<IV<?, ?>/* iv */, BigdataValue/* term */> ret,
+            final IConcurrentWeakValueCache<IV<?,?>, BigdataValue> termCache,
+            final BigdataValueFactory valueFactory) {
 
-            this.service = service;
-            
-            this.ndx = ndx;
-            
-            this.ivs = ivs;
-            
-            this.ret = ret;
-            
-            this.termCache = termCache;
-            
-            this.valueFactory = valueFactory;
+        this.service = service;
+        
+        this.ndx = ndx;
+        
+        this.ivs = ivs;
+        
+        this.ret = ret;
+        
+        this.termCache = termCache;
+        
+        this.valueFactory = valueFactory;
+        
+    }
+
+    public Void call() throws Exception {
+
+        final int numNotFound = ivs.size();
+        
+        // An array of IVs that to be resolved against the index.
+        final BlobIV<?>[] notFound = ivs.toArray(new BlobIV[numNotFound]);
+        
+        // Sort IVs into index order.
+        Arrays.sort(notFound, 0, numNotFound);
+
+        // Encode IVs as keys for the index.
+        final byte[][] keys = new byte[numNotFound][];
+        {
+
+            final IKeyBuilder keyBuilder = KeyBuilder.newInstance();
+
+            for (int i = 0; i < numNotFound; i++) {
+
+                keys[i] = notFound[i].encode(keyBuilder.reset()).getKey();
+
+            }
             
         }
 
-        public Void call() throws Exception {
+        /*
+         * Note: This parameter is not terribly sensitive.
+         * 
+         * FIXME Config, tune, and use smaller chunk size for blobs.
+         */
+        final int MAX_CHUNK = 4000;
+        if (numNotFound < MAX_CHUNK) {
 
-            final int numNotFound = ivs.size();
-            
-            // An array of IVs that to be resolved against the index.
-            final BlobIV<?>[] notFound = ivs.toArray(new BlobIV[numNotFound]);
-            
-            // Sort IVs into index order.
-            Arrays.sort(notFound, 0, numNotFound);
+            /*
+             * Resolve everything in one go.
+             */
 
-            // Encode IVs as keys for the index.
-            final byte[][] keys = new byte[numNotFound][];
-            {
+            new ResolveBlobsTask(ndx, 0/* fromIndex */,
+                    numNotFound/* toIndex */, keys, notFound, ret,
+                    termCache, valueFactory).call();
 
-                final IKeyBuilder keyBuilder = KeyBuilder.newInstance();
+        } else {
 
-                for (int i = 0; i < numNotFound; i++) {
+            /*
+             * Break it down into multiple chunks and resolve those chunks
+             * in parallel.
+             */
 
-                    keys[i] = notFound[i].encode(keyBuilder.reset()).getKey();
+            // #of elements.
+            final int N = numNotFound;
+            // target maximum #of elements per chunk.
+            final int M = MAX_CHUNK;
+            // #of chunks
+            final int nchunks = (int) Math.ceil((double) N / M);
+            // #of elements per chunk, with any remainder in the last chunk.
+            final int perChunk = N / nchunks;
 
-                }
+            // System.err.println("N="+N+", M="+M+", nchunks="+nchunks+", perChunk="+perChunk);
+
+            final List<Callable<Void>> tasks = new ArrayList<Callable<Void>>(
+                    nchunks);
+
+            int fromIndex = 0;
+            int remaining = numNotFound;
+
+            for (int i = 0; i < nchunks; i++) {
+
+                final boolean lastChunk = i + 1 == nchunks;
+
+                final int chunkSize = lastChunk ? remaining : perChunk;
+
+                final int toIndex = fromIndex + chunkSize;
+
+                remaining -= chunkSize;
+
+                // System.err.println("chunkSize=" + chunkSize
+                // + ", fromIndex=" + fromIndex + ", toIndex="
+                // + toIndex + ", remaining=" + remaining);
+
+                tasks.add(new ResolveBlobsTask(ndx, fromIndex, toIndex,
+                        keys, notFound, ret, termCache, valueFactory));
+
+                fromIndex = toIndex;
+
+            }
+
+            try {
+
+                // Run tasks.
+                final List<Future<Void>> futures = service.invokeAll(tasks);
+
+                // Check futures.
+                for (Future<?> f : futures)
+                    f.get();
+
+            } catch (Exception e) {
+
+                throw new RuntimeException(e);
                 
             }
 
-            /*
-             * Note: This parameter is not terribly sensitive.
-             * 
-             * FIXME Config, tune, and use smaller chunk size for blobs.
-             */
-            final int MAX_CHUNK = 4000;
-            if (numNotFound < MAX_CHUNK) {
-
-                /*
-                 * Resolve everything in one go.
-                 */
-
-                new ResolveBlobsTask(ndx, 0/* fromIndex */,
-                        numNotFound/* toIndex */, keys, notFound, ret,
-                        termCache, valueFactory).call();
-
-            } else {
-
-                /*
-                 * Break it down into multiple chunks and resolve those chunks
-                 * in parallel.
-                 */
-
-                // #of elements.
-                final int N = numNotFound;
-                // target maximum #of elements per chunk.
-                final int M = MAX_CHUNK;
-                // #of chunks
-                final int nchunks = (int) Math.ceil((double) N / M);
-                // #of elements per chunk, with any remainder in the last chunk.
-                final int perChunk = N / nchunks;
-
-                // System.err.println("N="+N+", M="+M+", nchunks="+nchunks+", perChunk="+perChunk);
-
-                final List<Callable<Void>> tasks = new ArrayList<Callable<Void>>(
-                        nchunks);
-
-                int fromIndex = 0;
-                int remaining = numNotFound;
-
-                for (int i = 0; i < nchunks; i++) {
-
-                    final boolean lastChunk = i + 1 == nchunks;
-
-                    final int chunkSize = lastChunk ? remaining : perChunk;
-
-                    final int toIndex = fromIndex + chunkSize;
-
-                    remaining -= chunkSize;
-
-                    // System.err.println("chunkSize=" + chunkSize
-                    // + ", fromIndex=" + fromIndex + ", toIndex="
-                    // + toIndex + ", remaining=" + remaining);
-
-                    tasks.add(new ResolveBlobsTask(ndx, fromIndex, toIndex,
-                            keys, notFound, ret, termCache, valueFactory));
-
-                    fromIndex = toIndex;
-
-                }
-
-                try {
-
-                    // Run tasks.
-                    final List<Future<Void>> futures = service.invokeAll(tasks);
-
-                    // Check futures.
-                    for (Future<?> f : futures)
-                        f.get();
-
-                } catch (Exception e) {
-
-                    throw new RuntimeException(e);
-                    
-                }
-
-            }
+        }
 
 //          final long elapsed = System.currentTimeMillis() - begin;
 //          
 //          if (log.isInfoEnabled())
 //              log.info("resolved " + numNotFound + " terms in "
 //                      + tasks.size() + " chunks and " + elapsed + "ms");
-            
-            // Done.
-            return null;
-            
-        }
+        
+        // Done.
+        return null;
         
     }
+    
+}
