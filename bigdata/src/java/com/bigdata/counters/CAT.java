5cfcddcdf28ec895f0e58e420503eca79fe24e49
thompsonbry <thompsonbry@users.sourceforge.net>
2012-01-29 18:10:29 +0000

    The CAT object was serializing the underlying ConcurrentAutoTable, which has a fat serialization including all of the state for the thread safety dynamics.  I believe that this may have contributed to bad updates of the query evaluation counters in scale-out.  This commit provides a simpler serialization format.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_1_0@5907 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/counters/CAT.java
+++ bigdata/src/java/com/bigdata/counters/CAT.java
@@ -27,6 +27,10 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package com.bigdata.counters;
 
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.cliffc.high_scale_lib.ConcurrentAutoTable;
@@ -37,10 +41,97 @@ import org.cliffc.high_scale_lib.ConcurrentAutoTable;
  * their internal state to minimize thread contention. However, they can do more
  * work than an {@link AtomicLong} on read since they must scan an internal
  * table to aggregate updates from various threads.
+ * <p>
+ * Note: The class uses a delegation pattern in order to replace the
+ * serialization logic with a simple serialized long value.
+ * 
+ * @see ConcurrentAutoTable
  * 
  * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan Thompson</a>
  * @version $Id$
  */
-public class CAT extends ConcurrentAutoTable {
+public class CAT /*extends ConcurrentAutoTable*/ implements Externalizable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    private ConcurrentAutoTable cat;
+
+    /**
+     * Initialize a new counter with a value of <code>0L</code>.
+     */
+    public CAT() {
+        cat = new ConcurrentAutoTable();
+    }
+
+    /**
+     * Add the value to the counter.
+     * 
+     * @see ConcurrentAutoTable#add(long)
+     */
+    final public void add(long x) {
+        cat.add(x);
+    }
+
+    /** Decrement the value of the counter. 
+     * 
+     * @see ConcurrentAutoTable#decrement()
+     */
+    final public void decrement() {
+        cat.decrement();
+    }
+
+    /**
+     * Increment the value of the counter.
+     * 
+     * @see ConcurrentAutoTable#increment()
+     */
+    final public void increment() {
+        cat.increment();
+    }
+
+    /**
+     * Set the value of the counter.
+     * 
+     * @see ConcurrentAutoTable#set(long)
+     */
+    final public void set(long x) {
+        cat.set(x);
+    }
+
+    /**
+     * Current value of the counter.
+     * 
+     * @see ConcurrentAutoTable#get()
+     */
+    final public long get() {
+        return cat.get();
+    }
+
+    /**
+     * Estimate the current value of the counter.
+     * 
+     * @see ConcurrentAutoTable#estimate_get()
+     */
+    final public long estimate_get() {
+        return cat.estimate_get();
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        
+        out.writeLong(get());
+        
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException,
+            ClassNotFoundException {
+
+        cat.set(in.readLong());
+        
+    }
 
 }
