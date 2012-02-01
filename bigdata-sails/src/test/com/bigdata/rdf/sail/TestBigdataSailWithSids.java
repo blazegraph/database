7b75e7cc20e1009b9be379dd7c1ccf5055fe66fb
thompsonbry <thompsonbry@users.sourceforge.net>
2012-02-01 21:43:44 +0000

    The new versions of TestMROWTransactions were not integrations into the triples and sids mode SAIL test suites.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_1_0@5942 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-sails/src/test/com/bigdata/rdf/sail/TestBigdataSailWithSids.java
+++ bigdata-sails/src/test/com/bigdata/rdf/sail/TestBigdataSailWithSids.java
@@ -88,8 +88,9 @@ public class TestBigdataSailWithSids extends AbstractBigdataSailTestCase {
 		suite.addTestSuite(com.bigdata.rdf.sail.TestRollbacks.class);
 		suite.addTestSuite(com.bigdata.rdf.sail.TestRollbacksTx.class);
         suite.addTestSuite(com.bigdata.rdf.sail.TestRollbacksTM.class);
-        suite.addTestSuite(com.bigdata.rdf.sail.TestMROWTransactions.class);
-		
+        suite.addTestSuite(com.bigdata.rdf.sail.TestMROWTransactionsNoHistory.class);
+        suite.addTestSuite(com.bigdata.rdf.sail.TestMROWTransactionsWithHistory.class);
+
         suite.addTestSuite(com.bigdata.rdf.sail.TestMillisecondPrecisionForInlineDateTimes.class);
 
         suite.addTestSuite(com.bigdata.rdf.sail.TestTicket275.class);
