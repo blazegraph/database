05192284268a74172f04545b8c5b1f9efbf22870
thompsonbry <thompsonbry@users.sourceforge.net>
2012-02-01 17:20:50 +0000

    Hooked the TestMROWTransactionsNoHistory and WithHistory test suites into TestBigdataSailWithQuads (it was only running the older TestMROWTransactions which is now an abstract class).
    Added a stress test variant with history into TestMROWTransactionsWithHistory.
    
    @see https://sourceforge.net/apps/trac/bigdata/ticket/440
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_1_0@5939 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-sails/src/test/com/bigdata/rdf/sail/TestBigdataSailWithQuads.java
+++ bigdata-sails/src/test/com/bigdata/rdf/sail/TestBigdataSailWithQuads.java
@@ -104,8 +104,9 @@ public class TestBigdataSailWithQuads extends AbstractBigdataSailTestCase {
         
 		suite.addTestSuite(com.bigdata.rdf.sail.TestRollbacks.class);
 		suite.addTestSuite(com.bigdata.rdf.sail.TestRollbacksTx.class);
-		suite.addTestSuite(com.bigdata.rdf.sail.TestMROWTransactions.class);
-		
+		suite.addTestSuite(com.bigdata.rdf.sail.TestMROWTransactionsNoHistory.class);
+		suite.addTestSuite(com.bigdata.rdf.sail.TestMROWTransactionsWithHistory.class);
+        
 		suite.addTestSuite(com.bigdata.rdf.sail.TestMillisecondPrecisionForInlineDateTimes.class);
 		
         suite.addTestSuite(com.bigdata.rdf.sail.TestTicket275.class);
