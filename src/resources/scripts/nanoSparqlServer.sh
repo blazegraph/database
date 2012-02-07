58a7469bf574f58374507205f9f4274f13357f8b
thompsonbry <thompsonbry@users.sourceforge.net>
2012-02-07 20:15:23 +0000

    Override nthreads for the NSS on a cluster (from default of 16 to 64).
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_1_0@5978 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- src/resources/scripts/nanoSparqlServer.sh
+++ src/resources/scripts/nanoSparqlServer.sh
@@ -17,12 +17,17 @@ namespace=$2
 # read lock is optional and may be any commit time on the database.
 readLock=-1
 
+# The default is 16 threads.  This raises the #of threads up to 16 for the 
+# cluster.
+nthreads=64
+
 echo "port=$port namespace=$namespace config=$BIGDATA_CONFIG"
 
 java ${JAVA_OPTS} \
     -cp ${CLASSPATH} \
     com.bigdata.rdf.sail.webapp.NanoSparqlServer \
     -readLock $readLock \
+	-nthreads $nthreads \
     $port \
     $namespace \
     ${BIGDATA_CONFIG} ${BIGDATA_CONFIG_OVERRIDES}
