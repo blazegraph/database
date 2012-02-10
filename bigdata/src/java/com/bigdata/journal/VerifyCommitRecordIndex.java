eb0de2ec4b7dc3b8b9ab518650a6a03a661c5950
thompsonbry <thompsonbry@users.sourceforge.net>
2012-02-10 16:01:58 +0000

    Commit of journal migration utility by Martyn.  The utility prunes entries in the commit record index whose deferred deletes have already been released.  This utility is necessary when upgrading from bigdata 1.0.4 to 1.0.6 if deferred deletes were enabled for the journal (minReleaseAge > 0).  When minReleaseAge is ZERO (0), the database uses session protection mode and the older commit record entries will not cause a problem.
    This utility might also be necessary when moving from 1.1.0 to 1.1.1.
    
    @see https://sourceforge.net/apps/trac/bigdata/ticket/480 (Error releasing deferred frees using 1.0.6 against a 1.0.4 journal)
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_1_0@6008 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata/src/java/com/bigdata/journal/VerifyCommitRecordIndex.java
@@ -0,0 +1,107 @@
+/**
+
+Copyright (C) SYSTAP, LLC 2006-2012.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/*
+ * Created on Feb 10, 2012
+ */
+
+package com.bigdata.journal;
+
+import java.io.File;
+import java.util.Properties;
+
+import com.bigdata.btree.IIndex;
+import com.bigdata.btree.IndexMetadata;
+import com.bigdata.journal.BufferMode;
+import com.bigdata.journal.Journal;
+import com.bigdata.journal.Options;
+import com.bigdata.journal.RWStrategy;
+import com.bigdata.rwstore.RWStore;
+
+/**
+ * Given an existing journal, ensure that any commitRecords that reference a
+ * time prior to the last deferred release time are removed. This provides a
+ * "fix" for opening a bigdata 1.0.4 journal in bigdata 1.0.6.  
+ * 
+ * @see <a href="https://sourceforge.net/apps/trac/bigdata/ticket/480"> Error
+ *      releasing deferred frees using 1.0.6 against a 1.0.4 journal</a>
+ *      
+ * @author <a href="mailto:martyncutcher@users.sourceforge.net">Martyn Cutcher</a>
+ * @version $Id$
+ */
+public class VerifyCommitRecordIndex {
+    
+    public static void main(final String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: <journal filename>");
+            
+            return;
+        }
+        
+        final String fname = args[0];
+        final File f = new File(fname);
+        if (!f.exists()) {
+            System.err.println("File: " + fname + " not found");
+            
+            return;
+        }
+        
+        final Properties props = new Properties();
+        props.setProperty(Options.FILE, fname);
+        
+        final Journal jrnl = new Journal(props);
+        
+        // Check if journal is DISKRW
+        if (jrnl.getBufferStrategy().getBufferMode() != BufferMode.DiskRW) {            
+            System.err.println("Buffer mode should be DiskRW not " + jrnl.getBufferStrategy().getBufferMode());
+            
+            return;
+        }
+        
+        final RWStrategy rwstrategy = (RWStrategy) jrnl.getBufferStrategy();
+        final RWStore rwstore = rwstrategy.getRWStore();
+        
+        final IIndex commitRecordIndex = jrnl.getReadOnlyCommitRecordIndex();
+        if (commitRecordIndex == null) {
+            System.err.println("Unexpected null commit record index");
+            return;
+        }
+
+        final IndexMetadata metadata = commitRecordIndex
+                .getIndexMetadata();
+
+        final byte[] zeroKey = metadata.getTupleSerializer()
+                .serializeKey(0L);
+
+        final byte[] releaseKey = metadata.getTupleSerializer()
+                .serializeKey( rwstore.getLastDeferredReleaseTime());
+
+        final int removed = jrnl.removeCommitRecordEntries(zeroKey, releaseKey);
+        
+        System.out.println("Commit Record Index verified with " + removed + " records removed");
+
+        jrnl.commit();
+        
+    }
+
+}
