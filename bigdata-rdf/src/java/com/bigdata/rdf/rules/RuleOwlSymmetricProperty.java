b4bd169a63fa0cba2ce5b1e7ce45ca34c6f0d53b
mrpersonick <mrpersonick@users.sourceforge.net>
2012-02-16 08:43:46 +0000

    added support for owl:SymmetricProperty
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_1_0@6031 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata-rdf/src/java/com/bigdata/rdf/rules/RuleOwlSymmetricProperty.java
@@ -0,0 +1,73 @@
+/**
+
+Copyright (C) SYSTAP, LLC 2006-2007.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/*
+ * Created on Nov 1, 2007
+ */
+
+package com.bigdata.rdf.rules;
+
+import org.openrdf.model.vocabulary.OWL;
+import org.openrdf.model.vocabulary.RDF;
+
+import com.bigdata.bop.IConstraint;
+import com.bigdata.bop.constraint.Constraint;
+import com.bigdata.bop.constraint.NE;
+import com.bigdata.rdf.spo.SPOPredicate;
+import com.bigdata.rdf.vocab.Vocabulary;
+import com.bigdata.relation.rule.Rule;
+
+/**
+ * owl:SymmetricProperty
+ * 
+ * <pre>
+ *   (x rdf:type owl:SymmetricProperty), (a x b) -&gt; (b x a).
+ * </pre>
+ */
+@SuppressWarnings("rawtypes")
+public class RuleOwlSymmetricProperty extends Rule {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -6688762355076324400L;
+
+	/**
+     * @param vocab
+     */
+    public RuleOwlSymmetricProperty(String relationName, Vocabulary vocab) {
+
+        super( "owlSymmetricProperty", //
+                new SPOPredicate(relationName,var("b"), var("x"), var("a")),//
+                new SPOPredicate[] {//
+                    new SPOPredicate(relationName,var("x"), vocab.getConstant(RDF.TYPE), vocab.getConstant(OWL.SYMMETRICPROPERTY)),//
+                    new SPOPredicate(relationName,var("a"), var("x"), var("b"))//
+                },
+                new IConstraint[] {
+					Constraint.wrap(new NE(var("a"),var("b")))
+		        }
+                );
+        
+    }
+
+}
---
+++ bigdata-rdf/src/test/com/bigdata/rdf/rules/TestRuleOwlSymmetricProperty.java
@@ -0,0 +1,118 @@
+/**
+
+Copyright (C) SYSTAP, LLC 2006-2009.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package com.bigdata.rdf.rules;
+
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.URIImpl;
+import org.openrdf.model.vocabulary.OWL;
+import org.openrdf.model.vocabulary.RDF;
+
+import com.bigdata.rdf.rio.IStatementBuffer;
+import com.bigdata.rdf.rio.StatementBuffer;
+import com.bigdata.rdf.store.AbstractTripleStore;
+import com.bigdata.rdf.vocab.Vocabulary;
+import com.bigdata.relation.rule.Rule;
+
+/**
+ * Test suite for owl:SymmetricProperty processing.
+ * 
+ * <pre>
+ * (x rdf:type owl:SymmetricProperty), (a x b) -&gt; (b x a)
+ * </pre>
+ * 
+ * @see RuleOwlSymmetricProperty
+ * 
+ * @author <a href="mailto:mrpersonick@users.sourceforge.net">Mike Personick</a>
+ */
+public class TestRuleOwlSymmetricProperty extends AbstractRuleTestCase {
+
+    /**
+     * 
+     */
+    public TestRuleOwlSymmetricProperty() {
+        super();
+    }
+
+    /**
+     * @param name
+     */
+    public TestRuleOwlSymmetricProperty(String name) {
+        super(name);
+    }
+
+    public void test_Rule() throws Exception {
+
+        AbstractTripleStore store = getStore();
+
+        try {
+
+            URI A = new URIImpl("http://www.foo.org/A");
+            URI B = new URIImpl("http://www.foo.org/B");
+            URI X = new URIImpl("http://www.foo.org/X");
+
+            IStatementBuffer buffer = new StatementBuffer(store, 100/* capacity */);
+            
+            buffer.add(X, RDF.TYPE, OWL.SYMMETRICPROPERTY);
+            buffer.add(A, X, B);
+
+            // write on the store.
+            buffer.flush();
+
+            // verify statement(s).
+            assertTrue(store.hasStatement(X, RDF.TYPE, OWL.SYMMETRICPROPERTY));
+            assertTrue(store.hasStatement(A, X, B));
+            final long nbefore = store.getStatementCount();
+
+            final Vocabulary vocab = store.getVocabulary();
+
+            final Rule r = new RuleOwlSymmetricProperty(store.getSPORelation()
+                    .getNamespace(), vocab);
+
+            // apply the rule.
+            applyRule(store, r, -1/*solutionCount*/,1/*mutationCount*/);
+
+            /*
+             * validate the state of the primary store.
+             */
+
+            // told
+            assertTrue(store.hasStatement(X, RDF.TYPE, OWL.SYMMETRICPROPERTY));
+            assertTrue(store.hasStatement(A, X, B));
+
+            // entailed
+            assertTrue(store.hasStatement(B, X, A));
+
+            // final #of statements in the store.
+            assertEquals(nbefore + 1, store.getStatementCount());
+
+        } finally {
+
+            store.__tearDownUnitTest();
+
+        }
+        
+    }
+
+}
