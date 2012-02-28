aa297f5cf030d8b36d8ac316eacc78086974a299
thompsonbry <thompsonbry@users.sourceforge.net>
2012-02-28 18:20:09 +0000

    I have caught up the RDF/XML parser with the changes in openrdf from 2.0 through 2.6.3.
    The RDF/XML parser/writer test suite is now integrated.   In order to avoid a dependency on xmlsec, I have imported and modified the RDFXMLParserTestCase. That class used the xmlsec library in its CanonXMLValueFactory class. One test fails with that dependency removed which would otherwise pass. The test failure looks like this:
    {{{
    (http://www.w3.org/2000/10/rdf-tests/rdfcore/rdfms-xml-literal-namespaces/test001.rdf#John_Smith, http://my.example.org/Name, "
          <html:h1 xmlns:html="http://NoHTML.example.org">
            <b xmlns="http://www.w3.org/1999/xhtml">John</b>
          </html:h1>
       "^^<http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral>)
    Actual:
    (http://www.w3.org/2000/10/rdf-tests/rdfcore/rdfms-xml-literal-namespaces/test001.rdf#John_Smith, http://my.example.org/Name, "
          <html:h1 xmlns:html="http://NoHTML.example.org" xmlns="http://www.w3.org/1999/xhtml">
            <b>John</b>
          </html:h1>
       "^^<http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral>)
    }}}
    
    I have written to the sesame-general list to see if someone can clarify why XML canonicalization was being used.  We could probably develop an alternative workaround which just strips out the offending attribute:
    {{{
    xmlns="http://www.w3.org/1999/xhtml
    }}}
    
    
    @see https://sourceforge.net/apps/trac/bigdata/ticket/498 (Bring bigdata RDF/XML parser up to openrdf 2.6.3.)
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_1_0@6051 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata-rdf/src/test/com/bigdata/rdf/rio/rdfxml/RDFXMLWriterTestCase.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 1997-2008.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package com.bigdata.rdf.rio.rdfxml;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.net.URL;
+
+import junit.framework.Assert;
+
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.repository.util.RepositoryUtil;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.RDFParseException;
+import org.openrdf.rio.RDFParserFactory;
+import org.openrdf.rio.RDFWriter;
+import org.openrdf.rio.RDFWriterFactory;
+import org.openrdf.sail.memory.MemoryStore;
+
+/*
+ * FIXME Drop this when we migrate to a modern junit. It exists because the
+ * RDFWriterTest class does not extend TestCase in openrdf.
+ */
+public abstract class RDFXMLWriterTestCase extends RDFWriterTest {
+
+	protected RDFXMLWriterTestCase(RDFWriterFactory writerF, RDFParserFactory parserF) {
+		super(writerF, parserF);
+	}
+
+	public void testWrite()
+		throws RepositoryException, RDFParseException, IOException, RDFHandlerException
+	{
+		Repository rep1 = new SailRepository(new MemoryStore());
+		rep1.initialize();
+
+		RepositoryConnection con1 = rep1.getConnection();
+
+		URL ciaScheme = this.getClass().getResource("/cia-factbook/CIA-onto-enhanced.rdf");
+		URL ciaFacts = this.getClass().getResource("/cia-factbook/CIA-facts-enhanced.rdf");
+
+		con1.add(ciaScheme, ciaScheme.toExternalForm(), RDFFormat.forFileName(ciaScheme.toExternalForm()));
+		con1.add(ciaFacts, ciaFacts.toExternalForm(), RDFFormat.forFileName(ciaFacts.toExternalForm()));
+
+		StringWriter writer = new StringWriter();
+		RDFWriter rdfWriter = rdfWriterFactory.getWriter(writer);
+		con1.export(rdfWriter);
+
+		con1.close();
+
+		Repository rep2 = new SailRepository(new MemoryStore());
+		rep2.initialize();
+
+		RepositoryConnection con2 = rep2.getConnection();
+
+		con2.add(new StringReader(writer.toString()), "foo:bar", RDFFormat.RDFXML);
+		con2.close();
+
+		Assert.assertTrue("result of serialization and re-upload should be equal to original", RepositoryUtil.equals(
+				rep1, rep2));
+	}
+
+	
+	@Override
+//	@Test
+//	@Ignore("[SES-879] round trip for RDF/XML currently fails on literals ending with newlines.")
+	public void testRoundTrip()
+		throws RDFHandlerException, RDFParseException, IOException {
+		// [SES-879] round trip for RDF/XML currently fails on literals ending with newlines. Test disabled to allow
+		// build to succeed pending fix.
+	}
+}
