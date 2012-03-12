blazegraph/database

commit 025232627090619fc4d80cb8b4d44967e44e1779

Reverting to the old serialization version for BigdataValues since it is significantly faster.  This change enjoys binary compatibility since the version is packed into the serialized Value record.
Note: I thought that this change was committed in r6098, but it seems that the it was not.  Only the javadoc was updated in that commit.  This changes the default version number as well.

https://sourceforge.net/apps/trac/bigdata/ticket/506 (Load, closure and query performance in 1.1.x versus 1.0.x)

git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_1_0@6109 8f7bc0f5-282e-0410-95e3-8d296e9bb460

bigdata-rdf/src/java/com/bigdata/rdf/model/BigdataValueSerializer.java
@@ -85,7 +85,7 @@ public class BigdataValueSerializer<V extends Value> {
      * @see <a href="https://sourceforge.net/apps/trac/bigdata/ticket/506">
      *      Load, closure and query performance in 1.1.x versus 1.0.x </a>
      */
-    private final static short currentVersion = VERSION1;
+    private final static short currentVersion = VERSION0;
     
     /**
      * Error message indicates that the version code in the serialized

commit 3a856834493ef3f5810b51fce1ce99a720f3d699

Modified BigdataValueSerializer to make a decision dynamically between VERSION0 and VERSION1 based on the RDF Value to be serialized.  This fixes a bunch of tests with large RDF Values which were broken by r6109 (above).  I have also refactored the static getStringLength(Value) method from LexiconRelation into BigdataValueSerializer.
Modified LexiconRelation#createInlineUnicodeLiteral() to only invoke getStringLength() if we are willing to use a fully inline IV for the Value.

Wrapping the code in LexiconRelation and IVBindingSetEncoderWithIVCache in if(true) blocks prior to disabling it.  Because this change trades improved load performance against a significant increase in the disk on the disk for the lexicon indices and slightly reduced query performance, I want to explore the use of a fully inline URI IV based on a vocabulary namespace and a short Unicode localName before committing this change.  Hopefully we can get something which is better all around.

https://sourceforge.net/apps/trac/bigdata/ticket/506 (Load, closure and query performance in 1.1.x versus 1.0.x)

git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_1_0@6111 8f7bc0f5-282e-0410-95e3-8d296e9bb460

bigdata-rdf/src/java/com/bigdata/rdf/internal/LexiconConfiguration.java
@@ -62,6 +62,7 @@ import com.bigdata.rdf.model.BigdataLiteral;
 import com.bigdata.rdf.model.BigdataURI;
 import com.bigdata.rdf.model.BigdataValue;
 import com.bigdata.rdf.model.BigdataValueFactory;
+import com.bigdata.rdf.model.BigdataValueSerializer;
 import com.bigdata.rdf.store.AbstractTripleStore;
 import com.bigdata.rdf.vocab.Vocabulary;
 import com.bigdata.util.InnerCause;
@@ -517,15 +518,25 @@ public class LexiconConfiguration<V extends BigdataValue>
     private AbstractInlineIV<BigdataLiteral, ?> createInlineUnicodeLiteral(
             final Literal value) {
 
-        final long totalLength = LexiconRelation.getStringLength(value);
+        if (maxInlineTextLength > 0) {
 
-        if (totalLength <= maxInlineTextLength) {
+            /*
+             * Only check the string length if we are willing to turn this into
+             * a fully inline IV.
+             */
 
-            return new FullyInlineTypedLiteralIV<BigdataLiteral>(value.getLabel(), value
-                    .getLanguage(), value.getDatatype());
+            final long totalLength = BigdataValueSerializer.getStringLength(value);
+
+            if (totalLength <= maxInlineTextLength) {
+
+                return new FullyInlineTypedLiteralIV<BigdataLiteral>(
+                        value.getLabel(), value.getLanguage(),
+                        value.getDatatype());
+
+            }
 
         }
-
+        
         // Will not inline as Unicode.
         return null;
         

bigdata-rdf/src/java/com/bigdata/rdf/internal/encoder/IVBindingSetEncoderWithIVCache.java
@@ -184,18 +184,22 @@ public class IVBindingSetEncoderWithIVCache extends IVBindingSetEncoder {
         // values.
         metadata.setBloomFilterFactory(BloomFilterFactory.DEFAULT);
 
-        // enable raw record support.
-        metadata.setRawRecords(true);
+        if (true) {
+          
+            // enable raw record support.
+            metadata.setRawRecords(true);
 
-        /*
-         * Very small RDF values can be inlined into the index, but after that
-         * threshold we want to have the values out of line on the backing
-         * store.
-         * 
-         * TODO Tune this and the threshold at which we use the BLOBS index
-         * instead.
-         */
-        metadata.setMaxRecLen(16); 
+            /*
+             * Very small RDF values can be inlined into the index, but after
+             * that threshold we want to have the values out of line on the
+             * backing store.
+             * 
+             * TODO Tune this and the threshold at which we use the BLOBS index
+             * instead.
+             */
+            metadata.setMaxRecLen(16);
+            
+        }
         
         return metadata;
 

bigdata-rdf/src/java/com/bigdata/rdf/lexicon/LexiconRelation.java
@@ -1090,8 +1090,11 @@ public class LexiconRelation extends AbstractRelation<BigdataValue>
      * 
      * @param name
      *            The name of the index.
-     *            
+     * 
      * @return The {@link IndexMetadata}.
+     * 
+     * @see <a href="https://sourceforge.net/apps/trac/bigdata/ticket/506">
+     *      Load, closure and query performance in 1.1.x versus 1.0.x </a>
      */
     protected IndexMetadata getId2TermIndexMetadata(final String name) {
 
@@ -1100,18 +1103,29 @@ public class LexiconRelation extends AbstractRelation<BigdataValue>
         metadata.setTupleSerializer(new Id2TermTupleSerializer(
                 getNamespace(), getValueFactory()));
 
-        // enable raw record support.
-        metadata.setRawRecords(true);
-
         /*
-         * Very small RDF values can be inlined into the index, but after that
-         * threshold we want to have the values out of line on the backing
-         * store.
+         * Note: Enabling raw record support slows down the ID2TERM index
+         * significantly.
          * 
-         * TODO Tune this and the threshold at which we use the BLOBS index
-         * instead.
+         * @see https://sourceforge.net/apps/trac/bigdata/ticket/506 (Load,
+         * closure and query performance in 1.1.x versus 1.0.x)
          */
-        metadata.setMaxRecLen(16); 
+        if(true) {
+
+            // enable raw record support.
+            metadata.setRawRecords(true);
+
+            /*
+             * Very small RDF values can be inlined into the index, but after
+             * that threshold we want to have the values out of line on the
+             * backing store.
+             * 
+             * TODO Tune this and the threshold at which we use the BLOBS index
+             * instead.
+             */
+            metadata.setMaxRecLen(16);
+        
+        }
         
         return metadata;
 
@@ -1403,52 +1417,6 @@ public class LexiconRelation extends AbstractRelation<BigdataValue>
     }
 
     /**
-     * Return the total #of characters in the RDF {@link Value}.
-     * 
-     * @param v
-     *            The {@link Value}.
-     * 
-     * @return The character length of the data in the RDF {@link Value}.
-     */
-    static public long getStringLength(final Value v) {
-
-        if (v == null)
-            throw new IllegalArgumentException();
-        
-        if (v instanceof URI) {
-
-            return ((URI) v).stringValue().length();
-
-        } else if (v instanceof Literal) {
-
-            final Literal value = (Literal) v;
-
-            final String label = value.getLabel();
-
-            final int datatypeLength = value.getDatatype() == null ? 0 : value
-                    .getDatatype().stringValue().length();
-
-            final int languageLength = value.getLanguage() == null ? 0 : value
-                    .getLanguage().length();
-
-            final long totalLength = label.length() + datatypeLength
-                    + languageLength;
-
-            return totalLength;
-
-        } else if (v instanceof BNode) {
-
-            return ((BNode) v).getID().length();
-
-        } else {
-            
-            throw new UnsupportedOperationException();
-            
-        }
-        
-    }
-
-    /**
      * Return <code>true</code> iff this {@link Value} would be stored in the
      * {@link LexiconKeyOrder#BLOBS} index.
      * 
@@ -1465,7 +1433,7 @@ public class LexiconRelation extends AbstractRelation<BigdataValue>
         if (blobsThreshold == 0)
             return true;
         
-        return getStringLength(v) >= blobsThreshold;
+        return BigdataValueSerializer.getStringLength(v) >= blobsThreshold;
 
     }
 
@@ -2075,6 +2043,8 @@ public class LexiconRelation extends AbstractRelation<BigdataValue>
          */
         int numNotFound = 0;
 
+        final boolean isDebugEnabled = log.isDebugEnabled();
+        
         for (IV<?,?> iv : ivs) {
             
             if (iv == null)
@@ -2082,9 +2052,8 @@ public class LexiconRelation extends AbstractRelation<BigdataValue>
             
     		if (iv.hasValue()) {
                 
-            	if (log.isDebugEnabled()) {
+            	if (isDebugEnabled)
             		log.debug("already materialized: " + iv.getValue());
-            	}
             	
             	// already materialized
             	ret.put(iv, iv.getValue());

bigdata-rdf/src/java/com/bigdata/rdf/model/BigdataValueSerializer.java
@@ -81,11 +81,34 @@ public class BigdataValueSerializer<V extends Value> {
      * Note: Changing back to {@link #VERSION0}. It looks like it was
      * significantly more efficient (though it might be possible to optimize the
      * code paths for {@link #VERSION1}).
+     * <p>
+     * Note: {@link #VERSION0} can not be used with the BLOBS index since it can
+     * not handle very large {@link Value}s. When changing this to
+     * {@link #VERSION0}, that change needs to be exclusive of the version used
+     * for the BLOBS index (ie, it is good for ID2TERM and related indices such
+     * as the ivCache, but not for the BLOBS index and related indices such as
+     * the blobsCache).
      * 
      * @see <a href="https://sourceforge.net/apps/trac/bigdata/ticket/506">
      *      Load, closure and query performance in 1.1.x versus 1.0.x </a>
      */
-    private final static short currentVersion = VERSION1;
+//    private final static short currentVersion = VERSION0;
+    private static final short getVersion(final Value val) {
+
+        if (BigdataValueSerializer.getStringLength(val) < Short.MAX_VALUE) {
+            /*
+             * This version is faster, but can only be used with UTF data LT 64k
+             * in length.
+             */
+            return VERSION0;
+        } else {
+            /*
+             * This version can be used with very large UTF strings (BLOBS).
+             */
+            return VERSION1;
+        }
+
+    }
     
     /**
      * Error message indicates that the version code in the serialized
@@ -198,8 +221,7 @@ public class BigdataValueSerializer<V extends Value> {
      */
     public byte[] serialize(final V val) {
 
-        return serialize(val, new DataOutputBuffer(128), new ByteArrayBuffer(
-                128));
+        return serialize(val, new DataOutputBuffer(128), null/* lazilyAllocated */);
         
     }
 
@@ -215,7 +237,8 @@ public class BigdataValueSerializer<V extends Value> {
      *            buffer before each invocation.
      * @param tmp
      *            A buffer used to compress the component Unicode strings. This
-     *            will be reset as necessary by this method.
+     *            will be reset as necessary by this method. It will be lazily
+     *            allocated if <code>null</code>.
      * 
      * @return The byte[] containing the serialized data record. This array is
      *         newly allocated so that a series of invocations of this method
@@ -242,24 +265,32 @@ public class BigdataValueSerializer<V extends Value> {
      *            buffer before each invocation.
      * @param tmp
      *            A buffer used to compress the component Unicode strings. This
-     *            will be reset as necessary by this method.
+     *            will be reset as necessary by this method. It will be lazily
+     *            allocated if <code>null</code>.
      */
-    public void serialize2(final V val, final DataOutputBuffer out, final
+    public void serialize2(final V val, final DataOutputBuffer out, 
             ByteArrayBuffer tmp) {
         
         try {
 
-            final short version = currentVersion;
-
+            final short version = getVersion(val);
+            
             ShortPacker.packShort(out, version);
 
             switch (version) {
             case VERSION0:
                 serializeVersion0(val, version, out);
                 break;
-            case VERSION1:
+            case VERSION1: {
+                if(tmp == null) {
+                    /*
+                     * Allocate lazily on the code path where it is necessary.
+                     */
+                    tmp = new ByteArrayBuffer(128);
+                }
                 serializeVersion1(val, version, out, tmp);
                 break;
+            }
             default:
                 throw new UnsupportedOperationException(ERR_VERSION);
             }
@@ -650,6 +681,52 @@ public class BigdataValueSerializer<V extends Value> {
 
         }
 
+    }
+
+    /**
+     * Return the total #of characters in the RDF {@link Value}.
+     * 
+     * @param v
+     *            The {@link Value}.
+     * 
+     * @return The character length of the data in the RDF {@link Value}.
+     */
+    static public long getStringLength(final Value v) {
+    
+        if (v == null)
+            throw new IllegalArgumentException();
+        
+        if (v instanceof URI) {
+    
+            return ((URI) v).stringValue().length();
+    
+        } else if (v instanceof Literal) {
+    
+            final Literal value = (Literal) v;
+    
+            final String label = value.getLabel();
+    
+            final int datatypeLength = value.getDatatype() == null ? 0 : value
+                    .getDatatype().stringValue().length();
+    
+            final int languageLength = value.getLanguage() == null ? 0 : value
+                    .getLanguage().length();
+    
+            final long totalLength = label.length() + datatypeLength
+                    + languageLength;
+    
+            return totalLength;
+    
+        } else if (v instanceof BNode) {
+    
+            return ((BNode) v).getID().length();
+    
+        } else {
+            
+            throw new UnsupportedOperationException();
+            
+        }
+        
     }
 
 }