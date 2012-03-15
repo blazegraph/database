76984b7c2e5c6b5e5c2dacee633e1686a99e96ef
thompsonbry <thompsonbry@users.sourceforge.net>
2012-03-15 14:19:42 +0000

    Added a unit test for local evaluation of a query which is basically Federated Query service04 to validate my interpretation of the query semantics.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_1_0@6140 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/eval/TestBindings.java
+++ bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/eval/TestBindings.java
@@ -32,6 +32,20 @@ package com.bigdata.rdf.sparql.ast.eval;
  * 
  * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan Thompson</a>
  * @version $Id$
+ * 
+ *          TODO Unit test to verify that static analysis correctly leverages
+ *          when a variable is known bound to a constant across multiple
+ *          solutions in the BINDINGS clause and when it takes on different
+ *          values in different solutions in the BINDINGS clause.
+ * 
+ *          TODO Unit test when a variable is bound in some solutions in the
+ *          BINDINGS clause but not in other solutions in that BINDINGS clause.
+ *          The variable can not be treated as "known" bound in the latter case.
+ * 
+ *          TODO Unit test when a variable which is known bound in the BINDINGS
+ *          clause is not projected into a sub-select. The test should verify
+ *          that a variable by the same name in the sub-select is in fact a
+ *          distinct variable.
  */
 public class TestBindings extends AbstractDataDrivenSPARQLTestCase {
 
@@ -126,4 +140,60 @@ public class TestBindings extends AbstractDataDrivenSPARQLTestCase {
 
     }
 
+    /**
+     * This is a variant of Federated Query <code>service04</code> where the
+     * remote end point is treated as a named graph. This makes it easier to
+     * examine the JOIN evaluation logic, which should be the same regardless of
+     * whether or not we use a SERVICE call.
+     * <p>
+     * This is the result when run as a SERVICE call.
+     * 
+     * <pre>
+     * SOLUTION:   7b4c0689-2141-4523-8eae-e46338f8953d    N/A -1  -1  8   { o2=http://example.org/b, s=http://example.org/a, o1="alan@example.org" }
+     * SOLUTION:   7b4c0689-2141-4523-8eae-e46338f8953d    N/A -1  -1  8   { o2=http://example.org/b, s=http://example.org/a, o1="Alan" }
+     * SOLUTION:   7b4c0689-2141-4523-8eae-e46338f8953d    N/A -1  -1  8   { o2=http://example.org/b, s=http://example.org/a, o1="alan@example.org" }
+     * SOLUTION:   7b4c0689-2141-4523-8eae-e46338f8953d    N/A -1  -1  8   { o2=http://example.org/b, s=http://example.org/a, o1="Alan" }
+     * SOLUTION:   7b4c0689-2141-4523-8eae-e46338f8953d    N/A -1  -1  8   { o2=http://example.org/b, s=http://example.org/b, o1="bob@example.org" }
+     * SOLUTION:   7b4c0689-2141-4523-8eae-e46338f8953d    N/A -1  -1  8   { o2=http://example.org/b, s=http://example.org/b, o1="Bob" }
+     * SOLUTION:   7b4c0689-2141-4523-8eae-e46338f8953d    N/A -1  -1  8   { o2=http://example.org/b, s=http://example.org/c, o1="alice@example.org" }
+     * SOLUTION:   7b4c0689-2141-4523-8eae-e46338f8953d    N/A -1  -1  8   { o2=http://example.org/b, s=http://example.org/c, o1="Alice" }
+     * ============ test4 =======================
+     * Unexpected bindings: 
+     * [o2=http://example.org/b;s=http://example.org/b;o1="bob@example.org"]
+     * [o2=http://example.org/b;s=http://example.org/b;o1="Bob"]
+     * </pre>
+     * 
+     * And the result when run as a named graph query:
+     * 
+     * <pre>
+     * SOLUTION:   1fe3bae3-10d0-481c-bb20-182b0f80813a    N/A -1  -1  6   { o2=http://example.org/b, s=http://example.org/a, o1="alan@example.org" }
+     * SOLUTION:   1fe3bae3-10d0-481c-bb20-182b0f80813a    N/A -1  -1  6   { o2=http://example.org/b, s=http://example.org/a, o1="Alan" }
+     * SOLUTION:   1fe3bae3-10d0-481c-bb20-182b0f80813a    N/A -1  -1  6   { o2=http://example.org/b, s=http://example.org/b, o1="bob@example.org" }
+     * SOLUTION:   1fe3bae3-10d0-481c-bb20-182b0f80813a    N/A -1  -1  6   { o2=http://example.org/b, s=http://example.org/b, o1="Bob" }
+     * SOLUTION:   1fe3bae3-10d0-481c-bb20-182b0f80813a    N/A -1  -1  6   { o2=http://example.org/b, s=http://example.org/c, o1="alice@example.org" }
+     * SOLUTION:   1fe3bae3-10d0-481c-bb20-182b0f80813a    N/A -1  -1  6   { o2=http://example.org/b, s=http://example.org/c, o1="Alice" }
+     * ===================================
+     * Unexpected bindings: 
+     * [o2=http://example.org/b;s=http://example.org/b;o1="bob@example.org"]
+     * [o2=http://example.org/b;s=http://example.org/b;o1="Bob"]
+     * </pre>
+     * 
+     * Thus, the query has the same behavior when run locally and run against a
+     * remote graph (actually, it produces duplicate solutions for "alan" when
+     * run locally which appears to be because the SERVICE call actually does
+     * one more hash join).
+     * <p>
+     * I suspect that the underlying problem has to do with bottom up
+     * evaluation.
+     * <p>
+     * I have modified the expected result to include the solutions for "bob"
+     * until someone can justify why they should be pruned in a manner which
+     * exposes the underlying "bottom up" evaluation semantics issue.
+     */
+    public void test_sparql11_bindings_04() throws Exception {
+
+        new TestHelper("sparql11-bindings-04").runTest();
+        
+    }
+    
 }
