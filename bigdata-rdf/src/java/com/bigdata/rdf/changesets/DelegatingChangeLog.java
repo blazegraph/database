63f4049b225540f4c9642a42bdbe5f9a129df139
mrpersonick <mrpersonick@users.sourceforge.net>
2012-03-26 18:16:45 +0000

    added a delegating change log that implements a listener pattern
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_1_0@6185 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata-rdf/src/java/com/bigdata/rdf/changesets/DelegatingChangeLog.java
@@ -0,0 +1,75 @@
+package com.bigdata.rdf.changesets;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+/**
+ * This delegating change log allows change events to be propogated to multiple
+ * delegates through a listener pattern.
+ * 
+ * @author mike
+ *
+ */
+public class DelegatingChangeLog implements IChangeLog {
+
+    protected static final Logger log = Logger.getLogger(DelegatingChangeLog.class);
+
+    private Set<IChangeLog> delegates;
+    
+    public DelegatingChangeLog() {
+    	this.delegates = new LinkedHashSet<IChangeLog>();
+    }
+    
+    public synchronized void addDelegate(final IChangeLog delegate) {
+    	this.delegates.add(delegate);
+    }
+    
+    public synchronized void removeDelegate(final IChangeLog delegate) {
+    	this.delegates.remove(delegate);
+    }
+    
+    /**
+     * See {@link IChangeLog#changeEvent(IChangeRecord)}.
+     */
+    public synchronized void changeEvent(final IChangeRecord record) {
+        
+        if (log.isInfoEnabled()) 
+            log.info(record);
+        
+        for (IChangeLog delegate : delegates) {
+        	delegate.changeEvent(record);
+        }
+        
+    }
+    
+    /**
+     * See {@link IChangeLog#transactionCommited()}.
+     */
+    public synchronized void transactionCommited() {
+    
+        if (log.isInfoEnabled()) 
+            log.info("transaction committed");
+        
+        for (IChangeLog delegate : delegates) {
+        	delegate.transactionCommited();
+        }
+        
+    }
+    
+    /**
+     * See {@link IChangeLog#transactionAborted()}.
+     */
+    public synchronized void transactionAborted() {
+
+        if (log.isInfoEnabled()) 
+            log.info("transaction aborted");
+        
+        for (IChangeLog delegate : delegates) {
+        	delegate.transactionAborted();
+        }
+        
+    }
+    
+}
