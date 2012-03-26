blazegraph/database

commit 71d8af07adf0f22220cbdbac7230b8adac1dae12

ticket #518 (Review function bop subclass hierarchy and optimize datatype bop and others)
git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_1_0@6181 8f7bc0f5-282e-0410-95e3-8d296e9bb460

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/BNodeBOp.java
@@ -25,6 +25,9 @@ package com.bigdata.rdf.internal.constraints;
 
 import java.util.Map;
 
+import org.openrdf.model.Literal;
+import org.openrdf.model.URI;
+
 import com.bigdata.bop.BOp;
 import com.bigdata.bop.IBindingSet;
 import com.bigdata.bop.IValueExpression;
@@ -44,7 +47,7 @@ import com.bigdata.rdf.sparql.ast.DummyConstantNode;
  * @author <a href="mailto:mrpersonick@users.sourceforge.net">Mike Personick</a>
  * @version $Id$
  */
-public class BNodeBOp extends AbstractLiteralBOp<IV> {
+public class BNodeBOp extends IVValueExpression<IV> implements INeedsMaterialization {
 
     private static final long serialVersionUID = -8448763718374010166L;
 
@@ -70,26 +73,17 @@ public class BNodeBOp extends AbstractLiteralBOp<IV> {
         return Requirement.SOMETIMES;
     }
 
-    public IV _get(final IBindingSet bs) throws SparqlTypeErrorException {
+    public IV get(final IBindingSet bs) throws SparqlTypeErrorException {
         
     	if (arity() == 0) {
     		
-    		return DummyConstantNode.toDummyIV(getValueFactory().createBNode());
+    		return super.createIV(getValueFactory().createBNode(), bs);
     		
     	}
     	
-    	@SuppressWarnings("rawtypes")
-        final IV iv = get(0).get(bs);
-    	
-        if (iv == null)
-            throw new SparqlTypeErrorException.UnboundVarException();
+        final Literal lit = literalValue(0, bs);
 
-        if (!iv.isLiteral())
-            throw new SparqlTypeErrorException();
-
-        final BigdataLiteral lit = literalValue(iv);
-
-        final BigdataURI dt = lit.getDatatype();
+        final URI dt = lit.getDatatype();
 
         if (dt != null && !dt.stringValue().equals(XSD.STRING.stringValue()))
             throw new SparqlTypeErrorException();
@@ -97,7 +91,7 @@ public class BNodeBOp extends AbstractLiteralBOp<IV> {
         final BigdataBNode bnode = getValueFactory().createBNode(
                 "-bnode-func-" + lit.getLabel());
 
-        return DummyConstantNode.toDummyIV(bnode);
+        return super.createIV(bnode, bs);
 
     }
     

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/ConcatBOp.java
@@ -25,6 +25,7 @@ package com.bigdata.rdf.internal.constraints;
 
 import java.util.Map;
 
+import org.openrdf.model.Literal;
 import org.openrdf.model.URI;
 
 import com.bigdata.bop.BOp;
@@ -32,10 +33,11 @@ import com.bigdata.bop.IBindingSet;
 import com.bigdata.bop.IValueExpression;
 import com.bigdata.bop.NV;
 import com.bigdata.rdf.internal.IV;
+import com.bigdata.rdf.internal.constraints.INeedsMaterialization.Requirement;
 import com.bigdata.rdf.model.BigdataLiteral;
 import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
-public class ConcatBOp extends AbstractLiteralBOp<IV> {
+public class ConcatBOp extends IVValueExpression<IV> implements INeedsMaterialization {
 
     private static final long serialVersionUID = 5894411703430694650L;
 
@@ -63,18 +65,23 @@ public class ConcatBOp extends AbstractLiteralBOp<IV> {
         super(op);
     }
 
+	@Override
+	public Requirement getRequirement() {
+		return Requirement.SOMETIMES;
+	}
+
     @Override
-    public IV _get(final IBindingSet bs) {
+    public IV get(final IBindingSet bs) {
         URI datatype = null;
         String lang = null;
         boolean allSame = true;
         final StringBuilder sb = new StringBuilder();
         for (int i = 0; i < arity(); i++) {
             @SuppressWarnings("rawtypes")
-            final IV v = getAndCheckIfMaterializedLiteral(i, bs);
+            final IV v = getAndCheckLiteral(i, bs);
             String label = null;
             if (allSame) {
-                final BigdataLiteral lit = literalValue(v);
+                final Literal lit = literalValue(v);
                 label = lit.getLabel();
                 if (lit.getDatatype() != null) {
                     if (lang != null) {
@@ -110,12 +117,12 @@ public class ConcatBOp extends AbstractLiteralBOp<IV> {
         }
         if (allSame) {
             if (datatype != null) {
-                return DummyConstantNode.toDummyIV(getValueFactory().createLiteral(sb.toString(),datatype));
+                return super.createIV(getValueFactory().createLiteral(sb.toString(),datatype), bs);
             } else if (lang != null) {
-                return DummyConstantNode.toDummyIV(getValueFactory().createLiteral(sb.toString(),lang));
+                return super.createIV(getValueFactory().createLiteral(sb.toString(),lang), bs);
             }
         }
-        return DummyConstantNode.toDummyIV(getValueFactory().createLiteral(sb.toString()));
+        return super.createIV(getValueFactory().createLiteral(sb.toString()), bs);
 
     }
 

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/DatatypeBOp.java
@@ -33,6 +33,7 @@ import com.bigdata.bop.IBindingSet;
 import com.bigdata.bop.IValueExpression;
 import com.bigdata.bop.NV;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
+import com.bigdata.rdf.internal.ILexiconConfiguration;
 import com.bigdata.rdf.internal.IV;
 import com.bigdata.rdf.internal.NotMaterializedException;
 import com.bigdata.rdf.internal.VTE;
@@ -43,6 +44,7 @@ import com.bigdata.rdf.model.BigdataURI;
 import com.bigdata.rdf.model.BigdataValue;
 import com.bigdata.rdf.model.BigdataValueFactory;
 import com.bigdata.rdf.model.BigdataValueFactoryImpl;
+import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 /**
  * Return the datatype of the literal argument.
@@ -118,17 +120,13 @@ public class DatatypeBOp extends IVValueExpression<IV>
 
 	    	@SuppressWarnings("rawtypes")
             IV datatypeIV = datatype.getIV();
-
 	    	if (datatypeIV == null) {
-
-                datatypeIV = TermId.mockIV(VTE.URI);
-	    		datatype.setIV(datatypeIV);
-
+	    		datatypeIV = super.createIV(datatype, bs);
 	    	}
 
 	    	// cache the value on the IV
 	    	datatypeIV.setValue(datatype);
-
+	    	
 	    	return datatypeIV;
 
         }
@@ -162,13 +160,8 @@ public class DatatypeBOp extends IVValueExpression<IV>
 
             @SuppressWarnings("rawtypes")
             IV datatypeIV = datatype.getIV();
-
 	    	if (datatypeIV == null) {
-
-	    	    datatypeIV = TermId.mockIV(VTE.URI);
-
-	    	    datatype.setIV(datatypeIV);
-
+	    		datatypeIV = super.createIV(datatype, bs);
 	    	}
 
 	    	// cache the value on the IV

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/DigestBOp.java
@@ -26,6 +26,8 @@ package com.bigdata.rdf.internal.constraints;
 import java.security.MessageDigest;
 import java.util.Map;
 
+import org.openrdf.model.Literal;
+
 import com.bigdata.bop.BOp;
 import com.bigdata.bop.IBindingSet;
 import com.bigdata.bop.IValueExpression;
@@ -33,6 +35,7 @@ import com.bigdata.bop.NV;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
 import com.bigdata.rdf.internal.XSD;
+import com.bigdata.rdf.internal.constraints.INeedsMaterialization.Requirement;
 import com.bigdata.rdf.model.BigdataLiteral;
 import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
@@ -41,11 +44,11 @@ import com.bigdata.rdf.sparql.ast.DummyConstantNode;
  * operation to be applied to the operands is specified by the
  * {@link DigestBOp.Annotations#OP} annotation.
  */
-public class DigestBOp extends AbstractLiteralBOp<IV> {
+public class DigestBOp extends IVValueExpression<IV> implements INeedsMaterialization {
 
     private static final long serialVersionUID = 9136864442064392445L;
 
-    public interface Annotations extends AbstractLiteralBOp.Annotations {
+    public interface Annotations extends IVValueExpression.Annotations {
 
         /**
          * The operation to be applied to the left operand (required). The value
@@ -133,18 +136,18 @@ public class DigestBOp extends AbstractLiteralBOp<IV> {
         
     }
 
-    public Requirement getRequirement() {
+	@Override
+	public Requirement getRequirement() {
+		return Requirement.SOMETIMES;
+	}
 
-        return Requirement.SOMETIMES;
-        
-    }
+	@Override
+    public IV get(final IBindingSet bs) throws SparqlTypeErrorException {
 
-    public IV _get(final IBindingSet bs) throws SparqlTypeErrorException {
-
-        final IV iv = getAndCheckIfMaterializedLiteral(0, bs);
+        final IV iv = getAndCheckLiteral(0, bs);
         //Recreate since they are not thread safe
         MessageDigest md = null;
-        final BigdataLiteral lit = literalValue(iv);
+        final Literal lit = literalValue(iv);
         if (lit.getLanguage() != null || lit.getDatatype() != null
                 && lit.getDatatype().equals(XSD.STRING)) {
             try {
@@ -175,7 +178,7 @@ public class DigestBOp extends AbstractLiteralBOp<IV> {
                 md.update(bytes);
                 byte[] digest = md.digest();
                 final BigdataLiteral str = getValueFactory().createLiteral(toHexString(digest));
-                return DummyConstantNode.toDummyIV(str);
+                return super.createIV(str, bs);
             } catch (Exception e) {
                 throw new SparqlTypeErrorException();
             }

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/EncodeForURIBOp.java
@@ -27,17 +27,17 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.Map;
 
+import org.openrdf.model.Literal;
+
 import com.bigdata.bop.BOp;
 import com.bigdata.bop.IBindingSet;
 import com.bigdata.bop.IValueExpression;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
-import com.bigdata.rdf.internal.NotMaterializedException;
 import com.bigdata.rdf.model.BigdataLiteral;
-import com.bigdata.rdf.model.BigdataValueFactory;
 import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
-public class EncodeForURIBOp extends AbstractLiteralBOp {
+public class EncodeForURIBOp extends IVValueExpression<IV> implements INeedsMaterialization {
 
     private static final long serialVersionUID = -8448763718374010166L;
 
@@ -55,16 +55,17 @@ public class EncodeForURIBOp extends AbstractLiteralBOp {
         super(op);
     }
 
-    public Requirement getRequirement() {
-        return Requirement.SOMETIMES;
-    }
+	@Override
+	public Requirement getRequirement() {
+		return Requirement.SOMETIMES;
+	}
 
-    public IV _get(final IBindingSet bs) throws SparqlTypeErrorException {
-        IV iv = getAndCheckIfMaterializedLiteral(0, bs);
-        final BigdataLiteral lit =literalValue(iv);
+	@Override
+    public IV get(final IBindingSet bs) throws SparqlTypeErrorException {
+        final Literal lit = literalValue(0, bs);
         try {
             final BigdataLiteral str = getValueFactory().createLiteral(URLEncoder.encode(lit.getLabel(), "UTF-8").replace("+", "%20"));
-            return DummyConstantNode.toDummyIV(str);
+            return super.createIV(str, bs);
         } catch (UnsupportedEncodingException uee) {
             throw new SparqlTypeErrorException();
         }

commit 6e5cd0666ebaef3ebaf9913f036f6387b9c45c2a

Bug fix to refactor of the IVValueExpressions.  There were several classes which had their own Annotation interfaces and still declared a NAMESPACE annotation which was distinct from the NAMESPACE annotation on the IVValueExpresion class.  BigdataSparqlTest is now green as is the TestASK class.  Both test suites were broken in r6181.
git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_1_0@6184 8f7bc0f5-282e-0410-95e3-8d296e9bb460

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/BNodeBOp.java
@@ -35,9 +35,6 @@ import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
 import com.bigdata.rdf.internal.XSD;
 import com.bigdata.rdf.model.BigdataBNode;
-import com.bigdata.rdf.model.BigdataLiteral;
-import com.bigdata.rdf.model.BigdataURI;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 /**
  * The BNODE()/BNODE(Literal) function as defined in <a

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/ConcatBOp.java
@@ -33,9 +33,6 @@ import com.bigdata.bop.IBindingSet;
 import com.bigdata.bop.IValueExpression;
 import com.bigdata.bop.NV;
 import com.bigdata.rdf.internal.IV;
-import com.bigdata.rdf.internal.constraints.INeedsMaterialization.Requirement;
-import com.bigdata.rdf.model.BigdataLiteral;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 public class ConcatBOp extends IVValueExpression<IV> implements INeedsMaterialization {
 

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/DatatypeBOp.java
@@ -33,18 +33,14 @@ import com.bigdata.bop.IBindingSet;
 import com.bigdata.bop.IValueExpression;
 import com.bigdata.bop.NV;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
-import com.bigdata.rdf.internal.ILexiconConfiguration;
 import com.bigdata.rdf.internal.IV;
 import com.bigdata.rdf.internal.NotMaterializedException;
-import com.bigdata.rdf.internal.VTE;
 import com.bigdata.rdf.internal.XSD;
-import com.bigdata.rdf.internal.impl.TermId;
 import com.bigdata.rdf.model.BigdataLiteral;
 import com.bigdata.rdf.model.BigdataURI;
 import com.bigdata.rdf.model.BigdataValue;
 import com.bigdata.rdf.model.BigdataValueFactory;
 import com.bigdata.rdf.model.BigdataValueFactoryImpl;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 /**
  * Return the datatype of the literal argument.
@@ -59,11 +55,11 @@ public class DatatypeBOp extends IVValueExpression<IV>
 
 	private static final transient Logger log = Logger.getLogger(DatatypeBOp.class);
 
-	public interface Annotations extends BOp.Annotations {
-
-		String NAMESPACE = DatatypeBOp.class.getName() + ".namespace";
-
-    }
+//	public interface Annotations extends BOp.Annotations {
+//
+//		String NAMESPACE = DatatypeBOp.class.getName() + ".namespace";
+//
+//    }
 
     public DatatypeBOp(final IValueExpression<? extends IV> x, final String lex) {
 

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/DigestBOp.java
@@ -35,9 +35,7 @@ import com.bigdata.bop.NV;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
 import com.bigdata.rdf.internal.XSD;
-import com.bigdata.rdf.internal.constraints.INeedsMaterialization.Requirement;
 import com.bigdata.rdf.model.BigdataLiteral;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 /**
  * A Digest expression involving a {@link IValueExpression} operand. The

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/EncodeForURIBOp.java
@@ -35,7 +35,6 @@ import com.bigdata.bop.IValueExpression;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
 import com.bigdata.rdf.model.BigdataLiteral;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 public class EncodeForURIBOp extends IVValueExpression<IV> implements INeedsMaterialization {
 

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/FuncBOp.java
@@ -42,7 +42,6 @@ import com.bigdata.rdf.internal.VTE;
 import com.bigdata.rdf.internal.impl.TermId;
 import com.bigdata.rdf.model.BigdataValue;
 import com.bigdata.rdf.model.BigdataValueFactory;
-import com.bigdata.rdf.model.BigdataValueFactoryImpl;
 
 /**
  * Call one of the Sesame casting functions.
@@ -57,9 +56,9 @@ public class FuncBOp extends IVValueExpression<IV> implements
 	
 //	private static final transient Logger log = Logger.getLogger(FuncBOp.class);
 
-	public interface Annotations extends BOp.Annotations {
+	public interface Annotations extends IVValueExpression.Annotations {
 
-		String NAMESPACE = FuncBOp.class.getName() + ".namespace";
+//		String NAMESPACE = FuncBOp.class.getName() + ".namespace";
 		
 		String FUNCTION = FuncBOp.class.getName() + ".function";
 

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/IriBOp.java
@@ -34,10 +34,7 @@ import com.bigdata.bop.IValueExpression;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
 import com.bigdata.rdf.internal.XSD;
-import com.bigdata.rdf.internal.constraints.INeedsMaterialization.Requirement;
-import com.bigdata.rdf.model.BigdataLiteral;
 import com.bigdata.rdf.model.BigdataURI;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 /**
  * The IRI function, as defined in <a

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/IsMaterializedBOp.java
@@ -32,9 +32,9 @@ import com.bigdata.bop.BOp;
 import com.bigdata.bop.IBindingSet;
 import com.bigdata.bop.IVariable;
 import com.bigdata.bop.NV;
-import com.bigdata.bop.PipelineOp;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
+import com.bigdata.rdf.model.BigdataValue;
 
 /**
  * Imposes the constraint <code>isMaterialized(x)</code>.

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/LangBOp.java
@@ -24,16 +24,13 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 package com.bigdata.rdf.internal.constraints;
 
-import java.util.LinkedHashSet;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.log4j.Logger;
 
 import com.bigdata.bop.BOp;
 import com.bigdata.bop.IBindingSet;
 import com.bigdata.bop.IValueExpression;
-import com.bigdata.bop.IVariable;
 import com.bigdata.bop.NV;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
@@ -58,11 +55,11 @@ public class LangBOp extends IVValueExpression<IV>
 	
 	private static final transient Logger log = Logger.getLogger(LangBOp.class);
 
-	public interface Annotations extends BOp.Annotations {
-
-		String NAMESPACE = (LangBOp.class.getName() + ".namespace").intern();
-
-    }
+//	public interface Annotations extends BOp.Annotations {
+//
+//		String NAMESPACE = (LangBOp.class.getName() + ".namespace").intern();
+//
+//    }
 	
     public LangBOp(final IValueExpression<? extends IV> x, final String lex) {
         

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/LcaseBOp.java
@@ -32,10 +32,8 @@ import com.bigdata.bop.BOp;
 import com.bigdata.bop.IBindingSet;
 import com.bigdata.bop.IValueExpression;
 import com.bigdata.rdf.internal.IV;
-import com.bigdata.rdf.internal.constraints.INeedsMaterialization.Requirement;
 import com.bigdata.rdf.model.BigdataLiteral;
 import com.bigdata.rdf.model.BigdataValueFactory;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 /**
  * <pre>http://www.w3.org/2005/xpath-functions#lower-case</pre>

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/MathBOp.java
@@ -40,7 +40,6 @@ import org.openrdf.model.Literal;
 import com.bigdata.bop.BOp;
 import com.bigdata.bop.IBindingSet;
 import com.bigdata.bop.IValueExpression;
-import com.bigdata.bop.ImmutableBOp;
 import com.bigdata.bop.NV;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.ILexiconConfiguration;
@@ -67,7 +66,7 @@ final public class MathBOp extends IVValueExpression
 	
 	private transient BigdataValueFactory vf;
     
-    public interface Annotations extends ImmutableBOp.Annotations {
+    public interface Annotations extends IVValueExpression.Annotations {
 
         /**
          * The operation to be applied to the left and right operands
@@ -78,7 +77,7 @@ final public class MathBOp extends IVValueExpression
          */
         String OP = (MathBOp.class.getName() + ".op").intern();
 
-        public String NAMESPACE = (MathBOp.class.getName() + ".namespace").intern();
+//        public String NAMESPACE = (MathBOp.class.getName() + ".namespace").intern();
     }
     
 	public enum MathOp {
@@ -116,7 +115,8 @@ final public class MathBOp extends IVValueExpression
     public MathBOp(final IValueExpression<? extends IV> left, 
     		final IValueExpression<? extends IV> right, final MathOp op,final String lex) {
 
-        this(new BOp[] { left, right }, NV.asMap(new NV(Annotations.OP, op),new NV(Annotations.NAMESPACE, lex)));
+        this(new BOp[] { left, right }, NV.asMap(new NV(Annotations.OP, op),
+                new NV(Annotations.NAMESPACE, lex)));
 
     }
 
@@ -128,9 +128,9 @@ final public class MathBOp extends IVValueExpression
 	 * @param op
 	 *            The operation.
 	 */
-    public MathBOp(final BOp[] args, Map<String,Object> anns) {
+    public MathBOp(final BOp[] args, final Map<String, Object> anns) {
     
-        super(args,anns);
+        super(args, anns);
 
 		if (args.length != 2 || args[0] == null || args[1] == null
 				|| getProperty(Annotations.OP) == null

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/RandBOp.java
@@ -33,25 +33,39 @@ import com.bigdata.rdf.internal.IV;
 import com.bigdata.rdf.internal.impl.literal.XSDNumericIV;
 
 public class RandBOp extends IVValueExpression<IV> {
-    Random rand = new Random();
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    final private Random rand = new Random();
 
     public RandBOp() {
+
         this(new BOp[] {}, null);
+        
     }
 
-    public RandBOp(BOp[] args, Map<String, Object> anns) {
+    public RandBOp(final BOp[] args, final Map<String, Object> anns) {
+
         super(args, anns);
+
         if (args.length != 0)
             throw new IllegalArgumentException();
 
     }
 
-    public RandBOp(RandBOp op) {
+    public RandBOp(final RandBOp op) {
+        
         super(op);
+        
     }
 
     public IV get(IBindingSet bindingSet) {
+
         return new XSDNumericIV(rand.nextDouble());
+        
     }
 
 }

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/ReplaceBOp.java
@@ -41,11 +41,8 @@ import com.bigdata.bop.IValueExpression;
 import com.bigdata.bop.NV;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
-import com.bigdata.rdf.internal.constraints.INeedsMaterialization.Requirement;
-import com.bigdata.rdf.internal.constraints.RegexBOp.Annotations;
 import com.bigdata.rdf.model.BigdataLiteral;
 import com.bigdata.rdf.model.BigdataValueFactory;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 /**
  * @see http://www.w3.org/2009/sparql/docs/query-1.1/rq25.xml#func-replace

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/StrAfterBOp.java
@@ -36,10 +36,7 @@ import com.bigdata.bop.NV;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
 import com.bigdata.rdf.internal.XSD;
-import com.bigdata.rdf.internal.constraints.INeedsMaterialization.Requirement;
-import com.bigdata.rdf.internal.impl.literal.AbstractLiteralIV;
 import com.bigdata.rdf.model.BigdataLiteral;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 /**
  * @see http://www.w3.org/2009/sparql/docs/query-1.1/rq25.xml#func-strafter

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/StrBOp.java
@@ -24,9 +24,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 package com.bigdata.rdf.internal.constraints;
 
-import java.util.LinkedHashSet;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.openrdf.model.URI;
@@ -34,16 +32,13 @@ import org.openrdf.model.URI;
 import com.bigdata.bop.BOp;
 import com.bigdata.bop.IBindingSet;
 import com.bigdata.bop.IValueExpression;
-import com.bigdata.bop.IVariable;
 import com.bigdata.bop.NV;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
-import com.bigdata.rdf.internal.impl.AbstractInlineIV;
 import com.bigdata.rdf.internal.impl.literal.AbstractLiteralIV;
 import com.bigdata.rdf.model.BigdataLiteral;
 import com.bigdata.rdf.model.BigdataValueFactory;
 import com.bigdata.rdf.model.BigdataValueFactoryImpl;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 /**
  * Convert the {@link IV} to a <code>xsd:string</code>.
@@ -58,11 +53,11 @@ public class StrBOp extends IVValueExpression<IV>
 
     private static final transient Logger log = Logger.getLogger(StrBOp.class);
 
-    public interface Annotations extends BOp.Annotations {
-
-        String NAMESPACE = StrBOp.class.getName() + ".namespace";
-
-    }
+//    public interface Annotations extends BOp.Annotations {
+//
+//        String NAMESPACE = StrBOp.class.getName() + ".namespace";
+//
+//    }
 
     public StrBOp(final IValueExpression<? extends IV> x, final String lex) {
 

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/StrBeforeBOp.java
@@ -36,10 +36,7 @@ import com.bigdata.bop.NV;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
 import com.bigdata.rdf.internal.XSD;
-import com.bigdata.rdf.internal.constraints.INeedsMaterialization.Requirement;
-import com.bigdata.rdf.internal.impl.literal.AbstractLiteralIV;
 import com.bigdata.rdf.model.BigdataLiteral;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 /**
  * @see http://www.w3.org/2009/sparql/docs/query-1.1/rq25.xml#func-strbefore

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/StrdtBOp.java
@@ -34,10 +34,8 @@ import com.bigdata.bop.NV;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
 import com.bigdata.rdf.internal.NotMaterializedException;
-import com.bigdata.rdf.internal.constraints.INeedsMaterialization.Requirement;
 import com.bigdata.rdf.model.BigdataLiteral;
 import com.bigdata.rdf.model.BigdataURI;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 public class StrdtBOp extends IVValueExpression<IV> implements INeedsMaterialization {
 

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/StrlangBOp.java
@@ -33,12 +33,7 @@ import com.bigdata.bop.IValueExpression;
 import com.bigdata.bop.NV;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
-import com.bigdata.rdf.internal.NotMaterializedException;
-import com.bigdata.rdf.internal.constraints.INeedsMaterialization.Requirement;
 import com.bigdata.rdf.model.BigdataLiteral;
-import com.bigdata.rdf.model.BigdataValue;
-import com.bigdata.rdf.model.BigdataValueFactory;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 public class StrlangBOp extends IVValueExpression<IV> implements INeedsMaterialization {
 

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/StrlenBOp.java
@@ -33,9 +33,7 @@ import com.bigdata.bop.IBindingSet;
 import com.bigdata.bop.IValueExpression;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
-import com.bigdata.rdf.internal.constraints.INeedsMaterialization.Requirement;
 import com.bigdata.rdf.internal.impl.literal.XSDIntegerIV;
-import com.bigdata.rdf.model.BigdataLiteral;
 
 public class StrlenBOp extends IVValueExpression<IV> implements INeedsMaterialization {
 

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/StrstartsBOp.java
@@ -31,8 +31,6 @@ import com.bigdata.bop.IValueExpression;
 import com.bigdata.bop.NV;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
-import com.bigdata.rdf.internal.NotMaterializedException;
-import com.bigdata.rdf.model.BigdataValueFactory;
 
 public class StrstartsBOp extends XSDBooleanIVValueExpression implements INeedsMaterialization {
 

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/SubstrBOp.java
@@ -35,10 +35,7 @@ import com.bigdata.bop.IValueExpression;
 import com.bigdata.bop.NV;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
-import com.bigdata.rdf.internal.constraints.INeedsMaterialization.Requirement;
 import com.bigdata.rdf.model.BigdataLiteral;
-import com.bigdata.rdf.model.BigdataURI;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 /**
  * This function has an origin of ONE (1) not ZERO (0). The start and offset

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/UcaseBOp.java
@@ -32,11 +32,7 @@ import com.bigdata.bop.IBindingSet;
 import com.bigdata.bop.IValueExpression;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 import com.bigdata.rdf.internal.IV;
-import com.bigdata.rdf.internal.NotMaterializedException;
-import com.bigdata.rdf.internal.constraints.INeedsMaterialization.Requirement;
 import com.bigdata.rdf.model.BigdataLiteral;
-import com.bigdata.rdf.model.BigdataValueFactory;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 public class UcaseBOp extends IVValueExpression<IV> implements INeedsMaterialization {
 

bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/XsdStrBOp.java
@@ -40,7 +40,6 @@ import com.bigdata.rdf.internal.impl.literal.AbstractLiteralIV;
 import com.bigdata.rdf.model.BigdataLiteral;
 import com.bigdata.rdf.model.BigdataValueFactory;
 import com.bigdata.rdf.model.BigdataValueFactoryImpl;
-import com.bigdata.rdf.sparql.ast.DummyConstantNode;
 
 /**
  * Convert the {@link IV} to a <code>xsd:string</code>.
@@ -55,11 +54,11 @@ public class XsdStrBOp extends IVValueExpression<IV>
 
     private static final transient Logger log = Logger.getLogger(XsdStrBOp.class);
 
-    public interface Annotations extends BOp.Annotations {
-
-        String NAMESPACE = XsdStrBOp.class.getName() + ".namespace";
-
-    }
+//    public interface Annotations extends BOp.Annotations {
+//
+//        String NAMESPACE = XsdStrBOp.class.getName() + ".namespace";
+//
+//    }
 
     public XsdStrBOp(final IValueExpression<? extends IV> x, final String lex) {
 