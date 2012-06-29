334ec4ce23d6428d4109197a7d77e3febd396143
thompsonbry <thompsonbry@users.sourceforge.net>
2012-06-29 13:00:40 +0000

    Tne problem here was that the URI was NOT known to the dictionary.  When testing for the existence of a named graph, an unknown URI is proof of non-existence.  However, it was failing to check for a null IV and thus this turned into a test for whether any statement existed in any named graph (all unbound existence test).
    I refactored the sparql update exceptions from RuntimeException to extend SparqlDynamicErrorException. I also added a set of parameters to the exception family to we can communicate things like the graphUri in question back to the caller.  I have reviewed the class which executes the SPARQL update operations and substituted the SparqlDynamicErrorExceptions throughout in place of existing RuntimeExceptions.
    
    @see https://sourceforge.net/apps/trac/bigdata/ticket/569 (LOAD-CREATE-LOAD using virgin journal fails with "Graph exists" exception)
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@6371 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-rdf/src/java/com/bigdata/rdf/error/SparqlDynamicErrorException.java
+++ bigdata-rdf/src/java/com/bigdata/rdf/error/SparqlDynamicErrorException.java
@@ -27,9 +27,12 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package com.bigdata.rdf.error;
 
+import org.openrdf.model.URI;
+
+import com.bigdata.util.NV;
 
 /**
- * A SPARQL static analysis error.
+ * A SPARQL dynamic (runtime) error.
  * 
  * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan Thompson</a>
  * @version $Id$
@@ -42,19 +45,192 @@ public class SparqlDynamicErrorException extends W3CQueryLanguageException {
     private static final long serialVersionUID = 1L;
 
     /**
-     * @param errorCode
-     *            The four digit error code.
+     * Generic type error.
      */
-    public SparqlDynamicErrorException(int errorCode) {
-     
-        super(LanguageFamily.SP, ErrorCategory.DY, errorCode, null/* msg */);
-        
+    static public String SPARQL_DYNAMIC_ERROR_0000 = toURI(0);
+
+    /**
+     * Error thrown when a graph exists in a context in which it is presumed
+     * that it should not pre-exist.
+     * 
+     * @see GraphExistsException
+     */
+    static public String SPARQL_DYNAMIC_ERROR_0001 = toURI(1);
+
+    /**
+     * Error thrown when a graph is empty in a context in which it is presumed
+     * that it should not be empty.
+     * 
+     * @see GraphEmptyException
+     */
+    static public String SPARQL_DYNAMIC_ERROR_0002 = toURI(2);
+
+    /**
+     * Error thrown when a named solution set exists in a context in which it is
+     * presumed that it should not pre-exist.
+     * 
+     * @see SolutionSetExistsException
+     */
+    static public String SPARQL_DYNAMIC_ERROR_0003 = toURI(3);
+
+    /**
+     * Error thrown when a named solution set does not exist in a context in
+     * which it is presumed that it should exist.
+     * 
+     * @see SolutionSetDoesNotExistException
+     */
+    static public String SPARQL_DYNAMIC_ERROR_0004 = toURI(4);
+
+    /**
+     * Error thrown when the Content-Type is not recognized or can not be
+     * handled.
+     * 
+     * @see UnknownContentTypeException
+     */
+    static public String SPARQL_DYNAMIC_ERROR_0005 = toURI(5);
+
+    /**
+     * Generic SPARQL dynamic error.
+     * 
+     * @see #SPARQL_DYNAMIC_ERROR_0000
+     */
+    public SparqlDynamicErrorException() {
+
+        this(0/* errorCode */, SPARQL_DYNAMIC_ERROR_0000);
+
     }
 
-    static protected String toURI(int errorCode) {
+    /**
+     * @param errorCode
+     *            The four digit error code.
+     * @param uri
+     *            The uri of the error.
+     */
+    protected SparqlDynamicErrorException(final int errorCode, final String uri) {
+
+        super(LanguageFamily.SP, ErrorCategory.DY, errorCode, uri);
+
+    }
+    
+    /**
+     * @param errorCode
+     *            The four digit error code.
+     * @param params
+     *            Addition parameters for the error (optional).
+     */
+    protected SparqlDynamicErrorException(final int errorCode, final NV[] params) {
+
+        super(LanguageFamily.SP, ErrorCategory.DY, errorCode, params);
+
+    }
+
+    /**
+     * @param errorCode
+     *            The four digit error code.
+     * @param uri
+     *            The uri of the error.
+     * @param params
+     *            Additional parameters for the error message (optional and may
+     *            be <code>null</code>). When non-<code>null</code> and
+     *            non-empty, the parameters are appended to the constructed URI
+     *            as query parameters.
+     */
+    protected SparqlDynamicErrorException(final int errorCode,
+            final String uri, final NV[] params) {
+
+        super(LanguageFamily.SP, ErrorCategory.DY, errorCode, params);
+
+    }
+
+    static protected String toURI(final int errorCode) {
 
         return W3CQueryLanguageException.toURI(LanguageFamily.SP,
-                ErrorCategory.DY, errorCode);
+                ErrorCategory.DY, errorCode, null/* params */);
+
+    }
+
+    /**
+     * Error thrown when a graph exists in a context which presumes that it
+     * should not pre-exist.
+     */
+    static public class GraphExistsException extends SparqlDynamicErrorException {
+
+        private static final long serialVersionUID = 1L;
+
+        public GraphExistsException(final URI graphUri) {
+
+            super(0001/* errorCode */, SPARQL_DYNAMIC_ERROR_0001,
+                    new NV[] { new NV("uri", graphUri.stringValue()) });
+
+        }
+
+    }
+
+    /**
+     * Error thrown when a graph is empty in a context which presumes that it
+     * should not be empty.
+     */
+    static public class GraphEmptyException extends SparqlDynamicErrorException {
+
+        private static final long serialVersionUID = 1L;
+
+        public GraphEmptyException(final URI graphUri) {
+
+            super(0002/* errorCode */, SPARQL_DYNAMIC_ERROR_0002,
+                    new NV[] { new NV("uri", graphUri.stringValue()) });
+
+        }
+
+    }
+
+    /**
+     * Error thrown when a named solution set exists in a context which presumes
+     * that it should not pre-exist.
+     */
+    static public class SolutionSetExistsException extends SparqlDynamicErrorException {
+
+        private static final long serialVersionUID = 1L;
+
+        public SolutionSetExistsException(final String solutionSet) {
+
+            super(0003/* errorCode */, SPARQL_DYNAMIC_ERROR_0003,
+                    new NV[] { new NV("name", solutionSet) });
+
+        }
+
+    }
+
+    /**
+     * Error thrown when an named solution set does not exist in a context which
+     * presumes that it should exist.
+     */
+    static public class SolutionSetDoesNotExistException extends SparqlDynamicErrorException {
+
+        private static final long serialVersionUID = 1L;
+
+        public SolutionSetDoesNotExistException(final String solutionSet) {
+
+            super(0004/* errorCode */, SPARQL_DYNAMIC_ERROR_0004,
+                    new NV[] { new NV("name", solutionSet) });
+
+        }
+
+    }
+
+    /**
+     * Error thrown when the Content-Type is not recognized or can not be
+     * handled.
+     */
+    static public class UnknownContentTypeException extends SparqlDynamicErrorException {
+
+        private static final long serialVersionUID = 1L;
+
+        public UnknownContentTypeException(final String contentType) {
+
+            super(0005/* errorCode */, SPARQL_DYNAMIC_ERROR_0005,
+                    new NV[] { new NV("Content-Type", contentType) });
+
+        }
 
     }
 
