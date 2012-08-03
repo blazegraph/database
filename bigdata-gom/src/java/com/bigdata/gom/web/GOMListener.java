366f6bd2a950b310062694b8e4959e6d231905f9
thompsonbry <thompsonbry@users.sourceforge.net>
2012-08-03 15:06:58 +0000

    exposed something to GOM
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@6407 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-gom/src/java/com/bigdata/gom/web/GOMListener.java
+++ bigdata-gom/src/java/com/bigdata/gom/web/GOMListener.java
@@ -1,26 +1,16 @@
 package com.bigdata.gom.web;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
 import java.util.UUID;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
 
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.sail.SailException;
 
-import com.bigdata.gom.om.IObjectManager;
 import com.bigdata.gom.om.ObjectManager;
-import com.bigdata.rdf.sail.BigdataSail;
-import com.bigdata.rdf.sail.BigdataSailRepository;
-import com.bigdata.rdf.sail.BigdataSailRepositoryConnection;
+import com.bigdata.rdf.sail.webapp.BigdataRDFContext;
 import com.bigdata.rdf.sail.webapp.BigdataRDFServletContextListener;
-import com.bigdata.rdf.sail.webapp.ConfigParams;
 
 /**
  * Extends the BigdataRDFServletContextListener to add a local ObjectManager
@@ -42,6 +32,8 @@ public class GOMListener extends BigdataRDFServletContextListener {
         final ServletContext context = ev.getServletContext();
 
         final UUID uuid = UUID.fromString(context.getInitParameter("om-uuid"));
+        
+        final BigdataRDFContext rdfContext = getBigdataRDFContext();
         
         final String namespace = rdfContext.getConfig().namespace;
         try {
