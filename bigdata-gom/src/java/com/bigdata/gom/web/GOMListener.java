9a6c73ad4436a1aba404a9fd85df47309db642f2
martyncutcher <martyncutcher@users.sourceforge.net>
2012-08-09 11:46:53 +0000

    fix constructor call
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@6424 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-gom/src/java/com/bigdata/gom/web/GOMListener.java
+++ bigdata-gom/src/java/com/bigdata/gom/web/GOMListener.java
@@ -1,16 +1,28 @@
 package com.bigdata.gom.web;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.UUID;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.sail.SailException;
 
+import com.bigdata.gom.om.IObjectManager;
 import com.bigdata.gom.om.ObjectManager;
-import com.bigdata.rdf.sail.webapp.BigdataRDFContext;
+import com.bigdata.journal.ITx;
+import com.bigdata.rdf.sail.BigdataSail;
+import com.bigdata.rdf.sail.BigdataSailRepository;
+import com.bigdata.rdf.sail.BigdataSailRepositoryConnection;
 import com.bigdata.rdf.sail.webapp.BigdataRDFServletContextListener;
+import com.bigdata.rdf.sail.webapp.ConfigParams;
+import com.bigdata.rdf.store.AbstractTripleStore;
 
 /**
  * Extends the BigdataRDFServletContextListener to add a local ObjectManager
@@ -33,14 +45,24 @@ public class GOMListener extends BigdataRDFServletContextListener {
 
         final UUID uuid = UUID.fromString(context.getInitParameter("om-uuid"));
         
-        final BigdataRDFContext rdfContext = getBigdataRDFContext();
-        
         final String namespace = rdfContext.getConfig().namespace;
         try {
-			final ObjectManager om = new ObjectManager(uuid, rdfContext.getUnisolatedConnection(namespace));
+            final AbstractTripleStore tripleStore = (AbstractTripleStore) rdfContext.getIndexManager()
+            .getResourceLocator().locate(namespace, ITx.UNISOLATED);
+
+		    if (tripleStore == null) {
+		        throw new RuntimeException("Not found: namespace=" + namespace);
+		    }
+		
+		    // Wrap with SAIL.
+		    final BigdataSail sail = new BigdataSail(tripleStore);
+		
+		    final BigdataSailRepository repo = new BigdataSailRepository(sail);
+		
+		    repo.initialize();
+		
+		    final ObjectManager om = new ObjectManager(uuid, repo);
 			context.setAttribute(ObjectManager.class.getName(), om);
-		} catch (SailException e) {
-			throw new RuntimeException(e);
 		} catch (RepositoryException e) {
 			throw new RuntimeException(e);
 		}
