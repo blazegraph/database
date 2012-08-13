e41a9f266aa475fde2e1c0cba1c6970f8e3a53cf
thompsonbry <thompsonbry@users.sourceforge.net>
2012-08-13 21:17:39 +0000

    LICENSE
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@6437 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-sails/src/java/com/bigdata/rdf/sail/BigdataSailGraphQuery.java
+++ bigdata-sails/src/java/com/bigdata/rdf/sail/BigdataSailGraphQuery.java
@@ -1,3 +1,25 @@
+/**
+Copyright (C) SYSTAP, LLC 2006-2007.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 package com.bigdata.rdf.sail;
 
 import java.util.concurrent.TimeUnit;
@@ -5,7 +27,6 @@ import java.util.concurrent.TimeUnit;
 import org.openrdf.query.Dataset;
 import org.openrdf.query.GraphQueryResult;
 import org.openrdf.query.QueryEvaluationException;
-import org.openrdf.query.TupleQueryResult;
 import org.openrdf.query.algebra.evaluation.QueryBindingSet;
 import org.openrdf.repository.sail.SailGraphQuery;
 
@@ -77,13 +98,13 @@ public class BigdataSailGraphQuery extends SailGraphQuery implements
 
     }
 
-    public GraphQueryResult evaluate(final BindingsClause bc) 
-    		throws QueryEvaluationException {
+    public GraphQueryResult evaluate(final BindingsClause bc)
+            throws QueryEvaluationException {
 
         final QueryRoot originalQuery = astContainer.getOriginalAST();
 
         if (bc != null)
-        	originalQuery.setBindingsClause(bc);
+            originalQuery.setBindingsClause(bc);
 
         if (getMaxQueryTime() > 0)
             originalQuery.setTimeout(TimeUnit.SECONDS
