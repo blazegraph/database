1c7c2897f7f2fda485a188fe65ed7ff39c8a500b
mrpersonick <mrpersonick@users.sourceforge.net>
2012-08-21 07:59:50 +0000

    fixed the parsing of fully numeric bnode ids, per ticket #572
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@6464 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata-rdf/src/java/com/bigdata/rdf/rio/turtle/BigdataTurtleParserFactory.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2007.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package com.bigdata.rdf.rio.turtle;
+
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFParser;
+import org.openrdf.rio.RDFParserFactory;
+import org.openrdf.rio.turtle.TurtleParser;
+
+/**
+ * An {@link RDFParserFactory} for Turtle parsers.
+ * 
+ * @author Arjohn Kampman
+ */
+public class BigdataTurtleParserFactory implements RDFParserFactory {
+
+	/**
+	 * Returns {@link RDFFormat#TURTLE}.
+	 */
+	public RDFFormat getRDFFormat() {
+		return RDFFormat.TURTLE;
+	}
+
+	/**
+	 * Returns a new instance of {@link TurtleParser}.
+	 */
+	public RDFParser getParser() {
+		return new BigdataTurtleParser();
+	}
+}
