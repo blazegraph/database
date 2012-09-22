aca7d7547a2d08c5cf99063a7998e0d3b4b124ab
thompsonbry <thompsonbry@users.sourceforge.net>
2012-09-22 23:51:18 +0000

    more HA config tweaks
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@6593 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- src/resources/HAJournal/HAJournalServer.sh
+++ src/resources/HAJournal/HAJournalServer.sh
@@ -7,5 +7,6 @@ source src/resources/HAJournal/HAJournal.env
 java\
  -cp ${CLASSPATH}\
  -Djava.security.policy=${POLICY_FILE}\
+ -Dlog4j.configuration=${LOG4J_CONFIG}\
  com.bigdata.journal.jini.ha.HAJournalServer\
  ${HAJOURNAL_CONFIG}
