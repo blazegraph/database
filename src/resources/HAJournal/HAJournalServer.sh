028870f1c9e6d606f8140a7f3df59e18eed9ec47
thompsonbry <thompsonbry@users.sourceforge.net>
2012-09-22 23:31:30 +0000

    Hacked a classpath setup using the find utility.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@6591 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- src/resources/HAJournal/HAJournalServer.sh
+++ src/resources/HAJournal/HAJournalServer.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Setup the environment.
-source ./HAJournal.env
+# Setup the environment.                                                                                                                       
+source src/resources/HAJournal/HAJournal.env
 
 # Start an HAJournalServer.  
 java\
+ -cp ${CLASSPATH}\
  -Djava.security.policy=${POLICY_FILE}\
  com.bigdata.journal.jini.ha.HAJournalServer\
  ${HAJOURNAL_CONFIG}
--- src/resources/HAJournal/lookupStarter.sh
+++ src/resources/HAJournal/lookupStarter.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Setup the environment.
-source ./HAJournal.env
+# Setup the environment.                                                                                                                       
+source src/resources/HAJournal/HAJournal.env
 
 # Start Lookup (on one or more machines as configured).
 java \
+ -cp ${CLASSPATH}\
  -Dapp.home=.\
  -Djini.lib=${JINI_LIB}\
  -Djini.lib.dl=${JINI_LIBDL}\
