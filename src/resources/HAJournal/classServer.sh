294e0b70ac87c7d27cb673e9f20f13798e42e7f7
thompsonbry <thompsonbry@users.sourceforge.net>
2012-09-22 23:31:30 +0000

    Hacked a classpath setup using the find utility.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@6591 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- src/resources/HAJournal/classServer.sh
+++ src/resources/HAJournal/classServer.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                                    
 
-# Setup the environment.
-source ./HAJournal.env
+# Setup the environment.                                                                                                                       
+source src/resources/HAJournal/HAJournal.env
 
-# Start ClassServer (on one or more machines)
+# Start ClassServer (on one or more machines)                                                                                                  
 java \
+ -cp ${CLASSPATH}\
  com.sun.jini.tool.ClassServer\
  -verbose -stoppable -port ${CODEBASE_PORT}\
  -dir ${JINI_LIBDL}
