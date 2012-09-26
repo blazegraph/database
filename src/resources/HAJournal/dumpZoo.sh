9d81c760bec08b06798da0b139a1424bc8313f1f
thompsonbry <thompsonbry@users.sourceforge.net>
2012-09-26 12:41:08 +0000

    Added script to dump zookeeper state into the HA resources directory.  This references the sample configuration file for the HA Journal rather than the as-deployed cluster configuration file.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@6623 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ src/resources/HAJournal/dumpZoo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Setup the environment.                                                                                                                       
+source src/resources/HAJournal/HAJournal.env
+
+# Start an HAJournalServer.  
+java\
+ -cp ${CLASSPATH}\
+ -Djava.security.policy=${POLICY_FILE}\
+ -Dlog4j.configuration=${LOG4J_CONFIG}\
+ com.bigdata.zookeeper.DumpZookeeper\
+ ${HAJOURNAL_CONFIG}
