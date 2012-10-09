ef54d232d7a1880c755d002789372fa780abad4b
mrpersonick <mrpersonick@users.sourceforge.net>
2012-10-09 15:16:59 +0000

    test case for ticket 610
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@6665 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata-sails/src/test/com/bigdata/rdf/sail/TestTicket610.java
@@ -0,0 +1,130 @@
+/**
+Copyright (C) SYSTAP, LLC 2011.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package com.bigdata.rdf.sail;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.openrdf.model.Graph;
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.GraphImpl;
+import org.openrdf.model.impl.StatementImpl;
+import org.openrdf.model.impl.URIImpl;
+import org.openrdf.model.vocabulary.OWL;
+import org.openrdf.model.vocabulary.RDF;
+import org.openrdf.model.vocabulary.RDFS;
+import org.openrdf.repository.RepositoryConnection;
+
+import com.bigdata.rdf.axioms.OwlAxioms;
+import com.bigdata.rdf.store.AbstractTripleStore;
+
+/**
+ * To run this test case, specify the following JVM property:
+ * <code>-DtestClass=com.bigdata.rdf.sail.TestBigdataSailWithoutSids</code>
+ *  
+ * @author <a href="mailto:mrpersonick@users.sourceforge.net">Mike Personick</a>
+ * @version $Id$
+ */
+public class TestTicket610 extends ProxyBigdataSailTestCase {
+
+    protected static final Logger log = Logger.getLogger(TestTicket610.class);
+
+    @Override
+    public Properties getProperties() {
+        
+        Properties props = super.getProperties();
+
+        props.setProperty(BigdataSail.Options.AXIOMS_CLASS, OwlAxioms.class.getName());
+        
+        return props;
+        
+    }
+
+    public TestTicket610() {
+    }
+
+    public TestTicket610(String arg0) {
+        super(arg0);
+    }
+    
+    public void testBug() throws Exception {
+    	
+        /*
+         * The bigdata store, backed by a temporary journal file.
+         */
+	  	final BigdataSail sail = getSail();
+	  	
+        final URI a = new URIImpl(":a");
+        final URI b = new URIImpl(":b");
+        
+        final Graph data = new GraphImpl(Arrays.asList(new StatementImpl[] {
+        		new StatementImpl(a, RDF.TYPE, OWL.TRANSITIVEPROPERTY),
+        		new StatementImpl(b, RDFS.SUBPROPERTYOF, a),
+        }));
+	  	
+	  	try {
+	  	
+	  		sail.initialize();
+	  		
+  			final BigdataSailRepository bigdataRepo = new BigdataSailRepository(sail);
+  			
+	  		{ // load the data into the bigdata store
+	  			
+	  			final RepositoryConnection cxn = bigdataRepo.getConnection();
+	  			try {
+	  				cxn.setAutoCommit(false);
+	  				cxn.add(data);
+	  				cxn.commit();
+	  			} finally {
+	  				cxn.close();
+	  			}
+	  			
+	  		}
+	  		
+	  		{ // check the closure
+	  			
+	  			final BigdataSailRepositoryConnection cxn = bigdataRepo.getReadOnlyConnection();
+	  			try {
+	  				
+	  				final AbstractTripleStore store = cxn.getTripleStore();
+	  				
+	  				final StringBuilder sb = store.dumpStore(true, true, false);
+	  				System.err.println(sb);
+	  				
+	  				assertTrue("missing the (<b> rdf:type owl:TransitiveProperty) inference", store.hasStatement(b, RDF.TYPE, OWL.TRANSITIVEPROPERTY));
+	  				
+	  			} finally {
+	  				cxn.close();
+	  			}
+	  			
+	  		}
+          
+        } finally {
+        	sail.__tearDownUnitTest();
+        }
+    	
+    }
+
+}
