897391c802ea0d58f03d0320fc0d3a1acfbbb034
thompsonbry <thompsonbry@users.sourceforge.net>
2012-12-03 14:43:16 +0000

    FutureTask that monitors its own outcome and logs out errors @ ERROR.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@6750 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata/src/java/com/bigdata/util/concurrent/MonitoredFutureTask.java
@@ -0,0 +1,77 @@
+/**
+
+Copyright (C) SYSTAP, LLC 2006-2010.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.bigdata.util.concurrent;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Helper task for monitoring the results of otherwise unwatched tasks.
+ * 
+ * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan Thompson</a>
+ * @param <T>
+ */
+public class MonitoredFutureTask<T> extends FutureTask<T> {
+
+    private static final Logger log = Logger
+            .getLogger(MonitoredFutureTask.class);
+    
+    public MonitoredFutureTask(final Callable<T> callable) {
+        super(callable);
+    }
+
+    public MonitoredFutureTask(final Runnable runnable, final T result) {
+        super(runnable, result);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Hooked to notice when the task is done.
+     */
+    @Override
+    public void run() {
+        try {
+            super.run();
+        } finally {
+            doCheck();
+        }
+    }
+
+    private void doCheck() {
+        if (super.isDone()) {
+            try {
+                super.get();
+            } catch (InterruptedException e) {
+                log.warn("Interrupted: " + e);
+            } catch (ExecutionException e) {
+                log.error(e, e);
+            }
+        }
+    }
+
+}
