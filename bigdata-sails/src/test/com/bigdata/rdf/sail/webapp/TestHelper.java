98d79d525520a2650d25b5a8e4e38d7666f34c42
thompsonbry <thompsonbry@users.sourceforge.net>
2013-03-05 15:38:21 +0000

    Added a TestHelper class to issue some HTTP requests that can not be issued through the index.html page (HTTP DELETEs) as an aid to debugging of REST API test failures.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@6955 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata-sails/src/test/com/bigdata/rdf/sail/webapp/TestHelper.java
@@ -0,0 +1,59 @@
+package com.bigdata.rdf.sail.webapp;
+
+import java.net.URISyntaxException;
+
+import junit.framework.TestCase;
+
+import org.openrdf.model.Resource;
+import org.openrdf.model.vocabulary.RDF;
+import org.openrdf.query.parser.sparql.FOAF;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+
+import com.bigdata.rdf.sail.remote.BigdataSailRemoteRepository;
+
+/**
+ * Helper class to debug the NSS by issuing commands that we can not issue
+ * from the index.html page (HTTP DELETEs, etc).
+ * 
+ * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan Thompson</a>
+ */
+public class TestHelper extends TestCase {
+
+    static public void main(final String[] args) throws URISyntaxException,
+            RepositoryException {
+
+        if (args.length != 1) {
+
+            System.err.println("usage: SPARQL-Endpoint-URL");
+
+            System.exit(1);
+
+        }
+
+        final String sparqlEndpointURL = args[0];
+
+        final BigdataSailRemoteRepository repo = new BigdataSailRemoteRepository(
+                sparqlEndpointURL);
+
+        RepositoryConnection cxn = null;
+        try {
+            
+            cxn = (RepositoryConnection) repo.getConnection();
+
+            cxn.remove(null/* s */, RDF.TYPE, FOAF.PERSON, (Resource[]) null/* c */);
+
+        } finally {
+
+            if (cxn != null) {
+                cxn.close();
+                cxn = null;
+            }
+
+            repo.shutDown();
+
+        }
+
+    }
+
+}
