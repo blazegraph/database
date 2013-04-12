b9386cebb46c145743eb73772eb8ada02dd586d1
thompsonbry <thompsonbry@users.sourceforge.net>
2013-04-12 20:09:49 +0000

    I have created an IPlugIn interface for the Journal.  There is no ability to register your own plugins yet, but the embedded behaviors for the queue statistics, O/S statistics, the HTTPD interface for performance counter reporting, and the ganglia peer are now all "plugins".
    The ganglia plugin will not start unless O/S performance counters are enabled.  This is controlled by the boolean property:
    {{{
            String COLLECT_PLATFORM_STATISTICS = Journal.class.getName() + ".collectPlatformStatistics";
    }}}
    
    This property is declared in PlatformStatsPlugIn.  It defaults to false.
    
    The Journal test suites are green with this change.
    
    @see https://sourceforge.net/apps/trac/bigdata/ticket/609 (bigdata-ganglia is required dependency for Journal)
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@7046 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata/src/java/com/bigdata/journal/QueueStatsPlugIn.java
@@ -0,0 +1,162 @@
+/**
+
+Copyright (C) SYSTAP, LLC 2006-2007.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+package com.bigdata.journal;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+
+import com.bigdata.util.concurrent.ThreadPoolExecutorBaseStatisticsTask;
+
+/**
+ * Plugin for sampling the {@link ExecutorService}. This collects interesting
+ * statistics about the thread pool for reporting to the load balancer service.
+ * 
+ * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan Thompson</a>
+ */
+public class QueueStatsPlugIn implements
+        IPlugIn<Journal, ThreadPoolExecutorBaseStatisticsTask> {
+
+    private static final Logger log = Logger.getLogger(QueueStatsPlugIn.class);
+
+    /**
+     * Performance counters options.
+     */
+    public interface Options {
+        
+        /**
+         * Boolean option for the collection of statistics from the various
+         * queues using to run tasks (default
+         * {@link #DEFAULT_COLLECT_QUEUE_STATISTICS}).
+         */
+        String COLLECT_QUEUE_STATISTICS = Journal.class.getName()
+                + ".collectQueueStatistics";
+
+        String DEFAULT_COLLECT_QUEUE_STATISTICS = "false";
+
+    }
+    
+    /**
+     * Collects interesting statistics on the {@link #executorService}.
+     * <p>
+     * Note: Guarded by synchronized(this).
+     * 
+     * @see Options#COLLECT_QUEUE_STATISTICS
+     */
+    private ThreadPoolExecutorBaseStatisticsTask queueSampleTask = null;
+
+    /**
+     * The {@link ScheduledFuture} for the task.
+     * <p>
+     * Note: Guarded by synchronized(this).
+     */
+    private ScheduledFuture<?> scheduledFuture = null;
+    
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Setup sampling on the client's thread pool.
+     */
+    @Override
+    public void startService(final Journal indexManager) {
+        
+        final boolean collectQueueStatistics = Boolean.valueOf(indexManager
+                .getProperty(Options.COLLECT_QUEUE_STATISTICS,
+                        Options.DEFAULT_COLLECT_QUEUE_STATISTICS));
+
+        if (log.isInfoEnabled())
+            log.info(Options.COLLECT_QUEUE_STATISTICS + "="
+                    + collectQueueStatistics);
+
+        if (!collectQueueStatistics) {
+
+            return;
+
+        }
+
+        final long initialDelay = 0; // initial delay in ms.
+        final long delay = 1000; // delay in ms.
+        final TimeUnit unit = TimeUnit.MILLISECONDS;
+
+        synchronized (this) {
+
+            queueSampleTask = new ThreadPoolExecutorBaseStatisticsTask(
+                    (ThreadPoolExecutor) indexManager.getExecutorService());
+
+            scheduledFuture = indexManager.addScheduledTask(queueSampleTask,
+                    initialDelay, delay, unit);
+
+        }
+
+    }
+
+    @Override
+    public void stopService(final boolean immediateShutdown) {
+
+        synchronized (this) {
+
+            if (scheduledFuture != null) {
+        
+                scheduledFuture
+                        .cancel(immediateShutdown/* mayInterruptIfRunning */);
+                
+                scheduledFuture = null;
+                
+            }
+
+            queueSampleTask = null;
+
+        }
+        
+    }
+
+    @Override
+    public ThreadPoolExecutorBaseStatisticsTask getService() {
+
+        synchronized(this) {
+
+            return queueSampleTask;
+            
+        }
+        
+    }
+
+    @Override
+    public boolean isRunning() {
+
+        synchronized (this) {
+        
+            if (scheduledFuture == null || scheduledFuture.isDone())
+                return false;
+            
+            return true;
+            
+        }
+        
+    }
+
+}
