fcebede48f000ba90a652d23f3a6899ca0bec6f1
thompsonbry <thompsonbry@users.sourceforge.net>
2013-04-25 15:34:18 +0000

    CreateKBTask:  - Missing license header.  - Log file was for the wrong class.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@7076 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-sails/src/java/com/bigdata/rdf/sail/CreateKBTask.java
+++ bigdata-sails/src/java/com/bigdata/rdf/sail/CreateKBTask.java
@@ -1,3 +1,29 @@
+/**
+
+Copyright (C) SYSTAP, LLC 2006-2011.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/*
+ * Created on Apr 13, 2011
+ */
 package com.bigdata.rdf.sail;
 
 import java.util.Properties;
@@ -13,7 +39,6 @@ import com.bigdata.journal.ITx;
 import com.bigdata.journal.Journal;
 import com.bigdata.quorum.AsynchronousQuorumCloseException;
 import com.bigdata.quorum.Quorum;
-import com.bigdata.rdf.sail.webapp.BigdataRDFServletContextListener;
 import com.bigdata.rdf.store.ScaleOutTripleStore;
 import com.bigdata.service.jini.JiniFederation;
 import com.bigdata.util.InnerCause;
@@ -26,7 +51,7 @@ import com.bigdata.util.InnerCause;
 public class CreateKBTask implements Callable<Void> {
 
     private static final transient Logger log = Logger
-            .getLogger(BigdataRDFServletContextListener.class);
+            .getLogger(CreateKBTask.class);
 
     private final IIndexManager indexManager;
     private final String namespace;
