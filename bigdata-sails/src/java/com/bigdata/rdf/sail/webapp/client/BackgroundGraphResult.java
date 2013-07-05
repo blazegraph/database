c55662f4c68a68fad0490311bd64f051ddd6376a
thompsonbry <thompsonbry@users.sourceforge.net>
2013-07-05 11:15:36 +0000

    inline comments and final fields and args.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@7213 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-sails/src/java/com/bigdata/rdf/sail/webapp/client/BackgroundGraphResult.java
+++ bigdata-sails/src/java/com/bigdata/rdf/sail/webapp/client/BackgroundGraphResult.java
@@ -32,124 +32,135 @@ import org.openrdf.rio.RDFParser;
  * 
  */
 public class BackgroundGraphResult implements GraphQueryResult, Runnable,
-		RDFHandler {
-	private volatile boolean closed;
-	private volatile Thread parserThread;
-	private RDFParser parser;
-	private Charset charset;
-	private InputStream in;
-	private String baseURI;
-	private CountDownLatch namespacesReady = new CountDownLatch(1);
-	private Map<String, String> namespaces = new ConcurrentHashMap<String, String>();
-	private QueueCursor<Statement> queue;
-	private HttpEntity entity;
+        RDFHandler {
+    private volatile boolean closed;
+    private volatile Thread parserThread;
+    final private RDFParser parser;
+    final private Charset charset;
+    final private InputStream in;
+    final private String baseURI;
+    final private CountDownLatch namespacesReady = new CountDownLatch(1);
+    final private Map<String, String> namespaces = new ConcurrentHashMap<String, String>();
+    final private QueueCursor<Statement> queue;
+    final private HttpEntity entity;
 
-	public BackgroundGraphResult(RDFParser parser, InputStream in,
-			Charset charset, String baseURI, HttpEntity entity) {
-		this(new QueueCursor<Statement>(10), parser, in, charset, baseURI,
-				entity);
-	}
+    public BackgroundGraphResult(final RDFParser parser, final InputStream in,
+            final Charset charset, final String baseURI, final HttpEntity entity) {
+        this(new QueueCursor<Statement>(10), parser, in, charset, baseURI,
+                entity);
+    }
 
-	public BackgroundGraphResult(QueueCursor<Statement> queue,
-			RDFParser parser, InputStream in, Charset charset, String baseURI,
-			HttpEntity entity) {
-		this.queue = queue;
-		this.parser = parser;
-		this.in = in;
-		this.charset = charset;
-		this.baseURI = baseURI;
-		this.entity = entity;
-	}
+    public BackgroundGraphResult(final QueueCursor<Statement> queue,
+            final RDFParser parser, final InputStream in, final Charset charset, final String baseURI,
+            final HttpEntity entity) {
+        this.queue = queue;
+        this.parser = parser;
+        this.in = in;
+        this.charset = charset;
+        this.baseURI = baseURI;
+        this.entity = entity;
+    }
 
-	public boolean hasNext() throws QueryEvaluationException {
-		return queue.hasNext();
-	}
+    @Override
+    public boolean hasNext() throws QueryEvaluationException {
+        return queue.hasNext();
+    }
 
-	public Statement next() throws QueryEvaluationException {
-		return queue.next();
-	}
+    @Override
+    public Statement next() throws QueryEvaluationException {
+        return queue.next();
+    }
 
-	public void remove() throws QueryEvaluationException {
-		queue.remove();
-	}
+    @Override
+    public void remove() throws QueryEvaluationException {
+        queue.remove();
+    }
 
-	public void close() throws QueryEvaluationException {
-		closed = true;
-		if (parserThread != null) {
-			parserThread.interrupt();
-		}
-		try {
-			queue.close();
-			in.close();
-		} catch (IOException e) {
-			throw new QueryEvaluationException(e);
-		}
-	}
+    @Override
+    public void close() throws QueryEvaluationException {
+        closed = true;
+        if (parserThread != null) {
+            parserThread.interrupt();
+        }
+        try {
+            queue.close();
+            in.close();
+        } catch (IOException e) {
+            throw new QueryEvaluationException(e);
+        }
+    }
 
-	public void run() {
-		boolean completed = false;
-		parserThread = Thread.currentThread();
-		try {
-			parser.setRDFHandler(this);
-			if (charset == null) {
-				parser.parse(in, baseURI);
-			} else {
-				parser.parse(new InputStreamReader(in, charset), baseURI);
-			}
-			EntityUtils.consume(entity);
-			completed = true;
-		} catch (RDFHandlerException e) {
-			// parsing was cancelled or interrupted
-		} catch (RDFParseException e) {
-			queue.toss(e);
-		} catch (IOException e) {
-			queue.toss(e);
-		} finally {
-			parserThread = null;
-			queue.done();
-			if (!completed) {
-				try {
-					EntityUtils.consume(entity);
-				} catch (IOException ex) { }
-			}
-		}
-	}
+    @Override
+    public void run() {
+        boolean completed = false;
+        parserThread = Thread.currentThread();
+        try {
+            parser.setRDFHandler(this);
+            if (charset == null) {
+                parser.parse(in, baseURI);
+            } else {
+                parser.parse(new InputStreamReader(in, charset), baseURI);
+            }
+            EntityUtils.consume(entity);
+            completed = true;
+        } catch (RDFHandlerException e) {
+            // parsing was cancelled or interrupted
+        } catch (RDFParseException e) {
+            queue.toss(e);
+        } catch (IOException e) {
+            queue.toss(e);
+        } finally {
+            parserThread = null;
+            queue.done();
+            if (!completed) {
+                try {
+                    EntityUtils.consume(entity);
+                } catch (IOException ex) { }
+            }
+        }
+    }
 
-	public void startRDF() throws RDFHandlerException {
-		// no-op
-	}
+    @Override
+    public void startRDF() throws RDFHandlerException {
+        // no-op
+    }
 
-	public Map<String, String> getNamespaces() {
-		try {
-			namespacesReady.await();
-			return namespaces;
-		} catch (InterruptedException e) {
-			throw new UndeclaredThrowableException(e);
-		}
-	}
+    @Override
+    public Map<String, String> getNamespaces() {
+        try {
+            namespacesReady.await();
+            return namespaces;
+        } catch (InterruptedException e) {
+            throw new UndeclaredThrowableException(e);
+        }
+    }
 
-	public void handleComment(String comment) throws RDFHandlerException {
-		// ignore
-	}
+    @Override
+    public void handleComment(final String comment) throws RDFHandlerException {
+        // ignore
+    }
 
-	public void handleNamespace(String prefix, String uri)
-			throws RDFHandlerException {
-		namespaces.put(prefix, uri);
-	}
+    @Override
+    public void handleNamespace(final String prefix, final String uri)
+            throws RDFHandlerException {
+        namespaces.put(prefix, uri);
+    }
 
-	public void handleStatement(Statement st) throws RDFHandlerException {
-		namespacesReady.countDown();
-		if (closed)
-			throw new RDFHandlerException("Result closed");
-		try {
-			queue.put(st);
-		} catch (InterruptedException e) {
-			throw new RDFHandlerException(e);
-		}
-	}
+    @Override
+    public void handleStatement(final Statement st) throws RDFHandlerException {
+        namespacesReady.countDown();
+        if (closed)
+            throw new RDFHandlerException("Result closed");
+        try {
+            queue.put(st);
+        } catch (InterruptedException e) {
+            throw new RDFHandlerException(e);
+        }
+    }
 
-	public void endRDF() throws RDFHandlerException {
-		namespacesReady.countDown();
-	}
+    @Override
+    public void endRDF() throws RDFHandlerException {
+        namespacesReady.countDown();
+    }
 
 }
