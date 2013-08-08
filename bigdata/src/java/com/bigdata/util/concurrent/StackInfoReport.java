5266491e04f6a13a76f2c3c09082f2f2e220874b
thompsonbry <thompsonbry@users.sourceforge.net>
2013-08-08 13:20:40 +0000

    Added exception whose sole purpose is to provide information during debugging rather than to provide exceptional control logic.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/READ_CACHE2@7262 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata/src/java/com/bigdata/util/concurrent/StackInfoReport.java
@@ -0,0 +1,67 @@
+/*
+
+Copyright (C) SYSTAP, LLC 2006-2008.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+package com.bigdata.util.concurrent;
+
+/**
+ * An exception class whose sole purpose is to provide information during
+ * debugging concerning the context in which some method is invoked. Instances
+ * of this exception ARE NOT errors. They are only informative and are used in
+ * patterns such as:
+ * <pre>
+ * if(log.isInfoEnabled())
+ *    log.info(new StackInfoReport())
+ * </pre>
+ * 
+ * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan Thompson</a>
+ */
+public class StackInfoReport extends RuntimeException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public StackInfoReport() {
+        super();
+    }
+
+    public StackInfoReport(String message, Throwable cause,
+            boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public StackInfoReport(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StackInfoReport(String message) {
+        super(message);
+    }
+
+    public StackInfoReport(Throwable cause) {
+        super(cause);
+    }
+
+}
