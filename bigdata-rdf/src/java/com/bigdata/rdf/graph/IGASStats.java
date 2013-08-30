cf7631d086f34732499314ddf173fd11a6373ed8
thompsonbry <thompsonbry@users.sourceforge.net>
2013-08-30 19:30:09 +0000

    Made the GAS statistics into a pure interface.
    See #629
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_2_0@7373 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-rdf/src/java/com/bigdata/rdf/graph/IGASStats.java
+++ bigdata-rdf/src/java/com/bigdata/rdf/graph/IGASStats.java
@@ -1,5 +1,31 @@
 package com.bigdata.rdf.graph;
 
+/**
+ * Statistics for GAS algorithms.
+ * 
+ * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan Thompson</a>
+ */
 public interface IGASStats {
+
+    void add(final long frontierSize, final long nedges, final long elapsedNanos);
+
+    void add(final IGASStats o);
+
+    long getNRounds();
+
+    /**
+     * The cumulative size of the frontier across the iterations.
+     */
+    long getFrontierSize();
+
+    /**
+     * The number of traversed edges across the iterations.
+     */
+    long getNEdges();
+
+    /**
+     * The elapsed nanoseconds across the iterations.
+     */
+    long getElapsedNanos();
 
 }
