ed571b3e52d0639de590429455f3fec291b448a9
jeremy_carroll <jeremy_carroll@users.sourceforge.net>
2013-10-03 15:45:34 +0000

    Deleted duplicate code, and added comments.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_3_0@7433 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/ProjectedConstraint.java
+++ bigdata-rdf/src/java/com/bigdata/rdf/internal/constraints/ProjectedConstraint.java
@@ -1,3 +1,28 @@
+/*
+
+Copyright (C) SYSTAP, LLC 2006-2013.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
 package com.bigdata.rdf.internal.constraints;
 
 import java.util.Map;
@@ -6,8 +31,34 @@ import com.bigdata.bop.BOp;
 import com.bigdata.bop.IBindingSet;
 import com.bigdata.rdf.error.SparqlTypeErrorException;
 
+
+/**
+ * This class implements 'AS' in a projection as found in both 
+ * subselects and bind, e.g.
+ * <pre>
+ *    { SUBSELECT (Foo(?x) AS ?y) ...
+ *      {  ...
+ *      }
+ *    }
+ * </pre>
+ * or
+ * <pre>
+ *    BIND (Bar(?x) AS ?y) 
+ * </pre>
+ * The bind fails if the expression evaluates to some value that is different from
+ * a pre-existing binding for the target variable.
+ * If the expr (Foo or Bar above) has an error then the specification is that no binding is done,
+ * but the unbound value is a success.
+ * 
+ * The specification for this operation is found as 
+ * <a href="http://www.w3.org/TR/2013/REC-sparql11-query-20130321/#defn_extend">Extend</a>.
+ *
+ */
+@SuppressWarnings({ "unchecked", "rawtypes" })
 public class ProjectedConstraint extends com.bigdata.bop.constraint.Constraint {
 
+    private static final long serialVersionUID = 1L;
+    
     public ProjectedConstraint(final BOp[] args, final Map<String, Object> annotations) {
         super(args, annotations);
     }
