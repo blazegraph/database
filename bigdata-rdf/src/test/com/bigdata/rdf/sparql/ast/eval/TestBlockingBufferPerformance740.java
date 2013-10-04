b63774a8b49250ee57bce91ccc50f12c279559e6
jeremy_carroll <jeremy_carroll@users.sourceforge.net>
2013-10-04 22:22:18 +0000

    added copyright, and some comments, improved var naming etc.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_3_0@7435 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/eval/TestBlockingBufferPerformance740.java
+++ bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/eval/TestBlockingBufferPerformance740.java
@@ -1,3 +1,26 @@
+/**
+
+Copyright (C) SYSTAP, LLC 2006-2013.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 package com.bigdata.rdf.sparql.ast.eval;
 
 import java.lang.management.ManagementFactory;
@@ -5,10 +28,8 @@ import java.lang.management.ThreadMXBean;
 
 import junit.framework.TestCase;
 
-import org.openrdf.query.algebra.evaluation.QueryBindingSet;
 
 import com.bigdata.rdf.sparql.ast.ASTContainer;
-import com.bigdata.rdf.sparql.ast.eval.AbstractDataDrivenSPARQLTestCase.TestHelper;
 
 public class TestBlockingBufferPerformance740 extends AbstractDataDrivenSPARQLTestCase {
 
@@ -19,7 +40,14 @@ public class TestBlockingBufferPerformance740 extends AbstractDataDrivenSPARQLTe
 		super(name);
 	}
 
-    public void test_ticket_747a() throws Exception {
+	/**
+	 * This test measures the percent utilization by comparing CPU time
+	 * with wall clock time. The test fails if this is less than 80%.
+	 * Obviously, the expected value is greater for machines with more CPUs, the
+	 * test does not take this into account.
+	 * @throws Exception
+	 */
+    public void test_ticket_740() throws Exception {
 
         new TestHelper("blocking-buffer-740",// testURI,
                 "blocking-buffer-740.rq",// queryFileURL
@@ -31,24 +59,29 @@ public class TestBlockingBufferPerformance740 extends AbstractDataDrivenSPARQLTe
             public ASTContainer runTest() throws Exception {
             	ThreadMXBean mgmt = ManagementFactory.getThreadMXBean();
             		//	mgmt.getThreadCPUTime() ;
-            	long totalThreadTime = sumForAllThreads(mgmt);
-            	long milli = System.currentTimeMillis();
+            	long startCpuTime = currentTotalCpuTime(mgmt);
+            	long startWallClock = System.currentTimeMillis();
             	try {
                 	return super.runTest();
             	}
             	finally {
-            		//totalThreadTime -= sumForAllThreads(mgmt);
-            		long cpuTime = sumForAllThreads(mgmt) - totalThreadTime;
-            		long clockTime = System.currentTimeMillis() - milli;
+            		long cpuTime = currentTotalCpuTime(mgmt) - startCpuTime;
+            		long clockTime = System.currentTimeMillis() - startWallClock;
             		cpuTime /= 1000000;
-            		//System.err.println(clockTime+":"+cpuTime);
-            		double percentUtilization = cpuTime * 1.0 / clockTime;
-            		TestCase.assertTrue("Performance was poor: "+(int)(percentUtilization*100)+"% [< 80%]",percentUtilization>0.8);
-            		System.out.println("BlockingBuffer: performance was good: "+(int)(percentUtilization*100)+"%");
+            		double utilization = cpuTime * 1.0 / clockTime;
+            		TestCase.assertTrue("Performance was poor: "+(int)(utilization*100)+"% [< 80%]",utilization>0.8);
+            		System.out.println("BlockingBuffer: performance was good: "+(int)(utilization*100)+"%");
             	}
             }
 
-			private long sumForAllThreads(ThreadMXBean mgmt) {
+            /**
+             * This is a pretty optimistic implementation ...
+             * it does not deal with threads dying.
+             * It seems to suffice for this test though.
+             * @param mgmt
+             * @return
+             */
+			private long currentTotalCpuTime(ThreadMXBean mgmt) {
 				long rslt = 0;
 				for (long tid:mgmt.getAllThreadIds()) {
 					long t = mgmt.getThreadCpuTime(tid);
