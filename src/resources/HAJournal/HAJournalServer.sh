efb1d85c64c4b74b59a62e2fac107997680a2946
thompsonbry <thompsonbry@users.sourceforge.net>
2013-10-30 12:37:37 +0000

    added optional property to enable remote debugging and/or profiler for the HAJournalServer.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_3_0@7498 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- src/resources/HAJournal/HAJournalServer.sh
+++ src/resources/HAJournal/HAJournalServer.sh
@@ -3,6 +3,15 @@
 # Setup the environment.                                                                                                                       
 source src/resources/HAJournal/HAJournal.env
 
+# Uncomment to enable profiler.
+#profilerAgent=-agentpath:/nas/install/yjp-12.0.6/bin/linux-x86-64/libyjpagent.so
+
+# Uncomment to have all profiling initially disabled.
+#profilerAgentOptions=-agentlib:yjpagent=disableexceptiontelemetry,disablestacktelemetry
+
+# Uncomment to enable remote debugging at the specified port.
+#debug=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1046
+
 # Start an HAJournalServer.  
 java\
  ${JAVAOPTS}\
@@ -10,5 +19,7 @@ java\
  -Djava.security.policy=${POLICY_FILE}\
  -Dlog4j.configuration=${LOG4J_CONFIG}\
  -Djava.util.logging.config.file=${LOGGING_CONFIG}\
+ ${debug}\
+ ${profilerAgent} ${profilerAgentOptions}\
  com.bigdata.journal.jini.ha.HAJournalServer\
  ${HAJOURNAL_CONFIG}
