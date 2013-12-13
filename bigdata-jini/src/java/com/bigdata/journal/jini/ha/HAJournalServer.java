7c49e65ce10db79ab1f7a2fd951187932516db00
thompsonbry <thompsonbry@users.sourceforge.net>
2013-12-13 21:05:10 +0000

    comment on purgeHA()
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_3_0@7648 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-jini/src/java/com/bigdata/journal/jini/ha/HAJournalServer.java
+++ bigdata-jini/src/java/com/bigdata/journal/jini/ha/HAJournalServer.java
@@ -4181,10 +4181,17 @@ public class HAJournalServer extends AbstractServer {
                      * Halt operation.
                      * 
                      * Note: This is not an error, but we can not remove
-                     * snapshots or HALogs if this invariant is violated. This
-                     * is because a leader fail could then cause the
-                     * IRestorePolicy to be violated if a service that lacked
-                     * some HALogs was elected as the leader.
+                     * snapshots or HALogs if this invariant is violated.
+                     * 
+                     * Note: We do not permit HALog files to be purged if the
+                     * quorum is not fully met. This is done in order to prevent
+                     * a situation a leader would not have sufficient log files
+                     * on hand to restore the failed service. If this were to
+                     * occur, then the failed service would have to undergo a
+                     * disaster rebuild rather than simply resynchronizing from
+                     * the leader. Hence, HALog files are NOT purged unless the
+                     * quorum is fully met (all services for its replication
+                     * count are joined with the met quorum).
                      */
                     return;
                 }
