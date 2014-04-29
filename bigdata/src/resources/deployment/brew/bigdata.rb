4b89b1a7ce6a38bb264cccfe86ad214cc18ea98d
dmekonnen <dmekonnen@users.sourceforge.net>
2014-04-29 03:37:31 +0000

    synching bigdata.rb updates to 1.3.0 branch.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_3_0@8142 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/resources/deployment/brew/bigdata.rb
+++ bigdata/src/resources/deployment/brew/bigdata.rb
@@ -4,35 +4,44 @@ require 'formula'
 #                /usr/local/Library/Contributions/example-formula.rb
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
-class SystapBigdata < Formula
-  homepage 'http://bigdata.com/bigdata/blog/'
-  url 'http://iweb.dl.sourceforge.net/project/bigdata/bigdata/1.3.0/REL.bigdata-1.3.0.tgz'
-  sha1 '605e800386300a6965125e0e9bfc06a268df0f08'
-
-  # depends_on 'cmake' => :build
-  # depends_on :java7 # if your formula requires any X11/XQuartz components
+class Bigdata < Formula
+  homepage 'http://bigdata.com/blog/'
+  url 'http://bigdata.com/deploy/bigdata-1.3.0.tgz'
+  sha1 'a395a243a2746ce47cf8893f2207fd2e0de4a9c1'
 
   def install
-    # Install the base files
     prefix.install Dir['*']
-
-    # Setup the lib files
-    # (var+'lib/bigdata').mkpath
-
-
-    # Extract bigdata and install to sbin
-
-    # sbin.install 'bigdata'
-    # (sbin/'bigdata').chmod 0755
   end
 
   def caveats; <<-EOS.undent
-    After launching, visit the Bigdata Workbench at: http://localhost:8080/bigdata
+     After launching, visit the Bigdata Workbench at: 
+
+       http://localhost:8080/bigdata
+
+     "bigdata" command synopis:
+     -------------------------
+
+     Start the server:
+
+          % bigdata start
+
+     Stop the server:
+
+          % bigdata stop
+
+     Restart the server:
+
+          % bigdata restart
+
+     To tune the server configuration, edit the "#{var}/jetty/WEB-INF/RWStore.properties" file.
+
+     Further documentation:
+	
+          #{doc}
     EOS
   end
 
-
-  plist_options :manual => 'bigdata'
+  plist_options :startup => 'true', :manual => 'bigdata start'
 
   def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
@@ -43,17 +52,14 @@ class SystapBigdata < Formula
         <key>Label</key>
         <string>#{plist_name}</string>
         <key>Program</key>
-        <string>#{opt_sbin}/bigdata</string>
+        <string>#{bin}/bigdata</string>
         <key>RunAtLoad</key>
         <true/>
-        <key>EnvironmentVariables</key>
-        <dict>
-          <!-- need erl in the path -->
-          <key>PATH</key>
-          <string>/usr/local/sbin:/usr/bin:/bin:/usr/local/bin</string>
-        </dict>
+        <key>WorkingDirectory</key>
+        <string>#{prefix}</string>
       </dict>
     </plist>
     EOS
   end
+
 end
