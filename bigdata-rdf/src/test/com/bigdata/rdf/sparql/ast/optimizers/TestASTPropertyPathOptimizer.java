3ac9782ddf43ad02c86223f2f954187e5aa96801
jeremy_carroll <jeremy_carroll@users.sourceforge.net>
2014-05-05 01:10:28 +0000

    File missed from last commit
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_3_0@8193 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/optimizers/TestASTPropertyPathOptimizer.java
@@ -0,0 +1,159 @@
+/**
+
+Copyright (C) SYSTAP, LLC 2006-2011.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/*
+ * Created on Aug 29, 2011
+ */
+
+package com.bigdata.rdf.sparql.ast.optimizers;
+
+import static com.bigdata.rdf.sparql.ast.optimizers.AbstractOptimizerTestCase.HelperFlag.ZERO_OR_MORE;
+
+import org.openrdf.model.impl.URIImpl;
+import org.openrdf.model.vocabulary.RDF;
+
+import com.bigdata.bop.IBindingSet;
+import com.bigdata.rdf.internal.IV;
+import com.bigdata.rdf.sparql.ast.ArbitraryLengthPathNode;
+import com.bigdata.rdf.sparql.ast.ConstantNode;
+import com.bigdata.rdf.sparql.ast.FilterNode;
+import com.bigdata.rdf.sparql.ast.FunctionNode;
+import com.bigdata.rdf.sparql.ast.FunctionRegistry;
+import com.bigdata.rdf.sparql.ast.IQueryNode;
+import com.bigdata.rdf.sparql.ast.JoinGroupNode;
+import com.bigdata.rdf.sparql.ast.ProjectionNode;
+import com.bigdata.rdf.sparql.ast.QueryRoot;
+import com.bigdata.rdf.sparql.ast.QueryType;
+import com.bigdata.rdf.sparql.ast.StatementPatternNode;
+import com.bigdata.rdf.sparql.ast.UnionNode;
+import com.bigdata.rdf.sparql.ast.ValueExpressionNode;
+import com.bigdata.rdf.sparql.ast.VarNode;
+
+/**
+ * Test suite for {@link ASTUnionFiltersOptimizer}.
+ * 
+ * @author <a href="mailto:mrpersonick@users.sourceforge.net">Mike Personick</a>
+ * @version $Id: TestASTEmptyGroupOptimizer.java 5302 2011-10-07 14:28:03Z
+ *          thompsonbry $
+ */
+public class TestASTPropertyPathOptimizer extends AbstractOptimizerTestCase {
+
+    /**
+     * 
+     */
+    public TestASTPropertyPathOptimizer() {
+    }
+
+    /**
+     * @param name
+     */
+    public TestASTPropertyPathOptimizer(String name) {
+        super(name);
+    }
+
+  
+	@Override
+	IASTOptimizer newOptimizer() {
+		return new ASTPropertyPathOptimizerInTest();
+	}
+	
+	/**
+	 * This is (nearly) the same as {@link TestALPPinTrac773#testSimpleALPP()
+	 */
+	public void test_basic_star() {
+		new Helper(){{
+
+    		given = select( varNode(x), 
+    				where (
+    						joinGroupNode( 
+    								propertyPathNode(varNode(x),"c*", constantNode(b))
+    							)
+    				) );
+    		
+    		
+    		expected = select( varNode(x), 
+    				where (
+    					joinGroupNode( 
+    						arbitartyLengthPropertyPath(varNode(x), constantNode(b), ZERO_OR_MORE,
+    										joinGroupNode( 
+    												statementPatternNode(leftVar(), constantNode(c),  rightVar())
+    												) ) )
+    				) );
+		}}.test();
+	}
+
+	public void test_filter_star() {
+		new Helper(){{
+
+    		given = select( varNode(x), 
+    				where (
+					  filter(
+						exists(varNode(y),
+    						joinGroupNode( 
+    								propertyPathNode(varNode(x),"c*", constantNode(b))
+    							) ) )
+    				) );
+    		
+    		expected = select( varNode(x), 
+    				where (
+    				  filter(
+    					exists(varNode(y),
+    					  joinGroupNode( 
+    						arbitartyLengthPropertyPath(varNode(x), constantNode(b), ZERO_OR_MORE,
+    										joinGroupNode( 
+    												statementPatternNode(leftVar(), constantNode(c),  rightVar())
+    												) ) ) ) )
+    				) );
+		}}.test();
+	}
+	public void test_filter_or_star() {
+		new Helper(){{
+
+    		given = select( varNode(x), 
+    				where (
+					  filter(
+						    	or (
+						    			constantNode(a),
+						exists(varNode(y),
+    						joinGroupNode( 
+    								propertyPathNode(varNode(x),"c*", constantNode(b))
+    							) ) )
+    							)
+    				) );
+    		
+    		expected = select( varNode(x), 
+    				where (
+    				  filter(
+						    	or (
+						    			constantNode(a),
+    					exists(varNode(y),
+    					  joinGroupNode( 
+    						arbitartyLengthPropertyPath(varNode(x), constantNode(b), ZERO_OR_MORE,
+    										joinGroupNode( 
+    												statementPatternNode(leftVar(), constantNode(c),  rightVar())
+    												) ) ) ) ) )
+    				) );
+		}}.test();
+	}
+
+}
