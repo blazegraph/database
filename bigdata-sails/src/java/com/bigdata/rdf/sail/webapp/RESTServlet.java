6c90a2b102564454a0b1e0072d72d609fb9455d7
mrpersonick <mrpersonick@users.sourceforge.net>
2014-05-08 17:53:52 +0000

    rolling back changes to build.xml and RESTServlet from r8223
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_3_0@8231 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-sails/src/java/com/bigdata/rdf/sail/webapp/RESTServlet.java
+++ bigdata-sails/src/java/com/bigdata/rdf/sail/webapp/RESTServlet.java
@@ -59,7 +59,6 @@ public class RESTServlet extends BigdataRDFServlet {
     private DeleteServlet m_deleteServlet;
     private UpdateServlet m_updateServlet;
     private WorkbenchServlet m_workbenchServlet;
-    private BlueprintsServlet m_blueprintsServlet;
     
     /**
      * @see <a href="https://sourceforge.net/apps/trac/bigdata/ticket/584">
@@ -85,7 +84,6 @@ public class RESTServlet extends BigdataRDFServlet {
         m_deleteServlet = new DeleteServlet();
         m_describeServlet = new DescribeCacheServlet();
         m_workbenchServlet = new WorkbenchServlet();
-        m_blueprintsServlet = new BlueprintsServlet();
 
         m_queryServlet.init(getServletConfig());
         m_insertServlet.init(getServletConfig());
@@ -93,7 +91,6 @@ public class RESTServlet extends BigdataRDFServlet {
         m_deleteServlet.init(getServletConfig());
         m_describeServlet.init(getServletConfig());
         m_workbenchServlet.init(getServletConfig());
-        m_blueprintsServlet.init(getServletConfig());
         
     }
     
@@ -131,11 +128,6 @@ public class RESTServlet extends BigdataRDFServlet {
         if (m_workbenchServlet != null) {
             m_workbenchServlet.destroy();
             m_workbenchServlet = null;
-        }
-
-        if (m_blueprintsServlet != null) {
-            m_blueprintsServlet.destroy();
-            m_blueprintsServlet = null;
         }
 
         super.destroy();
@@ -250,10 +242,6 @@ public class RESTServlet extends BigdataRDFServlet {
         	
         	m_workbenchServlet.doPost(req, resp);
         	
-        } else if (req.getParameter(BlueprintsServlet.ATTR_BLUEPRINTS) != null) {
-            
-            m_blueprintsServlet.doPost(req, resp);
-            
         } else if (req.getParameter("uri") != null) {
 
             // INSERT via w/ URIs
