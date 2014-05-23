0be4e86397b49e885224d8e411903c3db90b3fe9
thompsonbry <thompsonbry@users.sourceforge.net>
2014-05-23 15:29:40 +0000

    See #941 (merge deployments branch to main branch).
    - HARestore.sh: You can not safely rely on the limited classpath that
      is used in this script.  This is very likely to break based merely
      on the imports into the HARestore, Journal, AbstractJournal and
      related classes.  At a minimum, we would need to test this classpath
      for each release or in CI.  I would prefer that we had a means to
      assemble a better classpath. The startHAServices script has a
      similar problem.  The classpath is currently hacked there using the
      incantation
    
    export HAJOURNAL_CLASSPATH=`find ${LIB_DIR} -name '*.jar' -print0 | tr '\0' ':'`
    
    - What is the purpose of the "src/resources/deployment" directory?  Is
      this the "single-server, non-HA" NSS deployment?
    
    - /bigdata/deployment - we put all of this stuff under /src/resources NOT /bigdata.
    
      - I have deleted /bigdata/deployment entirely from branches/BIGDATA_RELEASE_1_3_0.
    
      - I have copied the files (but not the SVN folders) from the
        DEPLOYMENT_BRANCH_1_3_1/bigdata/src/resources/deployment into
        /src/resources/deployment.
    
    - jetty.xml: copied from the DEPLOYMENTS branch.
    
    - /Users/bryan/Documents/workspace/BIGDATA_RELEASE_1_3_0/src/resources/bin/bigdata.sh
    
      - This has been removed. The src/resources/deployment/nss directory
        has similar scripts.  It is Ok to add an ant task to start the nss
        for developers, but deployments should be based on the "ant stage"
        pattern.
    
    - src/resources/deployment/nss/WEB-INF/RWStore.properties should be
      removed. The brew script should replace the following line in the
      version from bigdata-war/src/WEB-INF/RWStore.properties with an 
      absolute filename.
    
    com.bigdata.journal.AbstractJournal.file=ZZZZ
    
    - src/resources/deployment/nss/WEB-INF/log4j.properties should be
      removed.  The brew script should replace the following lines in the
      version from dist/var/config/logging/log4j.properties in order to
      setup (a) logging to a file; and (b) to specify the absolution
      location of that file.
    
    log4j.rootCategory=XXXX
    
    log4j.appender.file.File=YYYY
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_3_0@8414 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/resources/deployment/vagrant/systap-bigdata/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default['tomcat']['base_version'] = 7
-default['systap-bigdata'][:url]  = "http://sourceforge.net/projects/bigdata/files/bigdata/1.3.0/bigdata.war/download"
-
-webapp_dir = node['tomcat']['webapp_dir']
-default['systap-bigdata'][:home] = webapp_dir + "/bigdata"
-default['systap-bigdata'][:etc]  = webapp_dir + "/bigdata/etc"
