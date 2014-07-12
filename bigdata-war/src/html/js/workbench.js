bbc8668d3bd270253f56047f0a20c4c9d2beb195
tobycraig <tobycraig@users.sourceforge.net>
2014-07-12 01:12:41 +0000

    Fixed queries/details links on status tab
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_3_0@8538 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-war/src/html/js/workbench.js
+++ bigdata-war/src/html/js/workbench.js
@@ -1346,8 +1346,8 @@ function getStatus(e) {
 
 function getStatusNumbers(data) {
    $('#status-text').html(data);
-   $('#status-text a').eq(1).click(function(e) { e.preventDefault(); showQueries(false); return false; });
-   $('#status-text a').eq(2).click(function(e) { e.preventDefault(); showQueries(true); return false; });
+   $('#status-text a').eq(-2).click(function(e) { e.preventDefault(); showQueries(false); return false; });
+   $('#status-text a').eq(-1).click(function(e) { e.preventDefault(); showQueries(true); return false; });
 }
 
 $('#show-queries').click(function(e) {
