f3574ea878026481e20d045b3646a4e59218fc5b
mrpersonick <mrpersonick@users.sourceforge.net>
2014-07-18 15:30:35 +0000

    Ticket 995: Better SPARQL support through BigdataGraph, plus some hardening and usability refinements.
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_3_0@8574 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-blueprints/src/java/com/bigdata/blueprints/BigdataEdge.java
+++ bigdata-blueprints/src/java/com/bigdata/blueprints/BigdataEdge.java
@@ -24,10 +24,11 @@ package com.bigdata.blueprints;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.openrdf.model.Statement;
 import org.openrdf.model.URI;
-import org.openrdf.model.vocabulary.RDFS;
 
 import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Edge;
@@ -42,6 +43,8 @@ import com.tinkerpop.blueprints.Vertex;
  */
 public class BigdataEdge extends BigdataElement implements Edge {
 
+    private static final transient Logger log = Logger.getLogger(BigdataEdge.class);
+    
 	private static final List<String> blacklist = Arrays.asList(new String[] {
 	        "id", "", "label"
 		});
@@ -57,13 +60,19 @@ public class BigdataEdge extends BigdataElement implements Edge {
 	@Override
 	public Object getId() {
 	    
-		return graph.factory.fromEdgeURI(uri);
+        if (log.isInfoEnabled())
+            log.info("()");
+        
+		return graph.factory.fromURI(uri);
 		
 	}
 
 	@Override
 	public void remove() {
 	    
+        if (log.isInfoEnabled())
+            log.info("()");
+        
 		graph.removeEdge(this);
 		
 	}
@@ -71,13 +80,19 @@ public class BigdataEdge extends BigdataElement implements Edge {
 	@Override
 	public String getLabel() {
 	    
-		return (String) graph.getProperty(uri, RDFS.LABEL);
+        if (log.isInfoEnabled())
+            log.info("()");
+        
+		return (String) graph.getProperty(uri, graph.getValueFactory().getLabelURI());
 		
 	}
 
 	@Override
 	public Vertex getVertex(final Direction dir) throws IllegalArgumentException {
 		
+        if (log.isInfoEnabled())
+            log.info("("+dir+")");
+        
 		if (dir == Direction.BOTH) {
 			throw new IllegalArgumentException();
 		}
@@ -85,7 +100,7 @@ public class BigdataEdge extends BigdataElement implements Edge {
 		final URI uri = (URI)  
 				(dir == Direction.OUT ? stmt.getSubject() : stmt.getObject());
 		
-		final String id = graph.factory.fromVertexURI(uri);
+		final String id = graph.factory.fromURI(uri);
 		
 		return graph.getVertex(id);
 		
@@ -94,6 +109,9 @@ public class BigdataEdge extends BigdataElement implements Edge {
 	@Override
 	public void setProperty(final String prop, final Object val) {
 
+        if (log.isInfoEnabled())
+            log.info("("+prop+", "+val+")");
+        
 		if (prop == null || blacklist.contains(prop)) {
 			throw new IllegalArgumentException();
 		}
@@ -111,5 +129,56 @@ public class BigdataEdge extends BigdataElement implements Edge {
         return "e["+p.getLocalName()+"]["+s.getLocalName()+"->"+o.getLocalName()+"]";
         
     }
+    
+    @Override
+    public <T> T getProperty(final String prop) {
+        
+        if (log.isInfoEnabled())
+            log.info("("+prop+")");
+        
+        return super.getProperty(prop);
+    }
+
+    @Override
+    public Set<String> getPropertyKeys() {
+        
+        if (log.isInfoEnabled())
+            log.info("()");
+        
+        return super.getPropertyKeys();
+        
+    }
+
+    @Override
+    public <T> T removeProperty(final String prop) {
+        
+        if (log.isInfoEnabled())
+            log.info("("+prop+")");
+        
+        return super.removeProperty(prop);
+        
+    }
+
+//    @Override
+//    public void addProperty(final String prop, final Object val) {
+//        
+//        if (log.isInfoEnabled())
+//            log.info("("+prop+", "+val+")");
+//        
+//        super.addProperty(prop, val);
+//        
+//    }
+//
+//    @Override
+//    public <T> List<T> getProperties(final String prop) {
+//        
+//        if (log.isInfoEnabled())
+//            log.info("("+prop+")");
+//        
+//        return super.getProperties(prop);
+//        
+//    }
+    
+    
     
 }
---
+++ bigdata-blueprints/src/java/com/bigdata/blueprints/ImmortalGraph.java
@@ -0,0 +1,84 @@
+/**
+Copyright (C) SYSTAP, LLC 2006-2014.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+package com.bigdata.blueprints;
+
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+/**
+ * An implementation of a Blueprints Graph that implements immortality using
+ * the RDR specification.
+ * 
+ * Modeling vertices
+ * <<:v1 rdf:type :Vertex>> :timestamp "t1" .
+ * 
+ * Modeling element properties
+ * <<:v1 :p1 "val1">> :timestamp "t1" .
+ * <<:v1 :p1 "val2">> :timestamp "t2" .
+ * 
+ * Modeling edges
+
+ * Two distinct edges between :v and :v2 with the same edge label.  Totally
+ * legal in Blueprints.
+ * :v1 :knows :v2 .
+ * <<:v1 :knows :v2>> :id :e1 .
+ * <<:v2 :knows :v2>> :id :e2 .
+ * 
+ * Created at different times.
+ * :e1 :timestamp "t1" .
+ * :e2 :timestamp "t2" .
+ * 
+ * With different properties.
+ * <<:e1 :p1 "foo">> :timestamp "t1" .
+ * <<:e2 :p1 "bar">> :timestamp "t2" .
+ * 
+ * Modeling a deleted property:
+ * 
+ * <<:v1 :p1 :null>> :timestamp "t3" .
+ * 
+ * Multiple values for the same property with the same timestamp implies a list:
+ * 
+ * <<:v2 :p2 "v1">> :timestamp "t1" .
+ * <<:v2 :p2 "v2">> :timestamp "t1" .
+ * <<:v2 :p2 "v3">> :timestamp "t1" .
+ * 
+ * Pure append writes - no removal of old property values.
+ * 
+ * @author mikepersonick
+ *
+ */
+public abstract class ImmortalGraph extends BigdataGraph {
+
+    private static final transient Logger log = Logger.getLogger(ImmortalGraph.class);
+    
+    public ImmortalGraph(final BlueprintsValueFactory factory) {
+        this(factory, new Properties());
+    }
+    
+	public ImmortalGraph(final BlueprintsValueFactory factory,
+	        final Properties props) {
+	    super(factory, props);
+	}
+	
+}
