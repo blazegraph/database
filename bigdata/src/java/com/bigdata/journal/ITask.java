2880f31288ffb1e6328c42ab1a8c891049781144
thompsonbry <thompsonbry@users.sourceforge.net>
2014-07-18 15:45:57 +0000

    Checkpoint on GIST refactor (#585) in support of group commit for the REST API (#566).
    This commit introduces hierarchical locking (vs enumeration of the indices) into the AbstractApiTask and makes some progress toward a refactor of the ITask interface and the AbstractTask implementation to support access at the ICheckpointProtocol layer (supports BTree, HTree and Stream) in addition to the IIndex layer (supports BTree, FusedView, and IsolatedFusedView).
    
    The next step will be to refactor AbstractTask.getIndex() to push down a method to obtaining the ICheckpointProtocol object.  In order to do this, the AbstractTask.indexCache must be relayered.  It is currently specific to the IIndex interface.  In order to provide caching for ICheckpointProtocol objects, it needs to be rewritten to the ICheckpointProtocol layer. However, the code needs to be carefully reviewed to determine whether we also need caching at the IIndex layer or if we can only cache at the ICheckpointProtocol layer.  This is a question of both correctness and performance.
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_3_0@8578 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/journal/ITask.java
+++ bigdata/src/java/com/bigdata/journal/ITask.java
@@ -98,7 +98,8 @@ public interface ITask<T> extends Callable<T> {
     String toString();
 
     /**
-     * Return an appropriate view of the named index for the operation.
+     * Return an appropriate view of the named B+Tree that has the appropriate
+     * isolation level for the operation (non-GIST).
      * <p>
      * When the task is isolated by a transaction, then the index will be
      * isolated by the transaction using the appropriate isolation level. If the
@@ -124,23 +125,63 @@ public interface ITask<T> extends Callable<T> {
      * 
      * @return An appropriate view of the named index.
      * 
-     * @exception NoSuchIndexException
-     *                if the named index does not exist at the time that the
-     *                operation is executed.
+     * @throws NullPointerException
+     *             if <i>name</i> is <code>null</code>.
+     * @throws IllegalStateException
+     *             if <i>name</i> is not a declared resource.
+     * @throws StaleLocatorException
+     *             if <i>name</i> identifies an index partition which has been
+     *             split, joined, or moved.
+     * @throws NoSuchIndexException
+     *             if the named index is not registered as of the timestamp.
      * 
-     * @exception StaleLocatorException
-     *                if the named index does not exist at the time the
-     *                operation is executed and the {@link IResourceManager} has
-     *                information which indicates that the index partition has
-     *                been split, joined or moved.
-     * 
-     * @exception IllegalStateException
-     *                if the named index is not one of the resources declared to
-     *                the constructor.
-     * 
-     * @see IGISTLocalManager
+     *             TODO modify to return <code>null</code> if the index is not
+     *             registered?
      */
     IIndex getIndex(String name); // non-GIST
+    
+    /**
+     * Return an appropriate view of the named index for the operation (GIST).
+     * <p>
+     * This method MUST be used to access non-B+Tree data structures that do not
+     * (yet) support {@link FusedView} style transaction isolation.
+     * <p>
+     * This method MAY NOT be used to access data structures if the operation is
+     * isolated by a read-write transaction.
+     * <p>
+     * This method DOES NOT understand the ordered views used by scale-out. The
+     * {@link ICheckpointProtocol} interface returned by this method is a
+     * concrete durable GIST data structure with a specific commit record. It is
+     * NOT a {@link FusedView} or similar data structure assembled from an
+     * ordered array of indices. If this method is used for a GIST data
+     * structure it will ONLY return the {@link ICheckpointProtocol} and will
+     * not wrap it with a {@link FusedView}. (This is of practical importance
+     * only for scale-out which uses {@link FusedView}s to support the dynamic
+     * key range partitioning algorithm for the distributed B+Tree data
+     * structure.)
+     * 
+     * @param name
+     *            The index name.
+     * 
+     * @return An appropriate view of the named index.
+     * 
+     * @throws NullPointerException
+     *             if <i>name</i> is <code>null</code>.
+     * @throws IllegalStateException
+     *             if <i>name</i> is not a declared resource.
+     * @throws StaleLocatorException
+     *             if <i>name</i> identifies an index partition which has been
+     *             split, joined, or moved.
+     * @throws NoSuchIndexException
+     *             if the named index is not registered as of the timestamp.
+     * @throws UnsupportedOperationException
+     *             if the {@link ITask} is associated with a read-write
+     *             transaction.
+     * 
+     *             TODO modify to return <code>null</code> if the index is not
+     *             registered?
+     */
+//    ICheckpointProtocol getIndexLocal(String name); // GIST
 
     /**
      * The object used to track events and times for the task.
