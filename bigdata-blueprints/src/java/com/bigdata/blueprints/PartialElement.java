5776c0ca41ac0563fa92cd81c5da4510c5c17694
mrpersonick <mrpersonick@users.sourceforge.net>
2014-09-09 16:35:15 +0000

    updated to support append-only writes as a config property
    git-svn-id: svn+ssh://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_3_0@8646 8f7bc0f5-282e-0410-95e3-8d296e9bb460

---
+++ bigdata-blueprints/src/java/com/bigdata/blueprints/PartialElement.java
@@ -0,0 +1,124 @@
+/**
+Copyright (C) SYSTAP, LLC 2006-Infinity.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+package com.bigdata.blueprints;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.tinkerpop.blueprints.Element;
+
+public class PartialElement implements Element {
+
+    private final String id;
+    
+    private final Map<String, Object> properties = 
+            new LinkedHashMap<String, Object>();
+    
+    public PartialElement(final String id) {
+        this.id = id;
+    }
+    
+    @Override
+    public Object getId() {
+        return id;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Object getProperty(final String name) {
+        return properties.get(name);
+    }
+
+    @Override
+    public Set<String> getPropertyKeys() {
+        return properties.keySet();
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Object removeProperty(final String key) {
+        return properties.remove(key);
+    }
+
+    @Override
+    public void setProperty(final String key, final Object value) {
+        
+        /*
+         * Gracefully turn a single value property into a 
+         * multi-valued property.
+         */
+        if (properties.containsKey(key)) {
+        
+            final Object o = properties.get(key);
+            
+            if (o instanceof List) {
+                
+                @SuppressWarnings("unchecked")
+                final List<Object> list = (List<Object>) o;
+                list.add(value);
+                
+            } else {
+                
+                final List<Object> list = new LinkedList<Object>();
+                list.add(o);
+                list.add(value);
+                
+                properties.put(key, list);
+                
+            }
+            
+        } else {
+        
+            properties.put(key, value);
+            
+        }
+        
+    }
+    
+    public void copyProperties(final PartialElement element) {
+        properties.putAll(element.properties);
+    }
+    
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("id: " + id);
+        sb.append(", props: ");
+        appendProps(sb);
+        return sb.toString();
+    }
+    
+    protected void appendProps(final StringBuilder sb) {
+        for (Map.Entry<String, Object> prop : properties.entrySet()) {
+            sb.append(prop.getKey()).append("=").append(prop.getValue());
+        }
+    }
+
+}
