665a1bf7e05ebeb071d340e85bc20147cc544f56
Bryan Thompson <bryan@systap.com>
2014-11-05 08:17:21 -0500

    Initial work on fast range count and predicate scan optimizers.

--- bigdata-rdf/src/java/com/bigdata/rdf/sparql/ast/optimizers/ASTFastRangeCountOptimizer.java
+++ bigdata-rdf/src/java/com/bigdata/rdf/sparql/ast/optimizers/ASTFastRangeCountOptimizer.java
@@ -35,6 +35,56 @@ import com.bigdata.rdf.sparql.ast.eval.AST2BOpContext;
  * Optimizes SELECT COUNT(*) { triple-pattern } using the fast range count
  * mechanisms when that feature would produce exact results for the KB instance.
  * 
+ * <h2>Cases handled by this optimizer</h2>
+ * 
+ * Basic combinations with identical semantics:
+ * <pre>SELECT COUNT(DISTINCT *) {?s ?p ?o}</pre>
+ * <pre>SELECT COUNT(REDUCED *) {?s ?p ?o}</pre>
+ * <pre>SELECT COUNT(*) {?s ?p ?o}</pre>
+ * 
+ * Combinations using a constrained range-count.
+ * <pre>SELECT COUNT(*) {:s ?p ?o}</pre>
+ * <pre>SELECT COUNT(*) {?s :p ?o}</pre>
+ * <pre>SELECT COUNT(*) {?s ?p :o}</pre>
+ * <pre>SELECT COUNT(*) {:s ?p :o}</pre>
+
+ * Combinations using a constrained range-count where the triple pattern is
+ * 1-unbound and the COUNT() references the unbound variable.
+ * <pre>SELECT COUNT(?s) {?s :p :o}</pre>
+ * <pre>SELECT COUNT(?p) {:s ?p :o}</pre>
+ * <pre>SELECT COUNT(?o) {:s :p ?o}</pre>
+
+ * Combinations using a constrained range-count with a QUADS mode access path.
+ * <pre>SELECT COUNT(*) { GRAPH ?g {?s ?p ?o} }</pre>
+ * <pre>SELECT COUNT(*) { GRAPH :g {:s ?p ?o} }</pre>
+ * 
+ * Combinations using a constrained range-count with a QUADS mode access path
+ * where the triple pattern is 1-unbound and the COUNT() references the unbound variable.
+ * <pre>SELECT COUNT(?s) { GRAPH :g {?s :p :o} }</pre>
+ * <pre>SELECT COUNT(?g) { GRAPH ?g {:s :p :o} }</pre>
+ * 
+ * Combinations using a sub-select with nothing projected in:
+ * <pre>SELECT * { { SELECT COUNT(*) {?s ?p ?o} } }</pre>
+ * <pre>SELECT * { { SELECT COUNT(*) {?s ?p ?o} } :s :p :o .}</pre>
+ * 
+ * Combinations using a sub-select with something projected in:
+ * <pre>SELECT * { ?s a :b . { SELECT COUNT(*) {?s ?p ?o} .}</pre>
+ *
+ * <h2>Correct rejection cases NOT handled by this optimizer</h2>
+ *
+ * Combinations using DISTINCT/REDUCED and a constrained range-count,
+ * explicitly naming the variables, and having variables that are not in
+ * the COUNT() aggregate and not projected in are NOT handled here. These
+ * are covered by the {@link ASTDistinctTermScanOptimizer} instead:
+ * 
+ * <pre>SELECT COUNT(?p) {:s ?p ?o}</pre>
+ * <pre>SELECT COUNT(DISTINCT ?p) {:s ?p ?o}</pre>
+ * <pre>SELECT COUNT(REDUCED ?p) {:s ?p ?o}</pre>
+ * 
+ * Sub-select that would be handled as a distinct term scan with something
+ * projected in.
+ * <pre>SELECT * { ?s a :b . { SELECT COUNT(?p) {?s ?p ?o} .}</pre>
+ * 
  * @see <a href="http://trac.bigdata.com/ticket/1037" > Rewrite SELECT
  *      COUNT(...) (DISTINCT|REDUCED) {single-triple-pattern} as ESTCARD </a>
  * 
--- bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/optimizers/TestASTFastRangeCountOptimizer.java
+++ bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/optimizers/TestASTFastRangeCountOptimizer.java
@@ -27,10 +27,33 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package com.bigdata.rdf.sparql.ast.optimizers;
 
+import org.openrdf.model.impl.URIImpl;
+import org.openrdf.query.algebra.StatementPattern.Scope;
+
+import com.bigdata.bop.IBindingSet;
+import com.bigdata.rdf.internal.IV;
 import com.bigdata.rdf.sparql.ast.AbstractASTEvaluationTestCase;
+import com.bigdata.rdf.sparql.ast.ConstantNode;
+import com.bigdata.rdf.sparql.ast.IQueryNode;
+import com.bigdata.rdf.sparql.ast.JoinGroupNode;
+import com.bigdata.rdf.sparql.ast.ProjectionNode;
+import com.bigdata.rdf.sparql.ast.QueryRoot;
+import com.bigdata.rdf.sparql.ast.QueryType;
+import com.bigdata.rdf.sparql.ast.StatementPatternNode;
+import com.bigdata.rdf.sparql.ast.UnionNode;
+import com.bigdata.rdf.sparql.ast.VarNode;
 
 /**
- * Test suite for {@link ASTFastRangeCountOptimizer}.
+ * Test suite for {@link ASTFastRangeCountOptimizer}. This needs to handle a
+ * variety of things related to the following, including where variables are
+ * projected into a sub-select (in which case we run the fast range count using
+ * the as-bound variables for the triple pattern).
+ * 
+ * <pre>
+ * SELECT COUNT(...) (DISTINCT|REDUCED) {single-triple-pattern}
+ * </pre>
+ * 
+ * @see ASTFastRangeCountOptimizer
  * 
  * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan Thompson</a>
  */
@@ -45,8 +68,90 @@ public class TestASTFastRangeCountOptimizer extends
 		super(name);
 	}
 
+	/**
+	 * 
+	 * <pre>
+	 * SELECT COUNT(DISTINCT *) {?s ?p ?o}
+	 * </pre>
+	 */
 	public void test_fastRangeCountOptimizer_01() {
-		fail("write tests");
+
+        /*
+         * Note: DO NOT share structures in this test!!!!
+         */
+        final IBindingSet[] bsets = new IBindingSet[]{};
+
+        @SuppressWarnings("rawtypes")
+        final IV p = makeIV(new URIImpl("http://example/p"));
+
+        @SuppressWarnings("rawtypes")
+        final IV q = makeIV(new URIImpl("http://example/q"));
+        
+        // The source AST.
+        final QueryRoot given = new QueryRoot(QueryType.SELECT);
+        {
+
+            final ProjectionNode projection = new ProjectionNode();
+            given.setProjection(projection);
+            projection.addProjectionVar(new VarNode("*"));
+            
+            final JoinGroupNode whereClause = new JoinGroupNode();
+            given.setWhereClause(whereClause);
+
+            final UnionNode union = new UnionNode();
+            final JoinGroupNode joinGroup1 = new JoinGroupNode(
+                    new StatementPatternNode(new VarNode("s"),
+                            new ConstantNode(p), new VarNode("o"), null/* c */,
+                            Scope.DEFAULT_CONTEXTS));
+            final JoinGroupNode joinGroup2 = new JoinGroupNode(
+                    new StatementPatternNode(new VarNode("s"),
+                            new ConstantNode(q), new VarNode("o"), null/* c */,
+                            Scope.DEFAULT_CONTEXTS));
+            whereClause.addChild(union);
+            union.addChild(joinGroup1);
+            union.addChild(joinGroup2);
+
+        }
+
+        // The expected AST after the rewrite.
+        final QueryRoot expected = new QueryRoot(QueryType.SELECT);
+        {
+            
+            final ProjectionNode projection = new ProjectionNode();
+            expected.setProjection(projection);
+            projection.addProjectionVar(new VarNode("s"));
+            projection.addProjectionVar(new VarNode("o"));
+
+            final JoinGroupNode whereClause = new JoinGroupNode();
+            expected.setWhereClause(whereClause);
+
+            final UnionNode union = new UnionNode();
+            final JoinGroupNode joinGroup1 = new JoinGroupNode(
+                    new StatementPatternNode(new VarNode("s"),
+                            new ConstantNode(p), new VarNode("o"), null/* c */,
+                            Scope.DEFAULT_CONTEXTS));
+            final JoinGroupNode joinGroup2 = new JoinGroupNode(
+                    new StatementPatternNode(new VarNode("s"),
+                            new ConstantNode(q), new VarNode("o"), null/* c */,
+                            Scope.DEFAULT_CONTEXTS));
+            whereClause.addChild(union);
+            union.addChild(joinGroup1);
+            union.addChild(joinGroup2);
+            
+        }
+
+        final IASTOptimizer rewriter = new ASTWildcardProjectionOptimizer();
+        
+        final IQueryNode actual = rewriter.optimize(null/* AST2BOpContext */,
+                given/* queryNode */, bsets);
+
+        assertSameAST(expected, actual);
+
+		fail("write test");
     }
-    
+
+	public void test_fastRangeCountOptimizer_xx() {
+		fail("more coverage of different cases");
+	}
+	
 }
