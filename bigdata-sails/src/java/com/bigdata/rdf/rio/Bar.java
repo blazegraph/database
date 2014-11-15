f90714c42685ca0b72c2a6707db47df21249cded
Bryan Thompson <bryan@systap.com>
2014-11-15 12:51:55 -0500

    Removed unused Foo and Bar classes in main code (not tests)

--- bigdata-sails/src/java/com/bigdata/rdf/rio/Bar.java
+++
@@ -1,8 +0,0 @@
-package com.bigdata.rdf.rio;
-
-public interface Bar extends Foo<Bar> {
-
-    @Override
-    Bar a();
-    
-}
--- bigdata-sails/src/java/com/bigdata/rdf/rio/Foo.java
+++
@@ -1,7 +0,0 @@
-package com.bigdata.rdf.rio;
-
-public interface Foo<T extends Foo<?>> {
-
-    T a();
-    
-}
