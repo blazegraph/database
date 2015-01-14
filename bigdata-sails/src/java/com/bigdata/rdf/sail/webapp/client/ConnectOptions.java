f59514f05e347bf1ac505d8514aad8f3bb24f850
Bryan Thompson <bryan@systap.com>
2015-01-14 12:19:08 -0500

    Rename of ConnectOptions.

---
+++ bigdata-sails/src/java/com/bigdata/rdf/sail/webapp/client/ConnectOptions.java
@@ -0,0 +1,81 @@
+/**
+
+Copyright (C) SYSTAP, LLC 2006-2012.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     4501 Tower Road
+     Greensboro, NC 27410
+     licenses@bigdata.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/*
+ * Created on Mar 27, 2012
+ */
+
+package com.bigdata.rdf.sail.webapp.client;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.message.BasicNameValuePair;
+
+/**
+ * Options for the HTTP connection.
+ */
+public class ConnectOptions extends AbstractConnectOptions {
+
+	/**
+	 * Request entity.
+	 */
+	public HttpEntity entity = null;
+
+	public ConnectOptions(String serviceURL) {
+		super(serviceURL);
+	}
+
+	/**
+	 * Add query params to an {@link IMimeTypes#MIME_APPLICATION_URL_ENCODED}
+	 * entity.
+	 */
+	public static HttpEntity getFormEntity(
+			final Map<String, String[]> requestParams) throws Exception {
+
+		final List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+
+		if (requestParams != null) {
+			for (Map.Entry<String, String[]> e : requestParams.entrySet()) {
+				final String name = e.getKey();
+				final String[] vals = e.getValue();
+
+				if (vals == null) {
+					formparams.add(new BasicNameValuePair(name, null));
+				} else {
+					for (String val : vals) {
+						formparams.add(new BasicNameValuePair(name, val));
+					}
+				}
+			} // next Map.Entry
+		}
+
+		return new UrlEncodedFormEntity(formparams, RemoteRepository.UTF8);
+
+	}
+
+}
