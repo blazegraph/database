0c3295e627fbd012e808460e839a8dd468d97718
Bradley R Bebee <beebs@systap.com>
2015-01-21 21:46:03 -0500

    Update to fix exception when loading larger GraphML files via the Gremlin console.

--- bigdata-blueprints/src/java/com/bigdata/blueprints/BigdataGraphBulkLoad.java
+++ bigdata-blueprints/src/java/com/bigdata/blueprints/BigdataGraphBulkLoad.java
@@ -19,7 +19,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ */
 package com.bigdata.blueprints;
 
 import java.util.Collection;
@@ -33,7 +33,6 @@ import org.openrdf.repository.RepositoryConnection;
 import com.bigdata.rdf.changesets.IChangeLog;
 import com.bigdata.rdf.changesets.IChangeRecord;
 import com.bigdata.rdf.sail.BigdataSailRepositoryConnection;
-import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.GraphQuery;
 import com.tinkerpop.blueprints.TransactionalGraph;
@@ -41,48 +40,48 @@ import com.tinkerpop.blueprints.Vertex;
 
 /**
  * Simple bulk loader that will insert graph data without any consistency
- * checking (won't check for duplicate vertex or edge identifiers).  Currently
+ * checking (won't check for duplicate vertex or edge identifiers). Currently
  * does not overwrite old property values, but we may need to change this.
  * <p>
  * Implements {@link IChangeLog} so that we can report a mutation count.
  * 
  * @author mikepersonick
- *
+ * 
  */
-public class BigdataGraphBulkLoad extends BigdataGraph 
-        implements TransactionalGraph, IChangeLog {
+public class BigdataGraphBulkLoad extends BigdataGraph implements
+		TransactionalGraph, IChangeLog {
 
 	private final BigdataSailRepositoryConnection cxn;
-	
+
 	public BigdataGraphBulkLoad(final BigdataSailRepositoryConnection cxn) {
 		this(cxn, BigdataRDFFactory.INSTANCE);
 	}
-	
-	public BigdataGraphBulkLoad(final BigdataSailRepositoryConnection cxn, 
+
+	public BigdataGraphBulkLoad(final BigdataSailRepositoryConnection cxn,
 			final BlueprintsValueFactory factory) {
-	    super(factory);
-	    
-	    this.cxn = cxn;
-	    this.cxn.addChangeLog(this);
+		super(factory);
+
+		this.cxn = cxn;
+		this.cxn.addChangeLog(this);
 	}
-	
+
 	protected RepositoryConnection getWriteConnection() throws Exception {
-	    return cxn;
+		return cxn;
 	}
-	
-    protected RepositoryConnection getReadConnection() throws Exception {
-        return cxn;
-    }
-    
+
+	protected RepositoryConnection getReadConnection() throws Exception {
+		return cxn;
+	}
+
 	@Override
 	public void commit() {
 		try {
-	        cxn.commit();
+			cxn.commit();
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	@Override
 	public void rollback() {
 		try {
@@ -96,7 +95,7 @@ public class BigdataGraphBulkLoad extends BigdataGraph
 	@Override
 	public void shutdown() {
 		try {
-	        cxn.close();
+			cxn.close();
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
@@ -106,240 +105,264 @@ public class BigdataGraphBulkLoad extends BigdataGraph
 	@Deprecated
 	public void stopTransaction(Conclusion arg0) {
 	}
-	
-	
-    static {
 
-        FEATURES.supportsTransactions = true;
-        
-    }
+	static {
 
+		FEATURES.supportsTransactions = true;
 
-    @Override
-    public Edge getEdge(Object arg0) {
-        throw new UnsupportedOperationException();        
-    }
+	}
 
-    @Override
-    public Iterable<Edge> getEdges() {
-        throw new UnsupportedOperationException();        
-    }
+	@Override
+	public Edge getEdge(Object arg0) {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public Iterable<Edge> getEdges(String arg0, Object arg1) {
-        throw new UnsupportedOperationException();        
-    }
+	@Override
+	public Iterable<Edge> getEdges() {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public Vertex getVertex(Object arg0) {
-        throw new UnsupportedOperationException();        
-    }
+	@Override
+	public Iterable<Edge> getEdges(String arg0, Object arg1) {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public Iterable<Vertex> getVertices() {
-        throw new UnsupportedOperationException();        
-    }
+	@Override
+	public Vertex getVertex(final Object key) {
 
-    @Override
-    public Iterable<Vertex> getVertices(String arg0, Object arg1) {
-        throw new UnsupportedOperationException();        
-    }
+		if (key == null)
+			throw new IllegalArgumentException();
 
-    @Override
-    public GraphQuery query() {
-        throw new UnsupportedOperationException();        
-    }
+		final URI uri = factory.toVertexURI(key.toString());
 
-    @Override
-    public void removeEdge(Edge arg0) {
-        throw new UnsupportedOperationException();        
-    }
+		try {
 
-    @Override
-    public void removeVertex(Vertex arg0) {
-        throw new UnsupportedOperationException();        
-    }
+			try {
 
-    /**
-     * Set a single-value property on an edge or vertex (remove the old
-     * value first).
-     * 
-     * @see {@link BigdataElement}
-     */
-    @Override
-    public void setProperty(final URI uri, final URI prop, final Literal val) {
-        
-        try {
+				if (cxn.hasStatement(uri, TYPE, VERTEX, false)) {
+					return new BigdataVertex(uri, this);
+				}
 
-            final RepositoryConnection cxn = getWriteConnection();
-            
-//            // remove the old value
-//            cxn.remove(uri, prop, null);
-            
-            // add the new value
-            cxn.add(uri, prop, val);
-            
-        } catch (RuntimeException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        
-    }
-    
-    /**
-     * Set a multi-value property on an edge or vertex (remove the old
-     * values first).
-     * 
-     * @see {@link BigdataElement}
-     */
-    @Override
-    public void setProperty(final URI uri, final URI prop, 
-            final Collection<Literal> vals) {
-        
-        try {
+				return null;
 
-            final RepositoryConnection cxn = getWriteConnection();
-            
-//            // remove the old value
-//            cxn.remove(uri, prop, null);
-            
-            // add the new values
-            for (Literal val : vals) {
-                cxn.add(uri, prop, val);
-            }
-            
-        } catch (RuntimeException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        
-    }
-    
-    /**
-     * Add a vertex.
-     */
-    @Override
-    public Vertex addVertex(final Object key) {
-        
-        try {
-            
-            final String vid = key != null ? 
-                    key.toString() : UUID.randomUUID().toString();
-                    
-            final URI uri = factory.toVertexURI(vid);
+			} finally {
 
-            // do we need to check this?
-//          if (cxn().hasStatement(vertexURI, TYPE, VERTEX, false)) {
-//              throw new IllegalArgumentException("vertex " + vid + " already exists");
-//          }
-            
-            getWriteConnection().add(uri, TYPE, VERTEX);
+			}
 
-            return new BigdataVertex(uri, this);
-            
-        } catch (RuntimeException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        
-    }
+		} catch (RuntimeException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    /**
-     * Add an edge.
-     */
-    @Override
-    public Edge addEdge(final Object key, final Vertex from, final Vertex to, 
-            final String label) {
-        
-        if (label == null) {
-            throw new IllegalArgumentException();
-        }
-        
-//        if (key != null && !laxEdges) {
-//            
-//            final Edge edge = getEdge(key);
-//            
-//            if (edge != null) {
-//                if (!(edge.getVertex(Direction.OUT).equals(from) &&
-//                        (edge.getVertex(Direction.IN).equals(to)))) {
-//                    throw new IllegalArgumentException("edge already exists: " + key);
-//                }
-//            }
-//            
-//        }
-            
-        final String eid = key != null ? key.toString() : UUID.randomUUID().toString();
-        
-        final URI edgeURI = factory.toEdgeURI(eid);
+	@Override
+	public Iterable<Vertex> getVertices() {
+		throw new UnsupportedOperationException();
+	}
 
-        try {
-                
-            // do we need to check this?
-//          if (cxn().hasStatement(edgeURI, TYPE, EDGE, false)) {
-//              throw new IllegalArgumentException("edge " + eid + " already exists");
-//          }
+	@Override
+	public Iterable<Vertex> getVertices(String arg0, Object arg1) {
+		throw new UnsupportedOperationException();
+	}
 
-            final URI fromURI = factory.toVertexURI(from.getId().toString());
-            final URI toURI = factory.toVertexURI(to.getId().toString());
-            
-            final RepositoryConnection cxn = getWriteConnection();
-            cxn.add(fromURI, edgeURI, toURI);
-            cxn.add(edgeURI, TYPE, EDGE);
-            cxn.add(edgeURI, LABEL, factory.toLiteral(label));
-            
-            return new BigdataEdge(new StatementImpl(fromURI, edgeURI, toURI), this);
-            
-        } catch (RuntimeException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        
-    }
+	@Override
+	public GraphQuery query() {
+		throw new UnsupportedOperationException();
+	}
 
-    private transient long mutationCountTotal = 0;
-    private transient long mutationCountCurrentCommit = 0;
-    private transient long mutationCountLastCommit = 0;
-    
-    @Override
-    public void changeEvent(final IChangeRecord record) {
-        mutationCountTotal++;
-        mutationCountCurrentCommit++;
-    }
+	@Override
+	public void removeEdge(Edge arg0) {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public void transactionBegin() {
-    }
+	@Override
+	public void removeVertex(Vertex arg0) {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public void transactionPrepare() {
-    }
+	/**
+	 * Set a single-value property on an edge or vertex (remove the old value
+	 * first).
+	 * 
+	 * @see {@link BigdataElement}
+	 */
+	@Override
+	public void setProperty(final URI uri, final URI prop, final Literal val) {
 
-    @Override
-    public void transactionCommited(long commitTime) {
-        mutationCountLastCommit = mutationCountCurrentCommit;
-        mutationCountCurrentCommit = 0;
-    }
+		try {
 
-    @Override
-    public void transactionAborted() {
-    }
+			final RepositoryConnection cxn = getWriteConnection();
 
-    public long getMutationCountTotal() {
-        return mutationCountTotal;
-    }
+			// // remove the old value
+			// cxn.remove(uri, prop, null);
 
-    public long getMutationCountCurrentCommit() {
-        return mutationCountCurrentCommit;
-    }
+			// add the new value
+			cxn.add(uri, prop, val);
 
-    public long getMutationCountLastCommit() {
-        return mutationCountLastCommit;
-    }
+		} catch (RuntimeException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 
+	}
 
+	/**
+	 * Set a multi-value property on an edge or vertex (remove the old values
+	 * first).
+	 * 
+	 * @see {@link BigdataElement}
+	 */
+	@Override
+	public void setProperty(final URI uri, final URI prop,
+			final Collection<Literal> vals) {
+
+		try {
+
+			final RepositoryConnection cxn = getWriteConnection();
+
+			// // remove the old value
+			// cxn.remove(uri, prop, null);
+
+			// add the new values
+			for (Literal val : vals) {
+				cxn.add(uri, prop, val);
+			}
+
+		} catch (RuntimeException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	/**
+	 * Add a vertex.
+	 */
+	@Override
+	public Vertex addVertex(final Object key) {
+
+		try {
+
+			final String vid = key != null ? key.toString() : UUID.randomUUID()
+					.toString();
+
+			final URI uri = factory.toVertexURI(vid);
+
+			// do we need to check this?
+			// if (cxn().hasStatement(vertexURI, TYPE, VERTEX, false)) {
+			// throw new IllegalArgumentException("vertex " + vid +
+			// " already exists");
+			// }
+
+			getWriteConnection().add(uri, TYPE, VERTEX);
+
+			return new BigdataVertex(uri, this);
+
+		} catch (RuntimeException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	/**
+	 * Add an edge.
+	 */
+	@Override
+	public Edge addEdge(final Object key, final Vertex from, final Vertex to,
+			final String label) {
+
+		if (label == null) {
+			throw new IllegalArgumentException();
+		}
+
+		// if (key != null && !laxEdges) {
+		//
+		// final Edge edge = getEdge(key);
+		//
+		// if (edge != null) {
+		// if (!(edge.getVertex(Direction.OUT).equals(from) &&
+		// (edge.getVertex(Direction.IN).equals(to)))) {
+		// throw new IllegalArgumentException("edge already exists: " + key);
+		// }
+		// }
+		//
+		// }
+
+		final String eid = key != null ? key.toString() : UUID.randomUUID()
+				.toString();
+
+		final URI edgeURI = factory.toEdgeURI(eid);
+
+		try {
+
+			// do we need to check this?
+			// if (cxn().hasStatement(edgeURI, TYPE, EDGE, false)) {
+			// throw new IllegalArgumentException("edge " + eid +
+			// " already exists");
+			// }
+
+			final URI fromURI = factory.toVertexURI(from.getId().toString());
+			final URI toURI = factory.toVertexURI(to.getId().toString());
+
+			final RepositoryConnection cxn = getWriteConnection();
+			cxn.add(fromURI, edgeURI, toURI);
+			cxn.add(edgeURI, TYPE, EDGE);
+			cxn.add(edgeURI, LABEL, factory.toLiteral(label));
+
+			return new BigdataEdge(new StatementImpl(fromURI, edgeURI, toURI),
+					this);
+
+		} catch (RuntimeException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	private transient long mutationCountTotal = 0;
+	private transient long mutationCountCurrentCommit = 0;
+	private transient long mutationCountLastCommit = 0;
+
+	@Override
+	public void changeEvent(final IChangeRecord record) {
+		mutationCountTotal++;
+		mutationCountCurrentCommit++;
+	}
+
+	@Override
+	public void transactionBegin() {
+	}
+
+	@Override
+	public void transactionPrepare() {
+	}
+
+	@Override
+	public void transactionCommited(long commitTime) {
+		mutationCountLastCommit = mutationCountCurrentCommit;
+		mutationCountCurrentCommit = 0;
+	}
+
+	@Override
+	public void transactionAborted() {
+	}
+
+	public long getMutationCountTotal() {
+		return mutationCountTotal;
+	}
+
+	public long getMutationCountCurrentCommit() {
+		return mutationCountCurrentCommit;
+	}
+
+	public long getMutationCountLastCommit() {
+		return mutationCountLastCommit;
+	}
 
 }
