e1df6bd84b0b87c548155e89f746672b89a6a8ac
Michael Schmidt <msc@Michaels-MacBook-Pro.local>
2015-02-25 16:47:45 +0100

    ticket #1071
    proposed fix: pass only those variables into join group that are also
    possibly bound there

--- bigdata-rdf/src/java/com/bigdata/rdf/sparql/ast/eval/AST2BOpUtility.java
+++ bigdata-rdf/src/java/com/bigdata/rdf/sparql/ast/eval/AST2BOpUtility.java
@@ -3809,8 +3809,51 @@ public class AST2BOpUtility extends AST2BOpRTO {
                 new NV(JVMHashIndexOp.Annotations.NAMED_SET_REF, namedSolutionSet)//
             ), subgroup, ctx);
         }
+        
+        
+        /*
+         * Only the variables which are projected by the subquery may flow
+         * into the subquery. Adding a projection operator before the
+         * subquery plan ensures that variables which are not visible are
+         * dropped out of the solutions flowing through the subquery.
+         * However, those variables are already present in the hash index so
+         * they can be reunited with the solutions for the subquery in the
+         * solution set hash join at the end of the subquery plan.
+         * 
+         * Note that, when projecting variables inside the subquery, we need
+         * to remove duplicates in the outer solution, to avoid a blowup in
+         * the result size (the inner result is joined with the complete
+         * outer result retained though the previous HashIndexOp at a later
+         * point anyway, what we're doing here just serves the purpose to avoid
+         * the computation of unneeded bindings inside the subclause, in the
+         * sense that we pass in every possible binding once), cf. ticket #835.
+         */
+        final List<NV> anns = new LinkedList<NV>();
+        anns.add(new NV(
+                JVMDistinctBindingSetsOp.Annotations.BOP_ID, ctx.nextId()));
+        anns.add(new NV(
+                JVMDistinctBindingSetsOp.Annotations.VARIABLES, joinVars));
+        anns.add(new NV(
+                JVMDistinctBindingSetsOp.Annotations.EVALUATION_CONTEXT,
+                BOpEvaluationContext.CONTROLLER));
+        anns.add(new NV(
+                JVMDistinctBindingSetsOp.Annotations.SHARED_STATE, true));
 
-        final PipelineOp subqueryPlan = convertJoinGroupOrUnion(op/* left */,
+        left = new JVMDistinctBindingSetsOp(leftOrEmpty(op),//
+                anns.toArray(new NV[anns.size()]));
+        
+        // and put the projection on top
+        left = applyQueryHints(new ProjectionOp(leftOrEmpty(left), //
+                new NV(BOp.Annotations.BOP_ID, ctx.nextId()),//
+                new NV(BOp.Annotations.EVALUATION_CONTEXT,
+                        BOpEvaluationContext.CONTROLLER),//
+                new NV(PipelineOp.Annotations.SHARED_STATE,true),// live stats
+                new NV(ProjectionOp.Annotations.SELECT, joinVars))//
+                , subgroup, ctx);        
+        
+                
+
+        final PipelineOp subqueryPlan = convertJoinGroupOrUnion(left,
                 subgroup, doneSet, ctx);
 
         // lastPass unless this is a normal join.
