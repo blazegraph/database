4391dac102d408ef9a0010cb6288af00c3225016
Bryan Thompson <bryan@systap.com>
2015-03-02 11:21:18 -0500

    Bug fix for OSX vm_stat format changes. See #1125

--- bigdata/src/test/com/bigdata/counters/osx/TestParse_vm_stat.java
+++ bigdata/src/test/com/bigdata/counters/osx/TestParse_vm_stat.java
@@ -122,4 +122,109 @@ public class TestParse_vm_stat extends AbstractParserTestCase {
 
     }
 
+    /**
+     * <pre>
+Mach Virtual Memory Statistics: (page size of 4096 bytes)
+    free   active   specul inactive throttle    wired  prgable   faults     copy    0fill reactive   purged file-backed anonymous cmprssed cmprssor  dcomprs   comprs  pageins  pageout  swapins swapouts
+   17656  1502912   110218  1329298        0   783121    32302 1122834K 22138464  897721K  4538117  2220029      382716   2559712  1093181   449012  1856773  4298532 67248050   263644   779970   886296
+   41993  1479331   110174  1329290        0   782241    33940     2115        1     1316        0        0      382671   2536124  1093181   449012        0        0       24        0        0        0
+     * </pre>
+     * 
+     * @see #1125 (OSX vm_stat output has changed)
+     */
+    public void test_parse_osx_yosemite() {
+
+       final Pattern pattern = VMStatCollector.pattern;
+
+     // first header line.
+     final String h0 = "Mach Virtual Memory Statistics: (page size of 4096 bytes)";
+     // second header line.
+     final String h1 = "    free   active   specul inactive throttle    wired  prgable   faults     copy    0fill reactive   purged file-backed anonymous cmprssed cmprssor  dcomprs   comprs  pageins  pageout  swapins swapouts";
+     // data line
+     final String d1 = "   41993  1479331   110174  1329290        0   782241    33940     2115        1     1316        0        0      382671   2536124  1093181   449012        0        0       24        0        0        0";
+
+       // test header parse.
+       {
+           final String[] fields = pattern.split(h0.trim(), 0/* limit */);
+
+           for (int i = 0; i < fields.length; i++) {
+
+               if(log.isInfoEnabled())
+                    log.info("fields[" + i + "]=[" + fields[i] + "]");
+               
+           }
+           
+           assertField(h0, fields, 0, "Mach"); // marker
+           assertField(h0, fields, 7, "4096"); // page size
+           
+       }
+       {
+        final String[] fields = pattern.split(h1.trim(), 0/* limit */);
+
+           for (int i = 0; i < fields.length; i++) {
+
+               if(log.isInfoEnabled())
+                    log.info("fields[" + i + "]=[" + fields[i] + "]");
+               
+           }
+           
+           assertField(h1, fields, 0, "free");
+           assertField(h1, fields, 1, "active");
+           assertField(h1, fields, 2, "specul");
+           assertField(h1, fields, 3, "inactive");
+           assertField(h1, fields, 4, "throttle");
+           assertField(h1, fields, 5, "wired");
+           assertField(h1, fields, 6, "prgable");
+           assertField(h1, fields, 7, "faults");
+           assertField(h1, fields, 8, "copy");
+           assertField(h1, fields, 9, "0fill");
+           assertField(h1, fields,10, "reactive");
+           assertField(h1, fields,11, "purged");
+           assertField(h1, fields,12, "file-backed");
+           assertField(h1, fields,13, "anonymous");
+           assertField(h1, fields,14, "cmprssed");
+           assertField(h1, fields,15, "cmprssor");
+           assertField(h1, fields,16, "dcomprs");
+           assertField(h1, fields,17, "comprs");
+           assertField(h1, fields,18, "pageins");
+           assertField(h1, fields,19, "pageout");
+           assertField(h1, fields,20, "swapins");
+           assertField(h1, fields,21, "swapouts");
+
+     }
+
+     // test data parse.
+//     final String h1 = "    free   active   specul inactive throttle    wired  prgable   faults     copy    0fill reactive   purged file-backed anonymous cmprssed cmprssor  dcomprs   comprs  pageins  pageout  swapins swapouts";
+//     final String d1 = "   41993  1479331   110174  1329290        0   782241    33940     2115        1     1316        0        0      382671   2536124  1093181   449012        0        0       24        0        0        0";
+     {
+
+        final String[] fields = pattern.split(d1.trim(), 0/* limit */);
+
+        assertField(d1, fields, 0, "41993");  // free
+        assertField(d1, fields, 1, "1479331");   // active
+        assertField(d1, fields, 2, "110174");   // specul
+        assertField(d1, fields, 3, "1329290");  // inactive
+        assertField(d1, fields, 4, "0");  // throttle
+        assertField(d1, fields, 5, "782241");  // wired
+        assertField(d1, fields, 6, "33940");  // prgable
+        assertField(d1, fields, 7, "2115"); // faults
+        assertField(d1, fields, 8, "1"); // copy
+        assertField(d1, fields, 9, "1316");// 0fill
+        assertField(d1, fields,10, "0"); // reactive
+        assertField(d1, fields,11, "0"); // purged
+        assertField(d1, fields,12, "382671"); // file-backed
+        assertField(d1, fields,13, "2536124"); // anonymous
+        assertField(d1, fields,14, "1093181"); // cmprssed
+        assertField(d1, fields,15, "449012"); // cmprssor
+        assertField(d1, fields,16, "0"); // dcomprs
+        assertField(d1, fields,17, "0"); // comprs
+        assertField(d1, fields,18, "24");  // pageins
+        assertField(d1, fields,19, "0");  // pageout
+        assertField(d1, fields,20, "0"); // swapins
+        assertField(d1, fields,21, "0"); // swapouts
+
+     }
+       
+    }
+    
 }
