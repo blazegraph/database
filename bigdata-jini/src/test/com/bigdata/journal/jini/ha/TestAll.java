d5b65ae9fd12e8328f57061fb8cf8b35e50bcb3d
Bryan Thompson <bryan@systap.com>
2015-03-06 13:40:58 -0500

    Integrating the HA1/HA3 group commit test suite into CI.

--- bigdata-jini/src/test/com/bigdata/journal/jini/ha/TestAll.java
+++ bigdata-jini/src/test/com/bigdata/journal/jini/ha/TestAll.java
@@ -115,8 +115,8 @@ public class TestAll extends TestCase {
         suite.addTestSuite(TestHAJournalServerOverride.class); 
 
         // Test suite for concurrent writers and group commit.
-//        suite.addTestSuite(TestHA1GroupCommit.class);
-//        suite.addTestSuite(TestHA3GroupCommit.class);
+        suite.addTestSuite(TestHA1GroupCommit.class);
+        suite.addTestSuite(TestHA3GroupCommit.class);
 
         // The HA load balancer test suite.
         suite.addTest(TestAll_LBS.suite());
