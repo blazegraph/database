9dffaa4338b0f6b61aec02f80ce0d8ea05da63bd
Bryan Thompson <bryan@systap.com>
2015-04-21 16:55:55 -0400

    Refactored the BigdataSailFactory and BigdataGraphFactory per proposal.
    https://docs.google.com/a/systap.com/document/d/1rzLfdYZSqnXeualuRVD-ai-
    vgX_Lk4QZe5uj3Ge6Xzc/edit?usp=sharing

--- bigdata-client/src/main/java/com/bigdata/rdf/sail/webapp/client/RemoteRepositoryManager.java
+++ bigdata-client/src/main/java/com/bigdata/rdf/sail/webapp/client/RemoteRepositoryManager.java
@@ -465,6 +465,23 @@ public class RemoteRepositoryManager extends RemoteRepositoryBase implements
 
    }
 
+//   FIXME Should we be doing this? Are we leaking HttpClient or Executor resources otherwise?
+//   /**
+//    * {@inheritDoc}
+//    * <p>
+//    * Ensure resource is closed.
+//    * 
+//    * @see AutoCloseable
+//    */
+//   @Override
+//   protected void finalize() throws Throwable {
+//
+//      close();
+//      
+//      super.finalize();
+//      
+//   }
+   
    @Override
    public void close() throws Exception {
 
