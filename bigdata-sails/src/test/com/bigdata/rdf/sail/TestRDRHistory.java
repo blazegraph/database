4ef85425bdb75f3d52d846ee19c8514f362b0ff1
mpersonick <mpersonick@px.ftw>
2015-04-22 15:15:01 -0600

    Checkpoint commit.

--- bigdata-sails/src/test/com/bigdata/rdf/sail/TestRDRHistory.java
+++ bigdata-sails/src/test/com/bigdata/rdf/sail/TestRDRHistory.java
@@ -29,14 +29,19 @@ package com.bigdata.rdf.sail;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
+import org.openrdf.model.Literal;
+import org.openrdf.model.Statement;
 import org.openrdf.model.URI;
 import org.openrdf.model.impl.URIImpl;
+import org.openrdf.repository.RepositoryResult;
 
 import com.bigdata.rdf.axioms.NoAxioms;
+import com.bigdata.rdf.changesets.IChangeRecord;
+import com.bigdata.rdf.internal.XSD;
+import com.bigdata.rdf.model.BigdataBNode;
 import com.bigdata.rdf.model.BigdataStatement;
 import com.bigdata.rdf.model.BigdataValueFactory;
 import com.bigdata.rdf.store.AbstractTripleStore;
-import com.bigdata.rdf.vocab.NoVocabulary;
 
 /**
  * Test suite {@link RDRHistory}.
@@ -56,13 +61,13 @@ public class TestRDRHistory extends ProxyBigdataSailTestCase {
         Properties props = super.getProperties();
         
         // no inference
-//        props.setProperty(BigdataSail.Options.TRUTH_MAINTENANCE, "false");
-//        props.setProperty(BigdataSail.Options.AXIOMS_CLASS, NoAxioms.class.getName());
-//        props.setProperty(BigdataSail.Options.JUSTIFY, "false");
-//        props.setProperty(BigdataSail.Options.TEXT_INDEX, "false");
+        props.setProperty(BigdataSail.Options.TRUTH_MAINTENANCE, "false");
+        props.setProperty(BigdataSail.Options.AXIOMS_CLASS, NoAxioms.class.getName());
+        props.setProperty(BigdataSail.Options.JUSTIFY, "false");
+        props.setProperty(BigdataSail.Options.TEXT_INDEX, "false");
         
         // turn on RDR history
-//        props.setProperty(AbstractTripleStore.Options.RDR_HISTORY_CLASS, cls.getName());
+        props.setProperty(AbstractTripleStore.Options.RDR_HISTORY_CLASS, cls.getName());
         
         return props;
         
@@ -83,9 +88,9 @@ public class TestRDRHistory extends ProxyBigdataSailTestCase {
 
     
     /**
-     * 
+     * Test basic add/remove.
      */
-    public void testAdd() throws Exception {
+    public void testAddAndRemove() throws Exception {
 
         BigdataSailRepositoryConnection cxn = null;
 
@@ -103,23 +108,59 @@ public class TestRDRHistory extends ProxyBigdataSailTestCase {
             final URI p = vf.createURI(":p");
             final URI o = vf.createURI(":o");
 
-//            BigdataStatement stmt = vf.createStatement(s, p, o);
-//            cxn.add(stmt);
-//            cxn.commit();
+            BigdataStatement stmt = vf.createStatement(s, p, o);
+            cxn.add(stmt);
+            cxn.commit();
             
-//            if (log.isInfoEnabled()) {
-//                log.info(cxn.getTripleStore().dumpStore().insert(0,'\n'));
-//            }
+            if (log.isInfoEnabled()) {
+                log.info(cxn.getTripleStore().dumpStore().insert(0,'\n'));
+            }
             
-//            RepositoryResult<Statement> stmts = cxn.getStatements(null, null,
-//                  null, false);
-//            while (stmts.hasNext()) {
-//                Statement res = stmts.next();
-//                if (log.isInfoEnabled()) {
-//                    log.info(res);
-//                }
-//            }
+            assertTrue(cxn.size() == 2);
 
+            {
+                RepositoryResult<Statement> stmts = cxn.getStatements(
+                        s, p, o, false);
+                assertTrue(stmts.hasNext());
+                stmts.close();
+            }
+
+            {
+                BigdataBNode sid = vf.createBNode(stmt);
+                RepositoryResult<Statement> stmts = cxn.getStatements(
+                        sid, MyRDRHistory.ADDED, null, false);
+                assertTrue(stmts.hasNext());
+                Literal l = (Literal) stmts.next().getObject();
+                assertTrue(l.getDatatype().equals(XSD.DATETIME));
+                stmts.close();
+            }
+            
+            cxn.remove(stmt);
+            cxn.commit();
+
+            if (log.isInfoEnabled()) {
+                log.info(cxn.getTripleStore().dumpStore().insert(0,'\n'));
+            }
+            
+            assertTrue(cxn.size() == 2);
+
+            {
+                RepositoryResult<Statement> stmts = cxn.getStatements(
+                        s, p, o, false);
+                assertFalse(stmts.hasNext());
+                stmts.close();
+            }
+
+            {
+                BigdataBNode sid = vf.createBNode(stmt);
+                RepositoryResult<Statement> stmts = cxn.getStatements(
+                        sid, MyRDRHistory.REMOVED, null, false);
+                assertTrue(stmts.hasNext());
+                Literal l = (Literal) stmts.next().getObject();
+                assertTrue(l.getDatatype().equals(XSD.DATETIME));
+                stmts.close();
+            }
+            
         } finally {
             if (cxn != null)
                 cxn.close();
@@ -128,7 +169,81 @@ public class TestRDRHistory extends ProxyBigdataSailTestCase {
         }
     }
     
-    public static final class MyRDRHistory extends RDRHistory {
+    /**
+     * Test custom history handler.
+     */
+    public void testCustomHistory() throws Exception {
+
+        BigdataSailRepositoryConnection cxn = null;
+
+        final BigdataSail sail = getSail(getProperties(CustomRDRHistory.class));
+
+        try {
+
+            sail.initialize();
+            final BigdataSailRepository repo = new BigdataSailRepository(sail);
+            cxn = (BigdataSailRepositoryConnection) repo.getConnection();
+
+            final BigdataValueFactory vf = (BigdataValueFactory) sail
+                  .getValueFactory();
+            final URI s = vf.createURI(":s");
+            final URI p = vf.createURI(":p");
+            final URI o = vf.createURI(":o");
+            final Literal l = vf.createLiteral("o");
+
+            BigdataStatement stmt1 = vf.createStatement(s, p, o);
+            BigdataStatement stmt2 = vf.createStatement(s, p, l);
+            cxn.add(stmt1);
+            cxn.add(stmt2);
+            cxn.commit();
+            
+            if (log.isInfoEnabled()) {
+                log.info(cxn.getTripleStore().dumpStore().insert(0,'\n'));
+            }
+            
+            assertTrue(cxn.size() == 3);
+
+            {
+                RepositoryResult<Statement> stmts = cxn.getStatements(
+                        s, p, o, false);
+                assertTrue(stmts.hasNext());
+                stmts.close();
+            }
+
+            {
+                RepositoryResult<Statement> stmts = cxn.getStatements(
+                        s, p, l, false);
+                assertTrue(stmts.hasNext());
+                stmts.close();
+            }
+
+            {
+                BigdataBNode sid = vf.createBNode(stmt1);
+                RepositoryResult<Statement> stmts = cxn.getStatements(
+                        sid, MyRDRHistory.ADDED, null, false);
+                assertFalse(stmts.hasNext());
+                stmts.close();
+            }
+            
+            {
+                BigdataBNode sid = vf.createBNode(stmt2);
+                RepositoryResult<Statement> stmts = cxn.getStatements(
+                        sid, MyRDRHistory.ADDED, null, false);
+                assertTrue(stmts.hasNext());
+                Literal l2 = (Literal) stmts.next().getObject();
+                assertTrue(l2.getDatatype().equals(XSD.DATETIME));
+                stmts.close();
+            }
+            
+        } finally {
+            if (cxn != null)
+                cxn.close();
+            
+            sail.__tearDownUnitTest();
+        }
+    }
+    
+    public static class MyRDRHistory extends RDRHistory {
 
         public static final URI ADDED = new URIImpl(":added");
         
@@ -150,4 +265,20 @@ public class TestRDRHistory extends ProxyBigdataSailTestCase {
         
     }
 
+    public static class CustomRDRHistory extends MyRDRHistory {
+
+        public CustomRDRHistory(AbstractTripleStore database) {
+            super(database);
+        }
+
+        /**
+         * Only accept stmts where isLiteral(stmt.o)
+         */
+        @Override
+        protected boolean accept(final IChangeRecord record) {
+            return record.getStatement().o().isLiteral();
+        }
+        
+    }
+    
 }
