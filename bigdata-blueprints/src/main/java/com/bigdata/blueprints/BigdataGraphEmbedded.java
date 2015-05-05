4749f664164058aff414b19e65866300025bcba5
mpersonick <mpersonick@px.ftw>
2015-05-05 11:52:48 -0600

    Closing the read connections on parse error.

--- bigdata-blueprints/src/main/java/com/bigdata/blueprints/BigdataGraphEmbedded.java
+++ bigdata-blueprints/src/main/java/com/bigdata/blueprints/BigdataGraphEmbedded.java
@@ -175,6 +175,7 @@ public class BigdataGraphEmbedded extends BigdataGraph implements TransactionalG
 //		    if (cxn != null) {
 //		        cxn.close();
 //		    }
+		    // commit shuts down cxn
 			commit();
 			repo.shutDown();
 		} catch (Exception e) {
