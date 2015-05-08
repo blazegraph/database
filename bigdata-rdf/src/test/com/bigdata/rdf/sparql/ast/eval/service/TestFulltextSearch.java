ca0633424937765ad2e4f69bf455cd1fdca9c0fb
Michael Schmidt <ms@metaphacts.com>
2015-05-08 12:53:54 +0200

    ticket #1199: changed documentation in test class

--- bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/eval/service/TestFulltextSearch.java
+++ bigdata-rdf/src/test/com/bigdata/rdf/sparql/ast/eval/service/TestFulltextSearch.java
@@ -28,9 +28,12 @@ import com.bigdata.service.fts.FulltextSearchException;
 
 /**
  * Data driven test suite for external full text search. At the time being,
- * this test suite requires a local Solr index set up at port 1234, with
+ * this test suite requires a local Solr index set up at 
+ * http://localhost:8983/solr/solrtest/select, with
  * a collection called "blazegraph" and the file fts-solr-collection.xml 
- * loaded into this collection.
+ * loaded into this collection. There's a small mvn project for automizing 
+ * the Solr index setup checked in at /src/build/solr, see the README file
+ * there.
  * 
  * @author <a href="mailto:ms@metaphacts.com">Michael Schmidt</a>
  * @version $Id$
@@ -45,7 +48,7 @@ public class TestFulltextSearch extends AbstractDataDrivenSPARQLTestCase {
 
     /**
      * @param name
-     */
+     */ 
     public TestFulltextSearch(String name) {
         super(name);
     }
