6767b71f545eba176e935fb0a7ad21370d85f978
Bradley R Bebee <beebs@systap.com>
2015-05-25 20:24:39 -0400

    Update to refactor BlueprintsServlet into the bigdata-blueprints module/component.

--- bigdata-blueprints/src/main/java/com/bigdata/blueprints/webapp/BlueprintsServlet.java
+++ bigdata-blueprints/src/main/java/com/bigdata/blueprints/webapp/BlueprintsServlet.java
@@ -20,7 +20,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-package com.bigdata.rdf.sail.webapp;
+package com.bigdata.blueprints.webapp;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -33,7 +33,9 @@ import org.apache.log4j.Logger;
 
 import com.bigdata.blueprints.BigdataGraphBulkLoad;
 import com.bigdata.rdf.sail.BigdataSailRepositoryConnection;
+import com.bigdata.rdf.sail.webapp.BigdataRDFServlet;
 import com.bigdata.rdf.sail.webapp.client.MiniMime;
+import com.bigdata.rdf.sail.webapp.AbstractRestApiTask;
 import com.tinkerpop.blueprints.util.io.graphml.GraphMLReader;
 
 /**
@@ -55,7 +57,7 @@ public class BlueprintsServlet extends BigdataRDFServlet {
     /**
      * Flag to signify a blueprints operation.
      */
-    static final transient String ATTR_BLUEPRINTS = "blueprints";
+    public static final transient String ATTR_BLUEPRINTS = "blueprints";
 
     public BlueprintsServlet() {
 
@@ -156,5 +158,19 @@ public class BlueprintsServlet extends BigdataRDFServlet {
         }
 
     }
+
+    /**
+     * 
+     * Convenience method to access doPost from a public method.
+     * 
+     * @param req
+     * @param resp
+     * @throws IOException 
+     */
+	public void doPostRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+		doPost(req, resp);
+		
+	}
 
 }
