6ef307ab14cab38ea517d688a0dc947df7c1af8c
Bradley R Bebee <beebs@systap.com>
2015-06-29 14:36:46 -0400

    BLZG-1269 commit for a manual merge into master to rebaseline the maven branch into master.

---
+++ bigdata-client/src/test/java/com/bigdata/rdf/sail/remote/TestAll.java
@@ -0,0 +1,70 @@
+package com.bigdata.rdf.sail.remote;
+
+/**
+Copyright (C) SYSTAP, LLC 2006-2015.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     2501 Calvert ST NW #106
+     Washington, DC 20008
+     licenses@systap.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/*
+ * Created on April 20, 2015
+ */
+
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import com.bigdata.rdf.sail.webapp.TestNanoSparqlServer;
+
+/**
+ * Test suite.
+ * 
+ * @author <a href="mailto:beebs@users.sourceforge.net">Brad Bebee</a>
+ */
+public class TestAll extends TestCase {
+
+    /**
+     * 
+     */
+    public TestAll() {
+        super();
+    }
+
+    /**
+     * @param arg0
+     */
+    public TestAll(String arg0) {
+        super(arg0);
+    }
+
+    public static Test suite() {
+
+        final TestSuite suite = new TestSuite("WebApp");
+
+        // Test suite of NSS startup behavior and overrides.
+        //suite.addTestSuite(TestNanoSparqlServer.class);
+        
+        suite.addTestSuite(BigdataSailFactoryTest.class);
+
+        return suite;
+
+    }
+
+}
