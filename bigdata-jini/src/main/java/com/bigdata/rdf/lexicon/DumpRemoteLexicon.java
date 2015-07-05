300616459dfba023b7298acfe92e14e64dffe7e6
Bradley R Bebee <beebs@systap.com>
2015-07-05 16:11:05 -0400

    Checkout point commit for refactoring of the bigdata-jini package dependencies.

---
+++ bigdata-jini/src/main/java/com/bigdata/rdf/lexicon/DumpRemoteLexicon.java
@@ -0,0 +1,91 @@
+package com.bigdata.rdf.lexicon;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Properties;
+
+import com.bigdata.btree.BytesUtil;
+import com.bigdata.btree.IIndex;
+import com.bigdata.btree.ITuple;
+import com.bigdata.btree.ITupleIterator;
+import com.bigdata.journal.IIndexManager;
+import com.bigdata.journal.IJournal;
+import com.bigdata.journal.ITx;
+import com.bigdata.journal.Journal;
+import com.bigdata.journal.TimestampUtility;
+import com.bigdata.rawstore.Bytes;
+import com.bigdata.rdf.internal.IVUtility;
+import com.bigdata.rdf.internal.impl.BlobIV;
+import com.bigdata.rdf.internal.impl.TermId;
+import com.bigdata.rdf.model.BigdataValue;
+import com.bigdata.rdf.model.BigdataValueFactory;
+import com.bigdata.rdf.model.BigdataValueFactoryImpl;
+import com.bigdata.rdf.model.BigdataValueSerializer;
+import com.bigdata.rdf.sail.BigdataSail;
+import com.bigdata.rdf.store.AbstractTripleStore;
+import com.bigdata.service.IBigdataClient;
+import com.bigdata.service.IBigdataFederation;
+import com.bigdata.service.jini.JiniClient;
+
+/**
+ * Utility class to dump the TERMS index of a triple store.
+ * 
+ * BLZG-1370  Moved into bigdata-jini package for the remove functionality.
+ * 
+ * Includes the functionality of the base
+ * 
+ * @author thompsonbry
+ */
+public class DumpRemoteLexicon  extends DumpLexicon {
+
+    /**
+     * Open the {@link IIndexManager} identified by the property file.
+     * 
+     * @param propertyFile
+     *            The property file (for a standalone bigdata instance) or the
+     *            jini configuration file (for a bigdata federation). The file
+     *            must end with either ".properties" or ".config".
+     *            
+     * @return The {@link IIndexManager}.
+     */
+    protected static IIndexManager openIndexManager(final String propertyFile) {
+
+        final File file = new File(propertyFile);
+
+        if (!file.exists()) {
+
+            throw new RuntimeException("Could not find file: " + file);
+
+        }
+
+        final IIndexManager indexManager;
+
+        try {
+        if (propertyFile.endsWith(".config")) {
+            // scale-out with Jini
+                @SuppressWarnings("unchecked")
+                final JiniClient<?> jiniClient = new JiniClient(
+                        new String[] { propertyFile });
+
+                indexManager = jiniClient.connect();
+        } else {
+        	indexManager = DumpLexicon.openIndexManager(propertyFile);
+        }
+        
+
+        } catch (Exception ex) {
+
+            throw new RuntimeException(ex);
+
+        }
+
+        return indexManager;
+        
+    }
+}
--- bigdata/src/java/com/bigdata/service/master/AbstractResourceScanner.java
+++ bigdata/src/java/com/bigdata/service/master/AbstractResourceScanner.java
@@ -25,7 +25,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * Created on Jul 11, 2009
  */
 
-package com.bigdata.service.jini.master;
+package com.bigdata.service.master;
 
 import java.util.LinkedList;
 import java.util.concurrent.ArrayBlockingQueue;
