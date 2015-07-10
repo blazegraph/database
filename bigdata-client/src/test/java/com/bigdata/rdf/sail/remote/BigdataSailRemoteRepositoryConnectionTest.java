52ca997250ff62d81c69e03013071b20c741eede
Igor Kim <igor.kim@ms2w.com>
2015-07-11 00:43:53 +0600

    close query results in test, add test for client-side getStatements()

--- bigdata-client/src/test/java/com/bigdata/rdf/sail/remote/BigdataSailRemoteRepositoryConnectionTest.java
+++ bigdata-client/src/test/java/com/bigdata/rdf/sail/remote/BigdataSailRemoteRepositoryConnectionTest.java
@@ -6,16 +6,21 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openrdf.http.protocol.Protocol;
+import org.openrdf.model.Resource;
+import org.openrdf.model.Statement;
 import org.openrdf.model.URI;
 import org.openrdf.model.Value;
 import org.openrdf.model.impl.URIImpl;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
 import org.openrdf.query.impl.DatasetImpl;
 import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.RepositoryResult;
 
 import com.bigdata.rdf.sail.webapp.QueryServlet;
 import com.bigdata.rdf.sail.webapp.client.MockRemoteRepository;
+import com.bigdata.rdf.sail.webapp.client.RemoteRepositoryDecls;
 
 public class BigdataSailRemoteRepositoryConnectionTest {
 	
@@ -45,12 +50,15 @@ public class BigdataSailRemoteRepositoryConnectionTest {
 		Value p = new URIImpl(":p");
 		tq.setBinding("s", s);
 		tq.setBinding("p", p);
-		tq.evaluate();
-		assertEquals("<:s>",remote.data.opts.getRequestParam("$s"));
-		assertEquals("<:p>",remote.data.opts.getRequestParam("$p"));
-		assertEquals("<:s>",remote.data.request.getParams().get("$s").getValue());
-		assertEquals("<:p>",remote.data.request.getParams().get("$p").getValue());
-		System.out.println("Done bindings");
+		TupleQueryResult tqr = tq.evaluate();
+		try {
+			assertEquals("<:s>",remote.data.opts.getRequestParam("$s"));
+			assertEquals("<:p>",remote.data.opts.getRequestParam("$p"));
+			assertEquals("<:s>",remote.data.request.getParams().get("$s").getValue());
+			assertEquals("<:p>",remote.data.request.getParams().get("$p").getValue());
+		} finally {
+			tqr.close();
+		}
 	}
 
 	@Test
@@ -58,14 +66,17 @@ public class BigdataSailRemoteRepositoryConnectionTest {
 		TupleQuery tq = con.prepareTupleQuery(QueryLanguage.SPARQL, "select * from {?s ?p ?o}");
 		tq.setIncludeInferred(false);
 		tq.evaluate();
-		assertEquals("false", remote.data.opts.getRequestParam(QueryServlet.INCLUDE_INFERRED));
-		assertEquals("false", remote.data.request.getParams().get(QueryServlet.INCLUDE_INFERRED).getValue());
+		assertEquals("false", remote.data.opts.getRequestParam(RemoteRepositoryDecls.INCLUDE_INFERRED));
+		assertEquals("false", remote.data.request.getParams().get(RemoteRepositoryDecls.INCLUDE_INFERRED).getValue());
 		
 		tq.setIncludeInferred(true);
-		tq.evaluate();
-		assertEquals("true", remote.data.opts.getRequestParam(QueryServlet.INCLUDE_INFERRED));
-		assertEquals("true", remote.data.request.getParams().get(QueryServlet.INCLUDE_INFERRED).getValue());
-		System.out.println("Done inferred");
+		TupleQueryResult tqr = tq.evaluate();
+		try {
+			assertEquals("true", remote.data.opts.getRequestParam(RemoteRepositoryDecls.INCLUDE_INFERRED));
+			assertEquals("true", remote.data.request.getParams().get(RemoteRepositoryDecls.INCLUDE_INFERRED).getValue());
+		} finally {
+			tqr.close();
+		}
 	}
 	
 	@Test
@@ -81,24 +92,47 @@ public class BigdataSailRemoteRepositoryConnectionTest {
 		dataset.addNamedGraph(namedGraph1);
 		dataset.addNamedGraph(namedGraph2);
 		tq.setDataset(dataset);
-		tq.evaluate();
-		assertEquals(defaultGraph1.stringValue(),remote.data.opts.getRequestParam(Protocol.DEFAULT_GRAPH_PARAM_NAME));
-		assertEquals(defaultGraph1.stringValue(),remote.data.request.getParams().get(Protocol.DEFAULT_GRAPH_PARAM_NAME).getValue());
-		assertEquals(namedGraph1.stringValue(),remote.data.opts.getRequestParam(Protocol.NAMED_GRAPH_PARAM_NAME));
-		assertEquals(namedGraph1.stringValue(),remote.data.request.getParams().get(Protocol.NAMED_GRAPH_PARAM_NAME).getValue());
-	
-		System.out.println("Done dataset");
+		TupleQueryResult tqr = tq.evaluate();
+		try {
+			assertEquals(defaultGraph1.stringValue(),remote.data.opts.getRequestParam(Protocol.DEFAULT_GRAPH_PARAM_NAME));
+			assertEquals(defaultGraph1.stringValue(),remote.data.request.getParams().get(Protocol.DEFAULT_GRAPH_PARAM_NAME).getValue());
+			assertEquals(namedGraph1.stringValue(),remote.data.opts.getRequestParam(Protocol.NAMED_GRAPH_PARAM_NAME));
+			assertEquals(namedGraph1.stringValue(),remote.data.request.getParams().get(Protocol.NAMED_GRAPH_PARAM_NAME).getValue());
+		} finally {
+			tqr.close();
+		}
 	}
 
 	@Test
 	public void testTupleQueryBaseURI() throws Exception {
 		String baseURI = ":baseURI";
 		TupleQuery tq = con.prepareTupleQuery(QueryLanguage.SPARQL, "select * from {?s ?p ?o}", baseURI);
-		tq.evaluate();
-		assertEquals(baseURI, remote.data.opts.getRequestParam(Protocol.BASEURI_PARAM_NAME));
-		assertEquals(baseURI,remote.data.opts.getRequestParam(Protocol.BASEURI_PARAM_NAME));
-	
-		System.out.println("Done baseURI");
+		TupleQueryResult tqr = tq.evaluate();
+		try {
+			assertEquals(baseURI, remote.data.opts.getRequestParam(Protocol.BASEURI_PARAM_NAME));
+			assertEquals(baseURI,remote.data.opts.getRequestParam(Protocol.BASEURI_PARAM_NAME));
+		} finally {
+			tqr.close();
+		}
 	}
 
+	@Test
+	public void testGetStatements() throws RepositoryException {
+		Resource s = new URIImpl(":s");
+		URI p = new URIImpl(":p");
+		Value o = new URIImpl(":o");
+		boolean includeInferred = false;
+		Resource c = new URIImpl(":c");
+		RepositoryResult<Statement> stmts = con.getStatements(s, p, o, includeInferred, c);
+		try {
+			assertEquals("<:s>", remote.data.opts.getRequestParam("s"));
+			assertEquals("<:p>", remote.data.opts.getRequestParam("p"));
+			assertEquals("<:o>", remote.data.opts.getRequestParam("o"));
+			assertEquals("<:c>", remote.data.opts.getRequestParam("c"));
+			assertEquals(Boolean.toString(includeInferred), remote.data.opts.getRequestParam(RemoteRepositoryDecls.INCLUDE_INFERRED));
+		} finally {
+			stmts.close();
+		}
+	}
+	
 }
