0892cc71168254717525051a7f370d66c53a7f6c
Igor Kim <igor.kim@ms2w.com>
2015-07-11 00:16:12 +0600

    Tear down BigdataSailRemoteRepositoryConnectionTest

--- bigdata-client/src/test/java/com/bigdata/rdf/sail/remote/BigdataSailRemoteRepositoryConnectionTest.java
+++ bigdata-client/src/test/java/com/bigdata/rdf/sail/remote/BigdataSailRemoteRepositoryConnectionTest.java
@@ -2,6 +2,7 @@ package com.bigdata.rdf.sail.remote;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openrdf.http.protocol.Protocol;
@@ -11,6 +12,7 @@ import org.openrdf.model.impl.URIImpl;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.TupleQuery;
 import org.openrdf.query.impl.DatasetImpl;
+import org.openrdf.repository.RepositoryException;
 
 import com.bigdata.rdf.sail.webapp.QueryServlet;
 import com.bigdata.rdf.sail.webapp.client.MockRemoteRepository;
@@ -19,12 +21,21 @@ public class BigdataSailRemoteRepositoryConnectionTest {
 	
 	private BigdataSailRemoteRepositoryConnection con;
 	private MockRemoteRepository remote;
+	private BigdataSailRemoteRepository repo;
 
 	@Before
 	public void prepare() {
 		remote = MockRemoteRepository.create("{\"head\":{\"vars\":[\"s\"]}}");
-		BigdataSailRemoteRepository repo = remote.getBigdataSailRemoteRepository();
+		repo = remote.getBigdataSailRemoteRepository();
 		con = new BigdataSailRemoteRepositoryConnection(repo);
+	}
+	
+	@After
+	public void tearDown() throws RepositoryException {
+		con.close();
+		con = null;
+		repo.shutDown();
+		remote = null;
 	}
 
 	@Test
