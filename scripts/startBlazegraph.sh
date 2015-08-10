f45436f5928f3af3813163d6a1c43767f346342f
beebs-systap <beebs@systap.com>
2015-08-10 15:57:45 -0400

    Updates for starting blazegraph.

--- scripts/startBlazegraph.sh
+++ scripts/startBlazegraph.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-mvn -f bigdata-jar/pom.xml package
+PORT=9999
+PROPERTIES_FILE=./bigdata-war-html/src/main/webapp/WEB-INF/RWStore.properties
+NAMESPACE="kb"
+JETTY_XML=./bigdata-war-html/src/main/resources/jetty.xml
+JETTY_RESOURCE_BASE=./bigdata-war-html/src/main/webapp/
+
+mvn -f bigdata-jar/pom.xml clean package
 
 JAVA_OPTS="$JAVA_OPTS -ea -Xmx4g -server -XX:+UseParallelOldGC"
 
-$JAVA_HOME/bin/java -jar bigdata-jar/target/bigdata-jar*.jar
+
+if [ -z $JAVA_HOME ] ; then
+	JAVA=${JAVA_HOME}/bin/java
+else
+	JAVA=`which java`
+fi
+
+$JAVA ${JAVA_OPTS} -jar bigdata-jar/target/bigdata-jar*.jar
+
+#$JAVA ${JAVA_OPTS} -cp bigdata-jar/target/bigdata-jar*.jar -Djetty.resourceBase=$JETTY_RESOURCE_BASE -DjettyXml=$JETTY_XML com.bigdata.rdf.sail.webapp.NanoSparqlServer $PORT $NAMESPACE $PROPERTIES_FILE 
+
