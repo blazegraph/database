accb844bf05c076d5c56ef034c63eb18a3315395
beebs-systap <beebs@systap.com>
2015-08-11 14:57:02 -0400

    Updates for startup scripts.

---
+++ scripts/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+JETTY_XML=./bigdata-war-html/src/main/resources/jetty.xml
+JETTY_RESOURCE_BASE=./bigdata-war-html/src/main/webapp/
+
+mvn -f bigdata-jar/pom.xml clean package
+
+if [ -z $JAVA_OPTS ] ; then
+	JAVA_OPTS="-ea -Xmx4g -server"
+fi
+
+if [ -z $JAVA_HOME ] ; then
+	JAVA=${JAVA_HOME}/bin/java
+else
+	JAVA=`which java`
+fi
+
+#$JAVA ${JAVA_OPTS} -jar bigdata-jar/target/bigdata-jar*.jar
+
+echo "Starting with JAVA_OPTS: $JAVA_OPTS."
+
+$JAVA ${JAVA_OPTS} -cp bigdata-jar/target/bigdata-jar*.jar -Djetty.resourceBase=$JETTY_RESOURCE_BASE -DjettyXml=$JETTY_XML com.bigdata.rdf.sail.webapp.NanoSparqlServer $*
+
--- scripts/startBlazegraph.sh
+++ scripts/startBlazegraph.sh
@@ -8,16 +8,19 @@ JETTY_RESOURCE_BASE=./bigdata-war-html/src/main/webapp/
 
 mvn -f bigdata-jar/pom.xml clean package
 
-JAVA_OPTS="$JAVA_OPTS -ea -Xmx4g -server -XX:+UseParallelOldGC"
+if [ -z "${JAVA_OPTS}" ] ; then
+	JAVA_OPTS="-ea -Xmx4g -server"
+fi
 
-
-if [ -z $JAVA_HOME ] ; then
+if [ -z ${JAVA_HOME} ] ; then
 	JAVA=${JAVA_HOME}/bin/java
 else
 	JAVA=`which java`
 fi
 
-$JAVA ${JAVA_OPTS} -jar bigdata-jar/target/bigdata-jar*.jar
+#$JAVA ${JAVA_OPTS} -jar bigdata-jar/target/bigdata-jar*.jar
 
-#$JAVA ${JAVA_OPTS} -cp bigdata-jar/target/bigdata-jar*.jar -Djetty.resourceBase=$JETTY_RESOURCE_BASE -DjettyXml=$JETTY_XML com.bigdata.rdf.sail.webapp.NanoSparqlServer $PORT $NAMESPACE $PROPERTIES_FILE 
+echo "Starting with JAVA_OPTS: $JAVA_OPTS."
+
+$JAVA ${JAVA_OPTS} -cp bigdata-jar/target/bigdata-jar*.jar -Djetty.resourceBase=$JETTY_RESOURCE_BASE -DjettyXml=$JETTY_XML com.bigdata.rdf.sail.webapp.NanoSparqlServer $PORT $NAMESPACE $PROPERTIES_FILE  $*
 
