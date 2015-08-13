60ea136ac1695fc815ea214410f2d91c951f475e
beebs-systap <beebs@systap.com>
2015-08-13 15:49:22 -0400

    Update for clean script.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 #must be run from repository root
-#mvn -f blazegraph-parent/pom.xml clean
-for pom in `find bin -name "pom.xml" -print`; do
+mvn -f blazegraph-parent/pom.xml clean
 
-	dir=`echo $pom | sed 's/\/pom\.xml//g'`
-	echo "Cleaning $dir"
-	rm -rf $dir
-done
-
-for file in `find bin -type f -iname ".project" -print`; do
+for file in `find . -type f -iname ".project" -print`; do
 	rm -f $file
 done
 
