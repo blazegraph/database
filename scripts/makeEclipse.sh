e6d0783e596029d332def856186710d16c09f0c8
beebs-systap <beebs@systap.com>
2015-08-20 08:30:47 -0400

    Updates for pom version settings and helper scripts.

--- scripts/makeEclipse.sh
+++ scripts/makeEclipse.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #assume it is run as ./scripts/makeEclipse.sh
+#Clean everything
 ./scripts/clean.sh
+#Update the POM versions for a branch
+./scripts/setPomVersions.sh
+#Generate the eclipse projects
 mvn -f blazegraph-parent/pom.xml eclipse:eclipse
 
