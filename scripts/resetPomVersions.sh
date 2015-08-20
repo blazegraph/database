7b53df3c589130124293cda9e36aad45c3bf2539
beebs-systap <beebs@systap.com>
2015-08-20 08:30:47 -0400

    Updates for pom version settings and helper scripts.

--- scripts/resetPomVersions.sh
+++ scripts/resetPomVersions.sh
@@ -3,22 +3,12 @@
 # Must be run from the root of the bigdata project, i.e. ./scripts/updatePomVersions.sh
 
 PARENT_POM=./blazegraph-parent/pom.xml
+CURRENT_VERSION=1.5.3
+BRANCH="master"
+SNAPSHOT="SNAPSHOT"
 
-CURRENT_SNAPSHOT=`cat $PARENT_POM | grep "SNAPSHOT" | perl -n -e '/^.*\<version\>(.*)\<\/version\>.*$/ && printf("%s", $1)'`
-CURRENT_BRANCH=`cat .git/HEAD | cut -d\/ -f3`
+echo "Updating POM versions to ${CURRENT_VERSION}-${BRANCH}-${SNAPSHOT}"
 
-
-RESET_SNAPSHOT=`echo $CURRENT_SNAPSHOT | cut -d\- -f1`
-
-echo "${CURRENT_SNAPSHOT} -> ${RESET_SNAPSHOT}"
-
-for file in `find . -name "pom.xml" -maxdepth 2 -print`; do
-
-	cat $file | sed "s/version\>.*SNAPSHOT/\version\>${RESET_SNAPSHOT}-SNAPSHOT/" > /tmp/$$
-	cat /tmp/$$ > $file
-	rm -f /tmp/$$
-
-done
-
+mvn versions:set -DnewVersion=${CURRENT_VERSION}-${BRANCH}-${SNAPSHOT} versions:update-child-modules -f ${PARENT_POM}
 
 
