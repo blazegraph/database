51be7f2d5ff28e1e69e7c63e9b7f8d35dbbeeef4
beebs-systap <beebs@systap.com>
2015-08-27 14:49:51 -0400

    Update for makeEclipse to use the ECLIPSE_WORKSPACE if present and accept parameters.

--- scripts/makeEclipse.sh
+++ scripts/makeEclipse.sh
@@ -4,5 +4,17 @@
 ./scripts/clean.sh
 #Update the POM versions for a branch
 ./scripts/setPomVersions.sh
+
 #Generate the eclipse projects and compile
-mvn -f blazegraph-parent/pom.xml eclipse:eclipse
+#Use the ECLIPSE_WORKSPACE variable, if it is present
+#Pass along any commandline parameters to the maven command
+
+if [ -z ${ECLIPSE_WORKSPACE} ] ; then
+
+ 	mvn -f blazegraph-parent/pom.xml eclipse:eclipse $*
+
+else 
+
+	mvn -f blazegraph-parent/pom.xml eclipse:eclipse -Declipse.workspace=${ECLIPSE_WORKSPACE} $*
+
+fi
