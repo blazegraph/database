b3c5c070253c77744dacaba4a7ad49bcf0e30079
beebs-systap <beebs@systap.com>
2015-08-31 15:30:33 -0400

    Revert "Added padding to StringBuilder to prevent resize on the last iteration for large literals tests."
    Caused 22 test failures in bigdata-rdf:  https://ci.blazegraph.com/job/GIT_DEVELOPMENT_MAVEN/197/com.blazegraph$bigdata-rdf-test/#showFailuresLink
    
    This reverts commit 028f55ace5b09d3aadbc722af4fffbc998fd9dd6.

--- bigdata-rdf-test/src/test/java/com/bigdata/rdf/lexicon/TestFullTextIndex.java
+++ bigdata-rdf-test/src/test/java/com/bigdata/rdf/lexicon/TestFullTextIndex.java
@@ -197,7 +197,7 @@ public class TestFullTextIndex extends AbstractTripleStoreTestCase {
         
         final int len = store.getLexiconRelation().getLexiconConfiguration().getBlobsThreshold();
 
-		final StringBuilder sb = new StringBuilder(len + 20);
+        final StringBuilder sb = new StringBuilder(len);
 
         final String[] tokens = new String[] {
                 "apple",
