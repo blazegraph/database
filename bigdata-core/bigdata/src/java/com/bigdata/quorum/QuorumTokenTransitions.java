16ae78824d86814e2c0548d3f9e60fdd6f4ec759
beebs-systap <beebs@systap.com>
2015-09-02 12:27:16 -0400

    Checkpoint commit for HA refactoring.

--- bigdata-core/bigdata/src/java/com/bigdata/quorum/QuorumTokenTransitions.java
+++ bigdata-core/bigdata/src/java/com/bigdata/quorum/QuorumTokenTransitions.java
@@ -23,8 +23,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 package com.bigdata.quorum;
 
-import com.bigdata.ha.HAGlue;
-import com.bigdata.ha.QuorumService;
 
 /**
  * Wraps the token/join transitions in a testable manner.
