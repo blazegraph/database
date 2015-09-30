0c09ea82250c77142c08f22584226f1f46f665e8
beebs-systap <beebs@systap.com>
2015-09-30 14:29:21 -0400

    Update for dataLoader scripts.

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,21 +3,7 @@
 JETTY_XML=./bigdata-war-html/src/main/resources/jetty.xml
 JETTY_RESOURCE_BASE=./bigdata-war-html/src/main/webapp/
 
-mvn -f bigdata-jar/pom.xml clean package
+BASE_DIR=`dirname $0`
 
-if [ -z $JAVA_OPTS ] ; then
-	JAVA_OPTS="-ea -Xmx4g -server"
-fi
-
-if [ -z $JAVA_HOME ] ; then
-	JAVA=${JAVA_HOME}/bin/java
-else
-	JAVA=`which java`
-fi
-
-#$JAVA ${JAVA_OPTS} -jar bigdata-jar/target/bigdata-jar*.jar
-
-echo "Starting with JAVA_OPTS: $JAVA_OPTS."
-
-$JAVA ${JAVA_OPTS} -cp bigdata-jar/target/bigdata-jar*.jar -Djetty.resourceBase=$JETTY_RESOURCE_BASE -DjettyXml=$JETTY_XML com.bigdata.rdf.sail.webapp.NanoSparqlServer $*
+${BASE_DIR}/prog.sh -Djetty.resourceBase=$JETTY_RESOURCE_BASE -DjettyXml=$JETTY_XML com.bigdata.rdf.sail.webapp.NanoSparqlServer $*
 
