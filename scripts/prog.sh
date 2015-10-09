8f393cece94b38606681b6f3c8fe099633ebe602
Bryan Thompson <bryan@systap.com>
2015-10-09 08:59:37 -0400

    Addressing BLZG-1567 in load-performance branch
    @beebs-systap
    
    Note: Additional quotes on line 10

--- scripts/prog.sh
+++ scripts/prog.sh
@@ -2,12 +2,12 @@
 
 mvn -f bigdata-jar/pom.xml clean package
 
-if [ -z $JAVA_OPTS ] ; then
+if [ -z "$JAVA_OPTS" ] ; then
 	JAVA_OPTS="-ea -Xmx4g -server"
 fi
 
-if [ -z $JAVA_HOME ] ; then
-	JAVA=${JAVA_HOME}/bin/java
+if [ -z "$JAVA_HOME" ] ; then
+	JAVA="${JAVA_HOME}/bin/java"
 else
 	JAVA=`which java`
 fi
@@ -16,5 +16,5 @@ fi
 
 echo "Starting with JAVA_OPTS: $JAVA_OPTS."
 
-$JAVA ${JAVA_OPTS} -cp bigdata-jar/target/bigdata-jar*.jar $*
+"$JAVA" ${JAVA_OPTS} -cp bigdata-jar/target/bigdata-jar*.jar $*
 
