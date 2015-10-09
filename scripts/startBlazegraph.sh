b731dfc440b59e70a715b4dc6d989f23bddc6872
beebs-systap <beebs@systap.com>
2015-10-09 15:30:54 -0400

    Updates for https://jira.blazegraph.com/browse/BLZG-1567 Also updated to use the dirname to run either as ./scripts/.. or from the directory.

--- scripts/startBlazegraph.sh
+++ scripts/startBlazegraph.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-PORT=9999
-PROPERTIES_FILE=./bigdata-war-html/src/main/webapp/WEB-INF/RWStore.properties
-NAMESPACE="kb"
-
 BASE_DIR=`dirname $0`
 
-$BASE_DIR/prog.sh com.bigdata.rdf.sail.webapp.NanoSparqlServer $PORT $NAMESPACE $PROPERTIES_FILE  $*
+PORT=9999
+PROPERTIES_FILE="${BASE_DIR}"/../bigdata-war-html/src/main/webapp/WEB-INF/RWStore.properties
+NAMESPACE="kb"
+
+
+"$BASE_DIR"/prog.sh com.bigdata.rdf.sail.webapp.NanoSparqlServer $PORT $NAMESPACE $PROPERTIES_FILE  $*
 
