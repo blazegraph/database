53d1a0bcdb45e66b19709fccf92af58f5ff41173
Bryan Thompson <bryan@systap.com>
2015-12-02 13:43:13 -0500

    Adds BTreeCounters for cache hits and cache tests.
    The cache is actually the Reference pointer from the parent to the
    child.  If it is null or non-null but cleared (Reference.get() returns
    null), then there is a cache miss and we need to go to the backing
    store.  These counters allow us to inspect how frequently such cache
    misses vs hits occur on an index by index basis.

--- bigdata-core/bigdata/src/java/com/bigdata/btree/BTreeCounters.java
+++ bigdata-core/bigdata/src/java/com/bigdata/btree/BTreeCounters.java
@@ -155,6 +155,8 @@ final public class BTreeCounters implements Cloneable, ICounterSetAccess {
         ntupleUpdateDelete += o.ntupleUpdateDelete;
         ntupleRemove += o.ntupleRemove;
         // IO reads
+        cacheTests.add(o.cacheTests.get());
+        cacheMisses.add(o.cacheMisses.get());
         nodesRead.add(o.nodesRead.get());
         leavesRead.add(o.leavesRead.get());
         bytesRead.add(o.bytesRead.get());
@@ -221,6 +223,8 @@ final public class BTreeCounters implements Cloneable, ICounterSetAccess {
         t.ntupleUpdateDelete -= o.ntupleUpdateDelete;
         t.ntupleRemove -= o.ntupleRemove;
         // IO reads
+        t.cacheTests.add(-o.cacheTests.get());
+        t.cacheMisses.add(-o.cacheMisses.get());
         t.nodesRead.add(-o.nodesRead.get());
         t.leavesRead.add(-o.leavesRead.get());
         t.bytesRead.add(-o.bytesRead.get());
@@ -341,6 +345,10 @@ final public class BTreeCounters implements Cloneable, ICounterSetAccess {
      */
     
     // IO reads (concurrent)
+    /** #of tests of the BTree cache (getChild()). See BLZG-1657. Should correlate to #of getChild() calls. */
+    public final CAT cacheTests = new CAT();
+    /** #of misses when testing the BTree cache (getChild()). See BLZG-1657. Should correlate to nodesRead+leavesRead. */
+    public final CAT cacheMisses = new CAT();
     /** #of node read operations. */
     public final CAT nodesRead = new CAT();
     /** #of leaf read operations. */
@@ -755,6 +763,26 @@ final public class BTreeCounters implements Cloneable, ICounterSetAccess {
             {
 
                 final CounterSet tmp = counterSet.makePath(IBTreeCounters.IO);
+
+                /*
+                 * Cache.
+                 */
+
+                /** #of tests of the BTree cache (getChild()). See BLZG-1657. Should correlate to #of getChild() calls. */
+                tmp.addCounter("cacheTests", new Instrument<Long>() {
+                    @Override
+                    protected void sample() {
+                        setValue(cacheTests.get());
+                    }
+                });
+
+                /** #of misses when testing the BTree cache (getChild()). See BLZG-1657. Should correlate to nodesRead+leavesRead. */
+                tmp.addCounter("cacheMisses", new Instrument<Long>() {
+                    @Override
+                    protected void sample() {
+                        setValue(cacheMisses.get());
+                    }
+                });
 
                 /*
                  * bytes on store.
