bd406dcfc98ef2ced5769b2f7197033e4884c804
beebs-systap <beebs@systap.com>
2016-01-06 00:06:35 -0500

    Update for version scripts.

---
+++ scripts/setVersionNoBranch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script to update the version numbers with the git branch of the snapshot build.
+
+BASE_DIR=`dirname $0`
+PARENT_POM="${BASE_DIR}"/../blazegraph-parent/pom.xml
+DEPLOYER_POM="${BASE_DIR}"/../blazegraph-artifacts/pom.xml
+CURRENT_VERSION=2.0.0
+BRANCH="master"
+SNAPSHOT="RC2-SNAPSHOT"
+
+echo "Updating POM versions to ${CURRENT_VERSION}-${SNAPSHOT}"
+
+mvn versions:set -DnewVersion=${CURRENT_VERSION}-${SNAPSHOT} versions:update-child-modules -f ${PARENT_POM}
+
+mvn versions:set -DnewVersion=${CURRENT_VERSION}-${SNAPSHOT} versions:update-child-modules -f ${DEPLOYER_POM}
+
+
