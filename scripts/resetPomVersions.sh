335e2d7ba64f948f361c1915aabc40568c39ff83
beebs-systap <beebs@systap.com>
2016-01-06 10:04:27 -0500

    Updates for 2.1.0 branch and setting pom version scripts.

--- scripts/resetPomVersions.sh
+++ scripts/resetPomVersions.sh
@@ -3,15 +3,17 @@
 
 BASE_DIR=`dirname $0`
 PARENT_POM="${BASE_DIR}"/../blazegraph-parent/pom.xml
-DEPLOYER_POM="${BASE_DIR}"/../blazegraph-artifacts/pom.xml
-CURRENT_VERSION=2.0.0
+ARTIFACTS_POM="${BASE_DIR}"/../blazegraph-artifacts/pom.xml
+CURRENT_VERSION=2.1.0
 BRANCH="master"
-SNAPSHOT="RC2"
+SNAPSHOT="SNAPSHOT"
 
 echo "Updating POM versions to ${CURRENT_VERSION}-${BRANCH}-${SNAPSHOT}"
 
-mvn versions:set -DnewVersion=${CURRENT_VERSION}-${SNAPSHOT} versions:update-child-modules -f ${PARENT_POM}
+mvn versions:set -DnewVersion=${CURRENT_VERSION}-${BRANCH}-${SNAPSHOT} versions:update-child-modules -f ${PARENT_POM}
 
-mvn versions:set -DnewVersion=${CURRENT_VERSION}-${SNAPSHOT} versions:update-child-modules -f ${DEPLOYER_POM}
+mvn versions:set -DnewVersion=${CURRENT_VERSION}-${BRANCH}-${SNAPSHOT} versions:update-child-modules -f ${ARTIFACTS_POM}
+
+mvn versions:set -DnewVersion=${CURRENT_VERSION}-${BRANCH}-${SNAPSHOT} versions:update-parent -f ${ARTIFACTS_POM}
 
 
