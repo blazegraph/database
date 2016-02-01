80064c9e9e73747af97f511c15386cd1fc365196
beebs-systap <beebs@systap.com>
2016-02-01 14:22:08 -0500

    Checkpoint for private maven.

--- scripts/mavenDeploy.sh
+++ scripts/mavenDeploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BASE_DIR=`dirname $0`
 
-mvn -f "${BASE_DIR}"/../pom.xml clean deploy -DskipTests=true -Plocal-deploy
+mvn -f "${BASE_DIR}"/../pom.xml clean deploy -DskipTests=true -Plocal-deploy --projects junit-ext,ctc-striterators,lgpl-utils,dsi-utils,system-utils,rdf-properties,sparql-grammar,bigdata-util,bigdata-common-util,bigdata-statics,bigdata-cache,bigdata-client,bigdata-ganglia,bigdata-gas,bigdata-core/,bigdata-war-html,bigdata-blueprints,bigdata-runtime,bigdata-core-test,bigdata-rdf-test,bigdata-sails-test,blazegraph-jar
