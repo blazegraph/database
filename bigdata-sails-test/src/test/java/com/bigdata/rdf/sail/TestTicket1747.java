1fe17df0b5f8f81e3d3e972006546b140850c0b9
Igor Kim <igor.kim@ms2w.com>
2016-02-10 18:15:54 +0600

    test case for BLZG-1747

---
+++ bigdata-sails-test/src/test/java/com/bigdata/rdf/sail/TestTicket1747.java
@@ -0,0 +1,139 @@
+/**
+Copyright (C) SYSTAP, LLC DBA Blazegraph 2011.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC DBA Blazegraph
+     2501 Calvert ST NW #106
+     Washington, DC 20008
+     licenses@blazegraph.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.bigdata.rdf.sail;
+
+import java.io.IOException;
+import java.util.Set;
+
+import org.openrdf.model.URI;
+import org.openrdf.model.vocabulary.XMLSchema;
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFParseException;
+
+import com.bigdata.rdf.internal.IV;
+import com.bigdata.rdf.model.BigdataURIImpl;
+import com.bigdata.rdf.model.BigdataValueFactory;
+import com.bigdata.rdf.sail.sparql.Bigdata2ASTSPARQLParser;
+import com.bigdata.rdf.sparql.ast.ASTContainer;
+import com.bigdata.rdf.sparql.ast.eval.ASTDeferredIVResolution;
+
+/**
+ * Test suite for an issue where IV resolution of vocabulary terms
+ * interferes with query parsing and deferred IV resolution ({@link ASTDeferredIVResolution}.
+ * 
+ * @see <a href="https://jira.blazegraph.com/browse/BLZG-1747">
+ * Persistent "Unknown extension" issue in WIkidata Query Service</a>
+ */
+public class TestTicket1747 extends QuadsTestCase {
+	
+    public TestTicket1747() {
+	}
+
+	public TestTicket1747(String arg0) {
+		super(arg0);
+	}
+
+	public void testBug() throws Exception {
+
+	    // try with Sesame MemoryStore:
+//		executeQuery(new SailRepository(new MemoryStore()));
+
+//		Setup a triple store using a vocabulary that declares xsd:dateTime (this should be in the default vocabulary).
+		
+		final BigdataSail sail = getSail();
+		try {
+			executeQuery(new BigdataSailRepository(sail));
+		} finally {
+			sail.__tearDownUnitTest();
+		}
+	}
+
+	private void executeQuery(final BigdataSailRepository repo)
+			throws RepositoryException, MalformedQueryException,
+			QueryEvaluationException, RDFParseException, IOException {
+		try {
+			repo.initialize();
+			final BigdataSailRepositoryConnection conn = repo.getConnection();
+			conn.setAutoCommit(false);
+			BigdataValueFactory vf = conn.getValueFactory();
+			try {
+			
+				//				Verify that you can resolve xsd:dateTime to a non-mock IV.
+				IV xsdDateTimeIV = conn.getTripleStore().getLexiconRelation().resolve(vf.createURI(XMLSchema.DATETIME.stringValue())).getIV();
+				assertTrue(xsdDateTimeIV.isVocabulary());
+				assertFalse(xsdDateTimeIV.isNullIV());
+
+//				Parse the first query on this ticket.
+				String query = "PREFIX wdt: <http://www.wikidata.org/prop/direct/>\r\n" + 
+						"PREFIX wikibase: <http://wikiba.se/ontology#>\r\n" + 
+						"PREFIX p: <http://www.wikidata.org/prop/>\r\n" + 
+						"PREFIX v: <http://www.wikidata.org/prop/statement/>\r\n" + 
+						"PREFIX q: <http://www.wikidata.org/prop/qualifier/>\r\n" + 
+						"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\r\n" + 
+						"SELECT ?entity (year(?date) as ?year)\r\n" + 
+						"WHERE\r\n" + 
+						"{\r\n" + 
+						"?entityS wdt:P569 ?date .\r\n" + 
+						"SERVICE wikibase:label {\r\n" + 
+						"bd:serviceParam wikibase:language \"en\" .\r\n" + 
+						"?entityS rdfs:label ?entity\r\n" + 
+						"}\r\n" + 
+						"FILTER (datatype(?date) = xsd:dateTime)\r\n" + 
+						"FILTER (month(?date) = month(now()))\r\n" + 
+						"FILTER (day(?date) = day(now()))\r\n" + 
+						"}\r\n" + 
+						"LIMIT 10"; 
+		        final ASTContainer astContainer = new Bigdata2ASTSPARQLParser().parseQuery2(query, null);
+
+//				Verify that re-resolution of xsd:dateTime does not return a MockIV.
+		        xsdDateTimeIV = conn.getTripleStore().getLexiconRelation().resolve(XMLSchema.DATETIME).getIV();
+		        assertTrue(xsdDateTimeIV.isVocabulary());
+		        assertFalse(xsdDateTimeIV.isNullIV());
+
+//				Invoke deferred IV resolution on the parsed query.
+		        ASTDeferredIVResolution.resolveQuery(conn.getTripleStore(), astContainer);
+		        // astContainer instance is updated with resolved IVs at this point
+	        
+//				Verify that re-resolution of xsd:dateTime does not return a MockIV.
+		        xsdDateTimeIV = conn.getTripleStore().getLexiconRelation().resolve(XMLSchema.DATETIME).getIV();
+		        assertTrue(xsdDateTimeIV.isVocabulary());
+		        assertFalse(xsdDateTimeIV.isNullIV());
+//				Verify that the parsed query does not contain a MockIV for xsd:dateTime
+		        // TODO
+			} finally {
+				conn.close();
+			}
+		} finally {
+			repo.shutDown();
+		}
+	}
+}
