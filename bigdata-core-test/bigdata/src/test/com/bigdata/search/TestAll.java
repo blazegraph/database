46e5e4f731a754f9700fdc081ba67dba578e213f
Brad Bebee <beebs@blazegraph.com>
2016-03-23 12:00:01 -0400

    Update TestAll.java
    Restored test suite.

--- bigdata-core-test/bigdata/src/test/com/bigdata/search/TestAll.java
+++ bigdata-core-test/bigdata/src/test/com/bigdata/search/TestAll.java
@@ -71,8 +71,7 @@ public class TestAll extends TestCase {
 //        suite.addTest(proxySuite(new TestLDS("LDS Search"),"LDS"));
 
         // search backed by EDS.
-        //FIXME:  Remove comment when testing is done.
-       // suite.addTest(proxySuite(new TestEDS("EDS Search"),"EDS"));
+        suite.addTest(proxySuite(new TestEDS("EDS Search"),"EDS"));
         
         suite.addTestSuite(TestLanguageRange.class);
 
