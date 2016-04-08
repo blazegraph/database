58cdd04615124b040ef0e47e6753c550c748e2a7
Igor Kim <igor.kim@ms2w.com>
2016-04-08 17:12:17 +0600

    There is no geo spacial configuration defined for STRING datatype of plain literals, so configuration could be null in GetSpatialServiceFactory

--- bigdata-core/bigdata-rdf/src/java/com/bigdata/rdf/sparql/ast/eval/GeoSpatialServiceFactory.java
+++ bigdata-core/bigdata-rdf/src/java/com/bigdata/rdf/sparql/ast/eval/GeoSpatialServiceFactory.java
@@ -2418,7 +2418,7 @@ public class GeoSpatialServiceFactory extends AbstractServiceFactoryBase {
             if (lit.getDatatype() != null) {
                 // If we have datatype that can extract coordinates, use it to exteract
                 pconfig = geoSpatialConfig.getConfigurationForDatatype(lit.getDatatype());
-                if (pconfig.hasLat() && pconfig.hasLon()) {
+                if (pconfig != null && pconfig.hasLat() && pconfig.hasLon()) {
                     serializer = pconfig.getLiteralSerializer();
                 }
             }
