e4c63e130cf6bc6273f23f7e0e7b70f495908ef8
beebs-systap <beebs@systap.com>
2016-05-25 15:56:25 -0400

    BLZG-1936, BLZG-1937 * Adds inline URI handler for UUIDs encoded in Hex * Adds prefixed suffixed inline UUID handler * Adds prefixed suffixed inline Hex UUID handler * Adds suffixed inline Hex UUID handler * Adds suffixed inline UUID handler * Adds unit tests for each

--- bigdata-rdf-test/src/test/java/com/bigdata/rdf/internal/TestInlineURIHandlers.java
+++ bigdata-rdf-test/src/test/java/com/bigdata/rdf/internal/TestInlineURIHandlers.java
@@ -1,9 +1,12 @@
 package com.bigdata.rdf.internal;
 
+import java.util.UUID;
+
 import junit.framework.TestCase2;
 
 import org.junit.Test;
 
+import com.bigdata.rdf.internal.impl.literal.UUIDLiteralIV;
 import com.bigdata.rdf.internal.impl.literal.XSDNumericIV;
 import com.bigdata.rdf.internal.impl.uri.URIExtensionIV;
 
@@ -214,6 +217,122 @@ public class TestInlineURIHandlers extends TestCase2 {
 
 		assertTrue (intValue == iv.getInlineValue().intValue());
 		
+		assertTrue (localName.equals(handler.getLocalNameFromDelegate(iv)));
+		
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Test	
+	public void testHexUUIDURIHandler() {
+	
+		final String nameSpace = "http://blazegraph.com/hex_uuid/";
+		final String localName = "010072F0000038090100000000D56C9E";
+		
+		InlineHexUUIDURIHandler handler = new InlineHexUUIDURIHandler(nameSpace);
+		
+		UUIDLiteralIV iv = (UUIDLiteralIV) handler.createInlineIV(localName);
+		
+		if(log.isDebugEnabled()) {
+			log.debug(iv.getDTE().name());
+		}
+		
+		assertTrue (iv != null);
+
+		assertTrue (localName.equals(handler.getLocalNameFromDelegate(iv)));
+		
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Test	
+	
+	public void testSuffixedHexUUIDURIHandler() {
+	
+		final String nameSpace = "http://blazegraph.com/hex_uuid/";
+		final String suffix = "suffix";
+		final String localName = "010072F0000038090100000000D56C9E"+suffix;
+		
+		InlineSuffixedHexUUIDURIHandler handler = new InlineSuffixedHexUUIDURIHandler(nameSpace, suffix);
+		
+		UUIDLiteralIV iv = (UUIDLiteralIV) handler.createInlineIV(localName);
+		
+		if(log.isDebugEnabled()) {
+			log.debug(iv.getDTE().name());
+		}
+		
+		assertTrue (iv != null);
+
+		assertTrue (localName.equals(handler.getLocalNameFromDelegate(iv)));
+		
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Test	
+	public void testSuffixedUUIDURIHandler() {
+	
+		final String nameSpace = "http://blazegraph.com/uuid/";
+		final UUID uuid = UUID.randomUUID();
+		final String suffix = "suffix";
+		final String localName = uuid.toString()+suffix;
+		
+		InlineSuffixedUUIDURIHandler handler = new InlineSuffixedUUIDURIHandler(nameSpace, suffix);
+		
+		UUIDLiteralIV iv = (UUIDLiteralIV) handler.createInlineIV(localName);
+		
+		if(log.isDebugEnabled()) {
+			log.debug(iv.getDTE().name());
+		}
+		
+		assertTrue (iv != null);
+
+		assertTrue (localName.equals(handler.getLocalNameFromDelegate(iv)));
+		
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Test	
+	
+	public void testPrefixedSuffixedHexUUIDURIHandler() {
+	
+		final String nameSpace = "http://blazegraph.com/hex_uuid/";
+		final String prefix = "prefix";
+		final String suffix = "suffix";
+		final String localName = prefix+"010072F0000038090100000000D56C9E"+suffix;
+		
+		InlinePrefixedSuffixedHexUUIDURIHandler handler = new InlinePrefixedSuffixedHexUUIDURIHandler(nameSpace, prefix, suffix);
+		
+		UUIDLiteralIV iv = (UUIDLiteralIV) handler.createInlineIV(localName);
+		
+		if(log.isDebugEnabled()) {
+			log.debug(iv.getDTE().name());
+		}
+		
+		assertTrue (iv != null);
+
+		assertTrue (localName.equals(handler.getLocalNameFromDelegate(iv)));
+		
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Test	
+	public void testPrefixedSuffixedUUIDURIHandler() {
+	
+		final String nameSpace = "http://blazegraph.com/uuid/";
+		final UUID uuid = UUID.randomUUID();
+		final String prefix = "prefix";
+		final String suffix = "suffix";
+		final String localName = prefix+uuid.toString()+suffix;
+		
+		InlinePrefixedSuffixedUUIDURIHandler handler = new InlinePrefixedSuffixedUUIDURIHandler(
+				nameSpace, prefix, suffix);
+		
+		UUIDLiteralIV iv = (UUIDLiteralIV) handler.createInlineIV(localName);
+		
+		if(log.isDebugEnabled()) {
+			log.debug(iv.getDTE().name());
+		}
+		
+		assertTrue (iv != null);
+
 		assertTrue (localName.equals(handler.getLocalNameFromDelegate(iv)));
 		
 	}
