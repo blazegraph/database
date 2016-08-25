f6c2dba9b85a587df3baf4a2adf14389ea798b72
beebs-systap <beebs@systap.com>
2016-08-25 10:35:21 -0400

    Updates per Araxis review.

--- bigdata-core/bigdata-rdf/src/java/com/bigdata/rdf/model/BigdataValueFactoryImpl.java
+++ bigdata-core/bigdata-rdf/src/java/com/bigdata/rdf/model/BigdataValueFactoryImpl.java
@@ -43,7 +43,6 @@ import org.openrdf.model.URI;
 import org.openrdf.model.Value;
 import org.openrdf.model.datatypes.XMLDatatypeUtil;
 import org.openrdf.model.impl.BooleanLiteralImpl;
-import org.openrdf.model.vocabulary.RDF;
 
 import com.bigdata.cache.WeakValueCache;
 import com.bigdata.rdf.internal.IV;
@@ -370,9 +369,6 @@ public class BigdataValueFactoryImpl implements BigdataValueFactory {
     private final BigdataURIImpl xsd_boolean = new BigdataURIImpl(this, xsd
             + "boolean");
 
-    private final BigdataURIImpl rdf_langstring = new BigdataURIImpl(this,
-    		RDF.LANGSTRING.stringValue());
-
 //    private final BigdataLiteralImpl TRUE = new BigdataLiteralImpl(this, "true", null,
 //            xsd_boolean);
 //
@@ -394,7 +390,7 @@ public class BigdataValueFactoryImpl implements BigdataValueFactory {
 
 		final BigdataURIImpl[] a = new BigdataURIImpl[] { xsd_string,
 				xsd_dateTime, xsd_date, xsd_long, xsd_int, xsd_byte, xsd_short,
-				xsd_double, xsd_float, xsd_boolean, rdf_langstring };
+				xsd_double, xsd_float, xsd_boolean };
 
 		for (BigdataURIImpl x : a) {
 
